a5afe7884b3556e74e5770db3838dd123b80eb8e
E. Dronkert <ednl@users.noreply.github.com>
2023-12-01 10:54:08 +0100

    23-1

--- 2023/01.c
+++ 2023/01.c
@@ -1,6 +1,6 @@
 /**
  * Advent of Code 2023
- * Day 1: ?
+ * Day 1: Trebuchet?!
  * https://adventofcode.com/2023/day/1
  * By: E. Dronkert https://github.com/ednl
  */
@@ -8,31 +8,75 @@
 #include <stdio.h>   // fopen, fclose, getline, printf
 #include <stdlib.h>  // free
 #include <ctype.h>   // isdigit
+#include <string.h>  // strlen
 
 static const char* name[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-static const int namelen[] = {4, 3, 3, 5, 4, 4, 3, 5, 5, 4};
+static int namelen[10];
+
+static int first(const char* const msg, const ssize_t len, const int part)
+{
+    for (int i = 0; i < len; ++i) {
+        if (isdigit(msg[i]))
+            return msg[i] - '0';
+        if (part == 2)
+            for (int digit = 1; digit <= 9; ++digit) {
+                int j = i, k = 0;
+                while (j < len && k < namelen[digit] && msg[j] == name[digit][k]) {
+                    ++j;
+                    ++k;
+                }
+                if (k == namelen[digit])
+                    return digit;
+            }
+    }
+    return 0;
+}
+
+static int last(const char* const msg, const ssize_t len, const int part)
+{
+    for (int i = (int)len - 1; i >= 0; --i) {
+        if (isdigit(msg[i]))
+            return msg[i] - '0';
+        if (part == 2)
+            for (int digit = 1; digit <= 9; ++digit) {
+                int j = i, k = namelen[digit] - 1;
+                while (j >= 0 && k >= 0 && msg[j] == name[digit][k]) {
+                    --j;
+                    --k;
+                }
+                if (k == -1)
+                    return digit;
+            }
+    }
+    return 0;
+}
+
+static int calibration(const char* const msg, const ssize_t len, const int part)
+{
+    return first(msg, len, part) * 10 + last(msg, len, part);
+}
 
 int main(void)
 {
-    int a = 0, b = 0;
-    char *buf = NULL;
+    FILE* f = fopen("../aocinput/2023-01-input.txt", "r");
+    if (!f)
+        return 1;
+
+    for (int i = 1; i <= 9; ++i)
+        namelen[i] = (int)strlen(name[i]);
+
+    int part1 = 0, part2 = 0;
+    char* buf = NULL;
     size_t bufsz;
     ssize_t len;
-    FILE *f = fopen("../aocinput/2023-01-input.txt", "r");
     while ((len = getline(&buf, &bufsz, f)) > 1) {
-        ssize_t i = 0, j = len - 1;
-        while (i < len && !isdigit(buf[i]))
-            ++i;
-        while (j > 0 && !isdigit(buf[j]))
-            --j;
-        if (i <= j) {
-            // printf("%c%c %s", buf[i], buf[j], buf);
-            a += (buf[i] - '0') * 10 + buf[j] - '0';
-        }
+        buf[--len] = '\0';  // remove newline
+        part1 += calibration(buf, len, 1);
+        part2 += calibration(buf, len, 2);
     }
     fclose(f);
     free(buf);
-    printf("%d\n%d\n", a, b);  // 54630 ?
 
+    printf("Part 1: %d\nPart 2: %d\n", part1, part2);  // example: 209 281, input: 54630 54770
     return 0;
 }
