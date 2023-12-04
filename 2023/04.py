986f16c549afc1cd84ae7063923e9604dc441820
E. Dronkert <ednl@users.noreply.github.com>
2023-12-04 22:04:08 +0100

    timing

--- 2023/04.py
+++ 2023/04.py
@@ -1,7 +1,12 @@
+from time import monotonic_ns
+t0 = monotonic_ns()
+
 import numpy as np
+t1 = monotonic_ns()
 
 win = np.loadtxt("../aocinput/2023-04-input.txt", dtype=np.uint8, usecols=range(2,12))
 our = np.loadtxt("../aocinput/2023-04-input.txt", dtype=np.uint8, usecols=range(13,38))
+t2 = monotonic_ns()
 
 cards = win.shape[0]
 copies = np.ones(cards, dtype=np.uint32)
@@ -15,5 +20,13 @@ for i in range(cards):
         if j < cards:
             k = min(j + match, cards)
             copies[j:k] += copies[i]
+t3 = monotonic_ns()
 
 print(score, np.sum(copies))
+t4 = monotonic_ns()
+
+print('import  : {:6.2f} ms'.format((t1 - t0)/1000000))
+print('loadtxt : {:6.2f} ms'.format((t2 - t1)/1000000))
+print('logic   : {:6.2f} ms'.format((t3 - t2)/1000000))
+print('print   : {:6.2f} ms'.format((t4 - t3)/1000000))
+print('total   : {:6.2f} ms'.format((t4 - t0)/1000000))
