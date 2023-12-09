621e25049c732cda6b69aeea9dcdcdb8ef8c2b99
E. Dronkert <ednl@users.noreply.github.com>
2023-12-09 11:58:24 +0100

    clean up

--- 2023/09.c
+++ 2023/09.c
@@ -21,6 +21,19 @@
 
 static int data[N][M], diff[N][M];
 
+// Show reduced matrix = difference table on one line
+static void showtable(void)
+{
+#if EXAMPLE || defined(DEBUG)
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j)
+            printf(" %d", diff[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+#endif
+}
+
 int main(void)
 {
     // Read data from file
@@ -34,57 +47,46 @@ int main(void)
 
     // Part 1
     memcpy(diff, data, sizeof data);
+
     // Reduce forward from left, collapse difference table pyramid into one line
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < M - 1; ++j)  // M-1 iterations
             for (int k = 0; k < M - j - 1; ++k)  // leave last value untouched, then modify 1 fewer each iteration
                 diff[i][k] = diff[i][k + 1] - diff[i][k];
 
-#if EXAMPLE || defined(DEBUG)
-    // Show reduced matrix, example:
+    showtable();
     // 0 0 0 0 3 15
     // 0 0 0 1 6 21
     // 0 0 2 6 15 45
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j)
-            printf(" %d", diff[i][j]);
-        printf("\n");
-    }
-#endif
 
     // Sum all values of reduced matrix
-    int sum = 0;
+    int part1 = 0;
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < M; ++j)
-            sum += diff[i][j];
-    printf("Part 1: %d\n", sum);  // example: 114, input: 1861775706
+            part1 += diff[i][j];
 
     // Part 2
     memcpy(diff, data, sizeof data);
+
     // Reduce backward from right, collapse difference table pyramid into one line
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < M - 1; ++j)  // M-1 iterations
             for (int k = M - 1; k > j; --k)  // leave first value untouched, then modify 1 fewer each iteration
                 diff[i][k] = diff[i][k] - diff[i][k - 1];
 
-#if EXAMPLE || defined(DEBUG)
-    // Show reduced matrix, example:
+    showtable();
     // 0 3 0 0 0 0
     // 1 2 1 0 0 0
     // 10 3 0 2 0 0
-    // Sum: 0-3+0-0+0-0 + 1-2+1-0+0-0 + 10-3+0-2+0-0 = -3 + 0 + 5 = 2
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j)
-            printf(" %d", diff[i][j]);
-        printf("\n");
-    }
-#endif
 
     // Sum all values of reduced matrix by adding even columns and subtracting odd columns
-    sum = 0;
+    // Example: 0-3+0-0+0-0 + 1-2+1-0+0-0 + 10-3+0-2+0-0 = -3 + 0 + 5 = 2
+    int part2 = 0;
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < M - 1; j +=2)  // skips last column if M is odd, but last value is always zero anyway
-            sum += diff[i][j] - diff[i][j + 1];
-    printf("Part 2: %d\n", sum);  // example: 2, input: 1082
+            part2 += diff[i][j] - diff[i][j + 1];
+
+    printf("Part 1: %d\n", part1);  // example: 114, input: 1861775706
+    printf("Part 2: %d\n", part2);  // example: 2  , input: 1082
     return 0;
 }
