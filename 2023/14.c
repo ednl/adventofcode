95a1d3eb2d5fe5cb46a2017d502a58b176d498ca
E. Dronkert <ednl@users.noreply.github.com>
2023-12-14 15:40:32 +0100

    comments

--- 2023/14.c
+++ 2023/14.c
@@ -27,29 +27,31 @@
 #define NAME "../aocinput/2023-14-input.txt"
 #define N 100
 #endif
+
 #define LIM (N + 1)
 #define DIM (N + 2)
 #define CYCLES 1000000000
+#define STEADY 100  // how many cycles to reach steady state loop
 
 static char map[DIM][DIM + 2];
 static char rot[DIM][DIM + 2];
 
-// Rotate anti-clockwise ("left")
+// Rotate anti-clockwise
 static void rot90(void)
 {
     memcpy(rot, map, sizeof map);
     for (int i = 1; i < LIM; ++i)
         for (int j = 1; j < LIM; ++j)
-            map[i][j] = rot[j][LIM - i];  // transpose, mirror horizontally
+            map[i][j] = rot[j][LIM - i];  // transpose and mirror horiz
 }
 
-// Rotate clockwise ("right")
+// Rotate clockwise
 static void rot270(void)
 {
     memcpy(rot, map, sizeof map);
     for (int i = 1; i < LIM; ++i)
         for (int j = 1; j < LIM; ++j)
-            map[i][j] = rot[LIM - j][i];  // transpose, mirror vertically
+            map[i][j] = rot[LIM - j][i];  // transpose and mirror vert
 }
 
 // Rotate 180 degrees
@@ -58,7 +60,7 @@ static void rot180(void)
     memcpy(rot, map, sizeof map);
     for (int i = 1; i < LIM; ++i)
         for (int j = 1; j < LIM; ++j)
-            map[i][j] = rot[LIM - i][LIM - j];  // mirror horizontally and vertically
+            map[i][j] = rot[LIM - i][LIM - j];  // mirror horiz and vert
 }
 
 #if EXAMPLE
@@ -80,30 +82,31 @@ static int max(const int a, const int b)
     return a > b ? a : b;
 }
 
-// Roll all round rocks to the left (West)
+// Roll round rocks all the way to the left
 static void roll(void)
 {
     for (int i = 1; i < LIM; ++i)  // row
         for (int head = 1; head < LIM; ++head) {  // col
-            while (head < LIM && map[i][head] != 'O')
+            while (head < LIM && map[i][head] != 'O')  // look for first round rock
                 ++head;
             if (head < LIM) {
                 int dest = head;
-                while (map[i][--dest] == '.');
-                const int shift = head - ++dest;
+                while (map[i][--dest] == '.');  // look for open space to the left
+                const int shift = head - ++dest;  // how far to move left
                 if (shift) {
                     int tail = head;
-                    while (map[i][++tail] == 'O');
-                    const int len = min(shift, tail - head);
-                    const int rem = max(head, tail - shift);
-                    memset(&map[i][dest], 'O', (size_t)len);
-                    memset(&map[i][rem] , '.', (size_t)len);
+                    while (map[i][++tail] == 'O');  // look for end of round rock sequence
+                    const int len = min(shift, tail - head);  // how many rocks to add+remove
+                    const int rem = max(head, tail - shift);  // start of rocks to be removed
+                    memset(&map[i][dest], 'O', (size_t)len);  // add round rocks
+                    memset(&map[i][rem] , '.', (size_t)len);  // remove round rocks
                 }
             }
         }
 }
 
 // Total load on the South support beams (= North, if N = down)
+// Also used as unique configuration ID, which happens to work
 static int load(void)
 {
     int sum = 0;
@@ -156,17 +159,16 @@ int main(void)
     printf("Part 1: %d\n", load());  // example: 136, input: 113525
 
     // Part 2
-    // Complete first cycle from part 1: N = down, next roll left should be W
+    // Complete first cycle from part 1: begin with N = down
     rot180(); /* N = up    */ roll(); // roll left = W
     rot270(); /* N = right */ roll(); // roll left = S
     rot270(); /* N = down  */ roll(); // roll left = E
 
     // Get reference value after 100 cycles
-    // which is enough to reach the steady state loop for my input
-    const int base = 100;
-    for (int i = 1; i < base; ++i)
+    // (for my input enough to reach steady state loop)
+    for (int i = 1; i < STEADY; ++i)  // one cycle already done
         cycle();
-    const int ref = load();
+    const int ref = load();  // not necessarily unique, but it works
 
     // How long is the loop?
     int loop = 0;
@@ -176,11 +178,10 @@ int main(void)
     } while (load() != ref);
 
     // How many left to reach all teh CYCLES in a whole number of loops?
-    const int mod = (CYCLES - base) % loop;
+    const int mod = (CYCLES - STEADY) % loop;
     for (int i = 0; i < mod; ++i)
         cycle();
     printf("Part 2: %d\n", load());  // example: 64, input: 101292
-
     printf("Time: %.0f ms\n", stoptimer_ms());
     return 0;
 }
