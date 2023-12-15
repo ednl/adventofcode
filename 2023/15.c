29e70a4ac4d198be4dbf22b9eabcf4b132d5ca68
E. Dronkert <ednl@users.noreply.github.com>
2023-12-15 11:05:19 +0100

    with static allocation

--- 2023/15.c
+++ 2023/15.c
@@ -1,6 +1,6 @@
 /**
  * Advent of Code 2023
- * Day 15: ???
+ * Day 15: Lens Library
  * https://adventofcode.com/2023/day/15
  * By: E. Dronkert https://github.com/ednl
  *
@@ -8,10 +8,10 @@
  *    clang -std=gnu17 -Ofast -march-native -Wall 15.c ../startstoptimer.c
  *    gcc   -std=gnu17 -Ofast -march-native -Wall 15.c ../startstoptimer.c
  * Get minimum runtime:
- *     m=999999;for((i=0;i<2000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
+ *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Apple M1 Mac Mini      : 507 µs
- *     Raspberry Pi 5 2.4 GHz : ?? µs
+ *     Apple M1 Mac Mini      : 502 µs
+ *     Raspberry Pi 5 2.4 GHz : ? µs
  */
 
 #include <stdio.h>    // fopen, fclose, fgets, printf
@@ -36,8 +36,8 @@ typedef struct lens {
 } Lens;
 
 typedef struct box {
-    Lens* lens;
-    size_t size, count;
+    Lens lens[M];
+    int count;
 } Box;
 
 static Box box[N];
@@ -61,31 +61,20 @@ static int hash(const char* s)
     return h & 0xff;
 }
 
-static bool grow(Lens** arr, size_t* oldsize)
-{
-    size_t newsize = *oldsize ? *oldsize << 1 : M;
-    Lens* p = realloc(*arr, newsize * sizeof **arr);
-    if (!p)
-        return false;
-    *arr = p;
-    *oldsize = newsize;
-    return true;
-}
-
 // Remove lens from box
 // Return true if found and removed, false if box empty or lens not in box
 static bool rem(const char* label)
 {
     Box* b = &box[hash(label)];
-    if (!b->lens || !b->size || !b->count)
+    if (!b->count)
         return false;
     const int64_t id = label2id(label);
     const Lens* end = b->lens + b->count;
-    for (Lens* a = b->lens; a != end; ++a)
-        if (a->id == id) {
-            const Lens* next = a + 1;
+    for (Lens* lens = b->lens; lens != end; ++lens)
+        if (lens->id == id) {
+            const Lens* next = lens + 1;
             if (next != end)
-                memcpy(a, next, (size_t)(end - next) * sizeof *a);
+                memcpy(lens, next, (size_t)(end - next) * sizeof *lens);
             --b->count;
             return true;
         }
@@ -97,17 +86,18 @@ static bool rem(const char* label)
 static bool add(const char* label, const int focal)
 {
     Box* b = &box[hash(label)];
-    if ((!b->lens || !b->size) && !grow(&b->lens, &b->size))
-        return false;
     const int64_t id = label2id(label);
-    for (size_t i = 0; i < b->count; ++i)
-        if (b->lens[i].id == id) {
-            b->lens[i].focal = focal;  // replace
+    const Lens* end = b->lens + M;
+    Lens* tail = b->lens + b->count;
+    for (Lens* lens = b->lens; lens != tail; ++lens)
+        if (lens->id == id) {
+            lens->focal = focal;  // replace
             return true;
         }
-    if (b->count == b->size && !grow(&b->lens, &b->size))
+    if (tail == end)
         return false;
-    b->lens[b->count++] = (Lens){id, focal};  // append
+    *tail = (Lens){id, focal};  // append
+    ++b->count;
     return true;
 }
 
@@ -121,33 +111,13 @@ static int power(void)
     return sum;
 }
 
-static void clean(void)
-{
-#if EXAMPLE || defined(DEBUG)
-    size_t used = 0, maxc = 0, maxs = 0;
-    for (int i = 0; i < N; ++i) {
-        if (box[i].count > maxc) maxc = box[i].count;
-        if (box[i].size > maxs) maxs = box[i].size;
-        if (box[i].lens) {
-            ++used;
-            printf("%d: %3zu %4zu %p\n", i, box[i].count, box[i].size, (void*)box[i].lens);
-            free(box[i].lens);
-        }
-    }
-    printf("used= %zu maxcount=%zu maxsize=%zu\n", used, maxc, maxs);
-#else
-    for (int i = 0; i < N; ++i)
-        free(box[i].lens);
-#endif
-}
-
 int main(void)
 {
     starttimer();
     FILE* f = fopen(NAME, "r");
     if (!f)
         return 1;
-    char buf[32], *s = buf;
+    char buf[16], *s = buf;
     int c, part1 = 0;
     while ((c = fgetc(f)) != EOF)
         switch (c) {
@@ -158,9 +128,8 @@ int main(void)
             default  : *s++ = (char)c;
         }
     fclose(f);
-    printf("Part 1: %d\n", part1);  // example: 1320, input: 514394
-    printf("Part 2: %d\n", power());  // example: 145, input: 236358
-    clean();
+    printf("Part 1: %d\n", part1);    // example: 1320, input: 514394
+    printf("Part 2: %d\n", power());  // example:  145, input: 236358
     printf("Time: %.0f us\n", stoptimer_us());
     return 0;
 }
