053b97609e83365f440b44f80fea974d50119bf2
E. Dronkert <ednl@users.noreply.github.com>
2023-12-18 21:54:06 +0100

    clean up

--- 2023/18.c
+++ 2023/18.c
@@ -1,30 +1,48 @@
+/**
+ * Advent of Code 2023
+ * Day 18: Lavaduct Lagoon
+ * https://adventofcode.com/2023/day/18
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Theory:
+ *     https://en.wikipedia.org/wiki/Shoelace_formula
+ *     https://en.wikipedia.org/wiki/Pick%27s_theorem
+ *
+ * Compile:
+ *    clang -std=gnu17 -Ofast -march=native -Wall 18.c ../startstoptimer.c
+ *    gcc   -std=gnu17 -Ofast -march=native -Wall 18.c ../startstoptimer.c
+ * Get minimum runtime:
+ *     m=999999;for((i=0;i<1000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
+ * Minimum runtime:
+ *     Apple M1 Mac Mini 2020 (3.2 GHz)               :    ? us
+ *     Raspberry Pi 5 (2.4 GHz)                       :    ? us
+ *     Apple iMac 2013 (Core i5 Haswell 4570 3.2 GHz) :  223 us
+ *     Raspberry Pi 4 (1.8 GHz)                       :    ? us
+ */
+
 #include <stdio.h>
 #include <stdint.h>
+#include "../startstoptimer.h"
 
-#define EXAMPLE 1
+#define EXAMPLE 0
 #if EXAMPLE
 #define NAME "../aocinput/2023-18-example.txt"
-#define DIG 14  // number of trench digging instructions in example
+#define N 14  // number of trench digging instructions in example
 #else
 #define NAME "../aocinput/2023-18-input.txt"
-#define DIG 766  // number of trench digging instructions in input
+#define N 766  // number of trench digging instructions in input
 #endif
 
 typedef enum dir {
     RIGHT, DOWN, LEFT, UP
 } Dir;
 
-typedef struct vec {
-    int x, y;
-} Vec;
-
 typedef struct dig {
     Dir dir;
-    int64_t len;
+    int len;
 } Dig;
 
-static const char dir2char[] = "RDLU";
-static Dig trench[2][DIG];  // two different trenches for parts 1 & 2
+static Dig trench[2][N];  // two different trenches for parts 1 & 2
 
 static Dir char2dir(const char c)
 {
@@ -39,34 +57,41 @@ static Dir char2dir(const char c)
 
 int main(void)
 {
+    starttimer();
     FILE* f = fopen(NAME, "r");
     char c;
-    unsigned len, rgb;
-    for (int i = 0; i < DIG && fscanf(f, " %c %2u (#%6x)", &c, &len, &rgb) == 3; ++i) {
-        trench[0][i] = (Dig){char2dir(c), (int64_t)len};
-        trench[1][i] = (Dig){rgb & 3, (int64_t)(rgb >> 4)};
+    int len, rgb;
+    for (int i = 0; i < N && fscanf(f, " %c %2d (#%6x)", &c, &len, &rgb) == 3; ++i) {
+        trench[0][i] = (Dig){char2dir(c), len};
+        trench[1][i] = (Dig){rgb & 3, rgb >> 4};
     }
     fclose(f);
 
     #if EXAMPLE || defined(DEBUG)
-    for (int i = 0; i < DIG; ++i) {
-        printf("%c %lld | %c %6lld\n", dir2char[trench[0][i].dir], trench[0][i].len, dir2char[trench[1][i].dir], trench[1][i].len);
+    const char *dir2char = "RDLU";
+    for (int i = 0; i < N; ++i) {
+        const Dig* const t0 = &trench[0][i];
+        const Dig* const t1 = &trench[1][i];
+        printf("%c %d | %c %6d\n", dir2char[t0->dir], t0->len, dir2char[t1->dir], t1->len);
     }
     printf("\n");
     #endif
 
-    for (int i = 0; i < 2; ++i) {  // parts 1 & 2
-        int64_t x = 0, y = 0, a0 = 0, a1 = 0;  // pos, area
-        for (int j = 0; j < DIG; ++j) {
-            const Dig* t = &trench[i][j];
+    for (int p = 0; p < 2; ++p) {  // parts 1 & 2
+        int64_t x = 0, y = 0, a = 0, b = 0;  // position, area, border
+        for (int i = 0; i < N; ++i) {
+            const Dig* t = &trench[p][i];
             switch (t->dir) {
-                case RIGHT: x += t->len; a0 += t->len; break;
-                case DOWN : y += t->len; a0 += t->len; a1 += t->len * x; break;
-                case LEFT : x -= t->len; a0 += t->len; break;
-                case UP   : y -= t->len; a0 += t->len; a1 -= t->len * (x + 1); break;
+                case RIGHT: x += t->len; a -= y * t->len; break;  // pos. oriented, but y=-y
+                case DOWN : y += t->len; break;
+                case LEFT : x -= t->len; a += y * t->len; break;
+                case UP   : y -= t->len; break;
             }
+            b += t->len;
         }
-        printf("Part %d: %lld + %lld = %lld\n", i + 1, a0, a1, a0 + a1);  // example: (62,952408144115), input: (46334,?)
+        // example: 62 952408144115, input: 46334 102000662718092
+        printf("Part %d: %lld\n", p + 1, (a > 0 ? a : -a) + b/2 + 1);
     }
+    printf("Time: %.0f us\n", stoptimer_us());
     return 0;
 }
