fdbce965268dbcd0e50bad0ff1eccca109a8203f
E. Dronkert <ednl@users.noreply.github.com>
2023-12-22 21:07:22 +0100

    drop works

--- 2023/22.c
+++ 2023/22.c
@@ -6,12 +6,15 @@
 #if EXAMPLE
 #define NAME "../aocinput/2023-22-example.txt"
 #define N 7
+#define M 3  // x/y-range=[0..2]
 #else
 #define NAME "../aocinput/2023-22-input.txt"
 #define N 1250
+#define M 10  // x/yy-range=[0..9]
 #endif
 
 static uint16_t brick[N][6];
+static uint16_t level[M][M];
 
 static int z_asc(const void* p, const void* q)
 {
@@ -44,6 +47,25 @@ int main(void)
     qsort(brick, N, sizeof *brick, z_asc);
     for (int i = 0; i < N; ++i)
         printf("%u,%u,%u~%u,%u,%u\n", brick[i][0], brick[i][1], brick[i][2], brick[i][3], brick[i][4], brick[i][5]);
+    printf("\n");
+
+    for (int i = 0; i < N; ++i) {
+        uint16_t top = 0;
+        for (uint16_t x = brick[i][0]; x <= brick[i][3]; ++x)
+            for (uint16_t y = brick[i][1]; y <= brick[i][4]; ++y)
+                if (level[x][y] > top)
+                    top = level[x][y];
+        uint16_t drop = brick[i][2] - top - 1;
+        brick[i][2] -= drop;
+        brick[i][5] -= drop;
+        for (uint16_t x = brick[i][0]; x <= brick[i][3]; ++x)
+            for (uint16_t y = brick[i][1]; y <= brick[i][4]; ++y)
+                level[x][y] = brick[i][5];
+    }
+
+    for (int i = 0; i < N; ++i)
+        printf("%u,%u,%u~%u,%u,%u\n", brick[i][0], brick[i][1], brick[i][2], brick[i][3], brick[i][4], brick[i][5]);
+    printf("\n");
 
     return 0;
 }
