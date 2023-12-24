5a5bd6abfec73977e0469ec0689d75b57cf585d9
E. Dronkert <ednl@users.noreply.github.com>
2023-12-24 22:40:36 +0100

    24 part 1

--- 2023/24.c
+++ 2023/24.c
@@ -5,120 +5,52 @@
  * By: E. Dronkert https://github.com/ednl
  */
 
-#include <stdio.h>    // fopen, fclose, fscanf, printf
-#include <stdlib.h>   // lldiv_t
-// #include <math.h>     // sqrt
-#include <stdint.h>   // int64_t
-#include <stdbool.h>  // bool
+#include <stdio.h>   // fopen, fclose, fscanf, printf
+#include <stdint.h>  // int64_t, INT64_C
+#include <math.h>    // fabs
+#include <stdbool.h>
 
-#define EXAMPLE 0
+#define EXAMPLE 1
 #if EXAMPLE
 #define NAME "../aocinput/2023-24-example.txt"
-#define N    5
-#define MIN  7
-#define MAX 27
+#define N   5
+#define MIN INT64_C(7)
+#define MAX INT64_C(27)
 #else
 #define NAME "../aocinput/2023-24-input.txt"
 #define N   300
-#define MIN 200000000000000LL
-#define MAX 400000000000000LL
+#define MIN INT64_C(200000000000000)
+#define MAX INT64_C(400000000000000)
 #endif
 
 typedef struct vec {
     int64_t x, y, z;
 } Vec;
 
-// typedef struct vecf {
-//     double x, y, z;
-// } Vecf;
-
 static Vec p[N], v[N];
 
-// static bool iszero2(const Vec a)
-// {
-//     return a.x == 0 && a.y == 0;
-// }
+static bool iszero(const Vec a)
+{
+    return a.x == 0 && a.y == 0 && a.z == 0;
+}
 
-// static bool iszero3(const Vec a)
-// {
-//     return a.x == 0 && a.y == 0 && a.z == 0;
-// }
+static Vec sub(const Vec a, const Vec b)
+{
+    return (Vec){a.x - b.x, a.y - b.y, a.z - b.z};
+}
 
-// static bool isequal2(const Vec a, const Vec b)
-// {
-//     return a.x == b.x && a.y == b.y;
-// }
+static int64_t dot(const Vec a, const Vec b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
 
-// static bool isequal3(const Vec a, const Vec b)
-// {
-//     return a.x == b.x && a.y == b.y && a.z == b.z;
-// }
-
-// static int64_t dot2(const Vec a, const Vec b)
-// {
-//     return a.x * b.x + a.y * b.y;
-// }
-
-// static int64_t dot3(const Vec a, const Vec b)
-// {
-//     return a.x * b.x + a.y * b.y + a.z * b.z;
-// }
-
-// static double len2(const Vec a)
-// {
-//     return sqrt(dot2(a, a));
-// }
-
-// static double len3(const Vec a)
-// {
-//     return sqrt(dot3(a, a));
-// }
-
-// static int64_t cross2(const Vec a, const Vec b)
-// {
-//     return a.x * b.y - a.y * b.x;
-// }
-
-static Vec cross3(const Vec a, const Vec b)
+static Vec cross(const Vec a, const Vec b)
 {
     return (Vec){
         a.y * b.z - a.z * b.y,
         a.z * b.x - a.x * b.z,
         a.x * b.y - a.y * b.x};
 }
-
-// static bool isparallel2(const Vec a, const Vec b)
-// {
-//     return cross2(a, b) == 0;
-// }
-
-// static bool isparallel3(const Vec a, const Vec b)
-// {
-//     return iszero3(cross3(a, b));
-// }
-
-// static Vec sub3(const Vec a, const Vec b)
-// {
-//     return (Vec){a.x - b.x, a.y - b.y, a.z - b.z};
-// }
-
-// t=0: p0=(x0,y0)
-// t=1: p1=(x1,y1) = p0+v = (x0+vx,y0+vy)
-// line: (y-y0)(x1-x0) = (y1-y0)(x-x0)
-//  => (y-y0)(x0+vx-x0) = (y0+vy-y0)(x-x0)
-// <=> (y-y0)vx = vy(x-x0)
-// <=> vy.x - vy.x0 - vx.y + vx.y0 = 0
-// <=> vy.x - vx.y + (vx.y0 - vy.x0) = 0
-//  => U = (vy,-vx,vx.y0 - vy.x0)
-static Vec homogenous2(const Vec pos, const Vec vel)
-{
-    return (Vec){vel.y, -vel.x, vel.x * pos.y - vel.y * pos.x};
-}
-
-// static Vecf divf(const Vec a, const int64_t d)
-// {
-//     return (Vecf){(double)a.x / d, (double)a.y / d, (double)a.z / d};
-// }
 
 int main(void)
 {
@@ -128,40 +60,49 @@ int main(void)
         fscanf(f, "%lld, %lld, %lld @ %lld, %lld, %lld", &p[i].x, &p[i].y, &p[i].z, &v[i].x, &v[i].y, &v[i].z);
     fclose(f);
 
-    // for (int i = 0; i < N; ++i)
-    //     printf("%lld, %lld, %lld @ %lld, %lld, %lld\n", p[i].x, p[i].y, p[i].z, v[i].x, v[i].y, v[i].z);
-    // printf("\n");
+    #if EXAMPLE || defined(DEBUG)
+    for (int i = 0; i < N; ++i)
+        printf("%lld, %lld, %lld @ %lld, %lld, %lld\n", p[i].x, p[i].y, p[i].z, v[i].x, v[i].y, v[i].z);
+    printf("\n");
+    #endif
 
     int part1 = 0;
     for (int i = 0; i < N - 1; ++i) {
-        const Vec hi = homogenous2(p[i], v[i]);
-        // printf("%lld, %lld, %lld @ %lld, %lld, %lld = (%lld,%lld,%lld)\n", p[i].x, p[i].y, p[i].z, v[i].x, v[i].y, v[i].z, hi.x, hi.y, hi.z);
+        const double ai = (double)v[i].y / v[i].x;
+        const double bi = p[i].y - ai * p[i].x;
         for (int j = i + 1; j < N; ++j) {
-            const Vec hj = homogenous2(p[j], v[j]);
-            // printf("\t%lld, %lld, %lld @ %lld, %lld, %lld = (%lld,%lld,%lld) -> ", p[j].x, p[j].y, p[j].z, v[j].x, v[j].y, v[j].z, hj.x, hj.y, hj.z);
-            const Vec hs = cross3(hi, hj);
-            if (hs.z != 0) {
-                lldiv_t sx = lldiv(hs.x, hs.z);
-                lldiv_t sy = lldiv(hs.y, hs.z);
-                if (sx.quot >= MIN && (sx.quot < MAX || (sx.quot == MAX && sx.rem == 0)) &&
-                    sy.quot >= MIN && (sy.quot < MAX || (sy.quot == MAX && sy.rem == 0))) {
-                    if (((sx.quot >= p[i].x && v[i].x >= 0) || (sx.quot <= p[i].x && v[i].x <= 0)) &&
-                        ((sy.quot >= p[i].y && v[i].y >= 0) || (sy.quot <= p[i].y && v[i].y <= 0)) &&
-                        ((sx.quot >= p[j].x && v[j].x >= 0) || (sx.quot <= p[j].x && v[j].x <= 0)) &&
-                        ((sy.quot >= p[j].y && v[j].y >= 0) || (sy.quot <= p[j].y && v[j].y <= 0))) {
+            const double aj = (double)v[j].y / v[j].x;
+            const double bj = p[j].y - aj * p[j].x;
+            if (fabs(ai - aj) > 0) {
+                const double x = (bj - bi) / (ai - aj);
+                const double y = ai * x + bi;
+                if (x >= MIN && x <= MAX &&
+                    y >= MIN && y <= MAX) {
+                    const double ti = (x - p[i].x) / v[i].x;
+                    const double tj = (x - p[j].x) / v[j].x;
+                    if (ti > 0 && tj > 0)
                         ++part1;
-                    } else {
-                        printf("(%d,%d) in the past", i, j);
-                    }
-                } else {
-                    // printf("outside");
                 }
-            } else {
-                // printf("parallel");
             }
-            // printf("\n");
         }
     }
-    printf("Part 1: %d\n", part1);  // 17907 too low, 21187 too high
+    printf("Part 1: %d\n", part1);  // example: 2, input: 20434
+
+    int i = 0;
+    printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", i, p[i].x, p[i].y, p[i].z, v[i].x, v[i].y, v[i].z);
+    int j = i + 1;
+    while (iszero(cross(v[i], v[j])))
+        ++j;
+    printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", j, p[j].x, p[j].y, p[j].z, v[j].x, v[j].y, v[j].z);
+    int k = j + 1;
+    while (iszero(cross(v[i], v[k])) || iszero(cross(v[j], v[k])))
+        ++k;
+    printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", k, p[k].x, p[k].y, p[k].z, v[k].x, v[k].y, v[k].z);
+    // Now 3 linearly independent lines: p,v[i] p,v[j] p,v[k]
+    // Find rock position r and speed s such that:
+    //   r = p[i] + (v[i] - s).t1
+    //   r = p[j] + (v[j] - s).t2
+    //   r = p[k] + (v[k] - s).t3
+
     return 0;
 }
