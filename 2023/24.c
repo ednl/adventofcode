54dd4097dacdaa5eeef91a50a3fe8ad6917e4d52
E. Dronkert <ednl@users.noreply.github.com>
2023-12-25 01:33:49 +0100

    overflow in part 2

--- 2023/24.c
+++ 2023/24.c
@@ -10,7 +10,7 @@
 #include <math.h>    // fabs
 #include <stdbool.h>
 
-#define EXAMPLE 1
+#define EXAMPLE 0
 #if EXAMPLE
 #define NAME "../aocinput/2023-24-example.txt"
 #define N   5
@@ -26,6 +26,11 @@
 typedef struct vec {
     int64_t x, y, z;
 } Vec;
+
+typedef struct plane {
+    Vec v;
+    int64_t n;
+} Plane;
 
 static Vec p[N], v[N];
 
@@ -50,6 +55,12 @@ static Vec cross(const Vec a, const Vec b)
         a.y * b.z - a.z * b.y,
         a.z * b.x - a.x * b.z,
         a.x * b.y - a.y * b.x};
+}
+
+static Plane plane(const int i, const int j)
+{
+    const Vec dp = sub(p[i], p[j]);
+    return (Plane){cross(dp, sub(v[i], v[j])), dot(dp, cross(v[i], v[j]))};
 }
 
 int main(void)
@@ -89,21 +100,56 @@ int main(void)
     printf("Part 1: %d\n", part1);  // example: 2, input: 20434
 
     int i = 0;
-    printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", i, p[i].x, p[i].y, p[i].z, v[i].x, v[i].y, v[i].z);
+    // printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", i, p[i].x, p[i].y, p[i].z, v[i].x, v[i].y, v[i].z);
     int j = i + 1;
     while (iszero(cross(v[i], v[j])))
         ++j;
-    printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", j, p[j].x, p[j].y, p[j].z, v[j].x, v[j].y, v[j].z);
+    // printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", j, p[j].x, p[j].y, p[j].z, v[j].x, v[j].y, v[j].z);
     int k = j + 1;
     while (iszero(cross(v[i], v[k])) || iszero(cross(v[j], v[k])))
         ++k;
-    printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", k, p[k].x, p[k].y, p[k].z, v[k].x, v[k].y, v[k].z);
+    // printf("%d: p=(%lld,%lld,%lld) v=(%lld,%lld,%lld)\n", k, p[k].x, p[k].y, p[k].z, v[k].x, v[k].y, v[k].z);
     // Now 3 linearly independent lines: p,v[i] p,v[j] p,v[k]
     // Find rock position r and speed s such that:
     //   r = p[i] + (v[i] - s).t1
     //   r = p[j] + (v[j] - s).t2
     //   r = p[k] + (v[k] - s).t3
-    // Intersect lines i and j: det|(v[i]-s) (p[i]-(p[j]+v[j]-s)) (v[j]-s)| = 0
+
+    Plane A = plane(i, j);
+    Plane B = plane(i, k);
+    Plane C = plane(j, k);
+    // printf("a=(%lld,%lld,%lld)\n", A.v.x, A.v.y, A.v.z);
+    // printf("b=(%lld,%lld,%lld)\n", B.v.x, B.v.y, B.v.z);
+    // printf("c=(%lld,%lld,%lld)\n", C.v.x, C.v.y, C.v.z);
+    const Vec bc = cross(B.v, C.v);
+    const Vec ca = cross(C.v, A.v);
+    const Vec ab = cross(A.v, B.v);
+    // printf("bc=(%lld,%lld,%lld)\n", bc.x, bc.y, bc.z);
+    // printf("ca=(%lld,%lld,%lld)\n", ca.x, ca.y, ca.z);
+    // printf("ab=(%lld,%lld,%lld)\n", ab.x, ab.y, ab.z);
+    const double sp = (double)A.n / dot(A.v, bc);
+    const double sq = (double)B.n / dot(B.v, ca);
+    const double sr = (double)C.n / dot(C.v, ab);
+    const Vec s = (Vec){
+        (int64_t)(round(sp * bc.x + sq * ca.x + sr * ab.x)),
+        (int64_t)(round(sp * bc.y + sq * ca.y + sr * ab.y)),
+        (int64_t)(round(sp * bc.z + sq * ca.z + sr * ab.z))};
+    printf("%lld %lld %lld\n", s.x, s.y, s.z);
+    v[i] = sub(v[i], s);
+    v[j] = sub(v[j], s);
+    const Vec vv = cross(v[i], v[j]);
+    const int64_t rp = dot(vv, cross(p[j], v[j]));
+    const int64_t rq = dot(vv, cross(p[i], v[i]));
+    const int64_t rr = dot(p[i], vv);
+    const int64_t vv2 = dot(vv, vv);
+    const Vec r = (Vec){
+        rp * v[i].x - rq * v[j].x + rr * vv.x,
+        rp * v[i].y - rq * v[j].y + rr * vv.y,
+        rp * v[i].z - rq * v[j].z + rr * vv.z};
+    printf("%lld %lld %lld\n", r.x, r.y, r.z);
+    printf("S=%lld\n", vv2);
+    printf("%lld %lld %lld\n", r.x / vv2, r.y / vv2, r.z / vv2);
+    printf("%lld %lld %lld\n", r.x % vv2, r.y % vv2, r.z % vv2);
 
     return 0;
 }
