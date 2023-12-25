760c7561aaba812bdc911c104f7d0bb795ec53a8
E. Dronkert <ednl@users.noreply.github.com>
2023-12-25 21:03:22 +0100

    alt timing

--- 2023/04alt.c
+++ 2023/04alt.c
@@ -10,10 +10,10 @@
  * Get minimum runtime:
  *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Apple M1 Mac Mini 2020 (3.2 GHz)               :  ? us
- *     Apple iMac 2013 (Core i5 Haswell 4570 3.2 GHz) : 59 us
- *     Raspberry Pi 5 (2.4 GHz)                       :  ? us
- *     Raspberry Pi 4 (1.8 GHz)                       :  ? us
+ *     Apple M1 Mac Mini 2020 (3.2 GHz)               :  24 µs
+ *     Raspberry Pi 5 (2.4 GHz)                       :  52 µs
+ *     Apple iMac 2013 (Core i5 Haswell 4570 3.2 GHz) :  59 µs
+ *     Raspberry Pi 4 (1.8 GHz)                       : 124 µs
  */
 
 #include <stdio.h>   // fopen, fclose, fgets, printf
