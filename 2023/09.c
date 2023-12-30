a4c1541f84996ecb130672880b60ec0f3cf5e77b
E. Dronkert <ednl@users.noreply.github.com>
2023-12-30 21:03:47 +0100

    day 9 shorter & faster

--- 2023/09.c
+++ 2023/09.c
@@ -10,10 +10,10 @@
  * Get minimum runtime:
  *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Apple M1 Mac Mini 2020 (3.2 GHz)               :  313 µs
- *     Raspberry Pi 5 (2.4 GHz)                       :  633 µs
- *     Apple iMac 2013 (Core i5 Haswell 4570 3.2 GHz) :  637 µs
- *     Raspberry Pi 4 (1.8 GHz)                       : 1788 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)          : ? µs
+ *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 523 µs
+ *     Raspberry Pi 5 (2.4 GHz)            : ? µs
+ *     Raspberry Pi 4 (1.8 GHz)            : ? µs
  */
 
 #include <stdio.h>   // fopen, fclose, fscanf, printf
@@ -23,31 +23,27 @@
 #define N 200  // sensors (lines in input file)
 #define M  21  // measurements (numbers per line)
 
-static int data[N][M][M];  // second dimension is for difference table
+static int table[M][M];  // difference table
 
 int main(void)
 {
     starttimer();
     FILE* f = fopen(NAME, "r");
     if (!f) { fputs("File not found.\n", stderr); return 1; }
-
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < M; ++j)
-            fscanf(f, "%d", &data[i][0][j]);  // measurements in row 0 of difference table
-    fclose(f);
-
     int part1 = 0, part2 = 0;
-    for (int i = 0; i < N; ++i) {            // sensors
-        part1 += data[i][0][M - 1];          // last measurement
-        part2 += data[i][0][0];              // first measurement
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j)
+            fscanf(f, "%d", &table[0][j]);   // measurements in row 0 of difference table
+        part1 += table[0][M - 1];            // last measurement
+        part2 += table[0][0];                // first measurement
         for (int k = 1, s = -1; k < M; ++k, s = -s) {  // difference table except row 0
             for (int j = 0; j < M - k; ++j)  // differences, 1 fewer every row
-                data[i][k][j] = data[i][k - 1][j + 1] - data[i][k - 1][j];
-            part1 += data[i][k][M - k - 1];  // last difference
-            part2 += data[i][k][0] * s;      // first difference, with alternating sign
+                table[k][j] = table[k - 1][j + 1] - table[k - 1][j];
+            part1 += table[k][M - k - 1];    // last difference
+            part2 += table[k][0] * s;        // first difference, with alternating sign
         }
     }
-
+    fclose(f);
     printf("%d %d\n", part1, part2);  // 1861775706, 1082
     printf("Time: %.0f us\n", stoptimer_us());
     return 0;
