9929f90f839f3eb06f1fabaf89d14b2f43934f0f
E. Dronkert <ednl@users.noreply.github.com>
2023-12-30 05:36:50 +0100

    timing

--- 2023/06.c
+++ 2023/06.c
@@ -5,15 +5,15 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    clang -std=gnu17 -Ofast -march=native -Wall -Wextra 11.c ../startstoptimer.c
- *    gcc   -std=gnu17 -Ofast -march=native -Wall -Wextra 11.c ../startstoptimer.c
+ *    clang -std=gnu17 -Ofast -march=native -Wall -Wextra 06.c ../startstoptimer.c
+ *    gcc   -std=gnu17 -Ofast -march=native -Wall -Wextra 06.c ../startstoptimer.c
  * Get minimum runtime:
  *     m=50000;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Apple M1 Mac Mini 2020 (3.2 GHz)               : ? µs
- *     Apple iMac 2013 (Core i5 Haswell 4570 3.2 GHz) : 39 µs
- *     Raspberry Pi 5 (2.4 GHz)                       : ? µs
- *     Raspberry Pi 4 (1.8 GHz)                       : ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)               : ? µs
+ *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz) : 39 µs
+ *     Raspberry Pi 5 (2.4 GHz)                 : ? µs
+ *     Raspberry Pi 4 (1.8 GHz)                 : ? µs
  */
 
 #include <stdio.h>     // fopen, fclose, fscanf, printf
