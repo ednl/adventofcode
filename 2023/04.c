5f8417b78e171ce1568f6a779ff88b202b38831a
E. Dronkert <ednl@users.noreply.github.com>
2023-12-30 21:14:24 +0100

    repo clean up

--- 2023/04.c
+++ 2023/04.c
@@ -10,33 +10,26 @@
  * Get minimum runtime:
  *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Apple M1 Mac Mini 2020 (3.2 GHz)               : 209 µs
- *     Apple iMac 2013 (Core i5 Haswell 4570 3.2 GHz) : 322 µs
- *     Raspberry Pi 5 (2.4 GHz)                       : 351 µs
- *     Raspberry Pi 4 (1.8 GHz)                       : 759 µs
+ *     Apple M1 Mac Mini 2020 (3.2 GHz)               :  24 µs
+ *     Raspberry Pi 5 (2.4 GHz)                       :  52 µs
+ *     Apple iMac 2013 (Core i5 Haswell 4570 3.2 GHz) :  59 µs
+ *     Raspberry Pi 4 (1.8 GHz)                       : 124 µs
  */
 
-#include <stdio.h>   // fopen, fclose, fgets, printf
-#include <stdlib.h>  // qsort
+#include <stdio.h>    // fopen, fclose, fgets, printf
+#include <string.h>   // memset
+#include <stdbool.h>  // bool
 #include "../startstoptimer.h"
 
 #define NAME "../aocinput/2023-04-input.txt"
-#define CARDS 202         // number of lines (cards) in input
-#define NWIN   10         // winning numbers on one card
-#define NHAVE  25         // numbers "you have" on one card
-#define N (NWIN + NHAVE)  // total amount of numbers on one card
+#define N 202  // lines (cards) in input
 
-static int copies[CARDS];
-static int numbers[N];
+static bool win[100];  // numbers in range [0..99]
+static int copies[N];
 
-// Sort int array ascending
-static int asc(const void* p, const void* q)
+static int min(const int a, const int b)
 {
-    const int a = *(const int*)p;
-    const int b = *(const int*)q;
-    if (a < b) return -1;
-    if (a > b) return  1;
-    return 0;
+    return a < b ? a : b;
 }
 
 // Convert 1 or 2 digits to number (' ' & 15 = 0, so leading space is fine)
@@ -51,31 +44,25 @@ int main(void)
     FILE* f = fopen(NAME, "r");
     if (!f) { fputs("File not found.\n", stderr); return 1; }
 
-    int part1 = 0, part2 = 0, card = 0;  // card = zero-based index
+    int part1 = 0, part2 = 0;
     char buf[128];  // every line is 116 chars + '\n\0'
-    while (card < CARDS && fgets(buf, sizeof buf, f)) {  // read one line at a time
-        char* s = buf + 10;  // skip to first winning number
-        for (int i = 0; i < NWIN; ++i, s += 3)  // read winning numbers
-            numbers[i] = readnum(s);
-        s += 2;  // skip divider
-        for (int i = NWIN; i < N; ++i, s += 3)  // read numbers "you have"
-            numbers[i] = readnum(s);
+    for (int card = 0; fgets(buf, sizeof buf, f); ) {  // read one line at a time
+        memset(win, 0, sizeof win);       // reset winning number scoring
+        char* s = buf + 10;               // skip to first winning number
+        for (; *s != '|'; s += 3)         // until '|', skip to next winning number
+            win[readnum(s)] = true;       // mark as winning number
 
-        int match = 0;  // matching number count = intersect(win,have).length
-        qsort(numbers, N, sizeof *numbers, asc);
-        for (int i = 1; i < N; ++i)
-            if (numbers[i] == numbers[i - 1]) {
-                ++match;
-                ++i;  // can't have more than two consecutive matching numbers
-            }
+        int match = 0;                    // how many winning numbers I have on this card
+        for (s += 2; *s; s += 3)          // skip divider, until '\0', skip to next number I have
+            match += win[readnum(s)];     // match numbers I have with winning
 
         part1 += match ? (1 << (match - 1)) : 0;  // score for current card
-        part2 += ++copies[card];  // count original card as one more copy
+        const int add = ++copies[card];   // count original card as one more copy
+        part2 += add;
 
-        const int m = card + match + 1;  // 1 past maximum index of extra copies to add
-        for (int i = card + 1; i < m && i < CARDS; ++i)  // add extra copies
-            copies[i] += copies[card];
-        ++card;
+        const int lim = min(++card + match, N);  // 1 past maximum index of extra copies to add
+        for (int i = card; i < lim; ++i)  // add extra copies
+            copies[i] += add;
     }
     fclose(f);
     printf("%d %d\n", part1, part2);  // 24733 5422730
