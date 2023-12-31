3830a287cb997dba7c639bfaa219ecd867b63eb7
E. Dronkert <ednl@users.noreply.github.com>
2023-12-31 20:03:18 +0100

    much faster without strtok

--- 2023/02.c
+++ 2023/02.c
@@ -8,12 +8,12 @@
  *    clang -std=gnu17 -Ofast -march=native -Wall -Wextra 02.c ../startstoptimer.c
  *    gcc   -std=gnu17 -Ofast -march=native -Wall -Wextra 02.c ../startstoptimer.c
  * Get minimum runtime:
- *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Mac Mini 2020 (M1 3.2 GHz)          : ? µs
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 57 µs
- *     Raspberry Pi 5 (2.4 GHz)            : ? µs
- *     Raspberry Pi 4 (1.8 GHz)            : ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)          :  26 µs
+ *     Raspberry Pi 5 (2.4 GHz)            :  43 µs
+ *     iMac 2013 (i5 Haswell 4570 3.2 GHz) :  57 µs
+ *     Raspberry Pi 4 (1.8 GHz)            : 101 µs
  */
 
 #include <stdio.h>   // fopen, fclose, fgets, fputs, printf
