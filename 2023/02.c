38241672b6c8d0086498d3c3d663fd33bfe42b00
E. Dronkert <ednl@users.noreply.github.com>
2023-12-31 19:59:45 +0100

    much faster without strtok

--- 2023/02.c
+++ 2023/02.c
@@ -11,13 +11,12 @@
  *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
  *     Mac Mini 2020 (M1 3.2 GHz)          : ? µs
+ *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 57 µs
  *     Raspberry Pi 5 (2.4 GHz)            : ? µs
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 182 µs
  *     Raspberry Pi 4 (1.8 GHz)            : ? µs
  */
 
-#include <stdio.h>    // fopen, fclose, getline, sscanf, printf
-#include <string.h>   // strtok
+#include <stdio.h>   // fopen, fclose, fgets, fputs, printf
 #include "../startstoptimer.h"
 
 // Limits per colour as imposed by the puzzle
@@ -26,6 +25,14 @@ static const unsigned rgblim[3] = {12, 13, 14};
 static unsigned max(const unsigned a, const unsigned b)
 {
     return a > b ? a : b;
+}
+
+static unsigned readnum(const char** s)
+{
+    unsigned n = 0;
+    while (**s >= '0' && **s <= '9')
+        n = n * 10 + (*(*s)++ & 15);
+    return n;
 }
 
 int main(void)
@@ -38,21 +45,19 @@ int main(void)
     unsigned part1 = 0, part2 = 0, game = 0;
     while (fgets(buf, sizeof buf, f)) {
         ++game;  // game numbers are consecutive & identical to line number, so no parsing
-        char* token = buf;  // buf content will be destroyed but pointer is saved
-        while (*token++ != ':');  // goes one past ':'
-        token = strtok(token, ",;");  // subsets don't matter, so tokenise at all separators
         unsigned rgbmax[3] = {0};  // maximum number of cubes per colour per game
-        while (token) {
-            unsigned cubes;
-            char colour;
-            if (sscanf(token, "%u %c", &cubes, &colour) == 2)  // %d absorbs leading space
-                switch (colour) {
-                    case 'r': rgbmax[0] = max(rgbmax[0], cubes); break;
-                    case 'g': rgbmax[1] = max(rgbmax[1], cubes); break;
-                    case 'b': rgbmax[2] = max(rgbmax[2], cubes); break;
-                }
-            token = strtok(NULL, ",;");  // next pair of number+word
-        }
+        const char* s = buf;
+        while (*s++ != ':');
+        do {
+            ++s;  // skip space
+            unsigned cubes = readnum(&s);
+            switch (*++s) {  // skip space
+                case 'r': rgbmax[0] = max(rgbmax[0], cubes); break;
+                case 'g': rgbmax[1] = max(rgbmax[1], cubes); break;
+                case 'b': rgbmax[2] = max(rgbmax[2], cubes); break;
+            }
+            while (*s && *s != ' ') ++s;  // skip to next space or EOL
+        } while (*s);
         if (rgbmax[0] <= rgblim[0] && rgbmax[1] <= rgblim[1] && rgbmax[2] <= rgblim[2])
             part1 += game;
         part2 += rgbmax[0] * rgbmax[1] * rgbmax[2];
