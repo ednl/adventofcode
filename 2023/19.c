e4b5289a6e2dcdcfa39c6b3cbd888f7e9f2bfdaf
E. Dronkert <ednl@users.noreply.github.com>
2024-04-09 13:03:50 +0200

    intermediate save

--- 2023/19.c
+++ 2023/19.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 #define NAME "../aocinput/2023-19-example.txt"
@@ -14,26 +15,40 @@
 #define WFLEN 4
 #define PARTS 5
 
+// Part rating category
 typedef enum cat {
-    X, M, A, S, NONE
+    NOCAT=0, X, M, A, S
 } Cat;
 
-typedef struct cond {
+// Rule comparison
+typedef enum cmp {
+    NOCMP=0, LT, GT
+} Cmp;
+
+// Rule action
+typedef enum act {
+    REJECT=0, ACCEPT, GONEXT
+} Act;
+
+typedef struct rule {
     Cat cat;
-    int lower, upper, nextid;
+    Cmp cmp;
+    unsigned val;
+    Act act;
+    unsigned nextid;
     char nextname[4];
-} Cond;
+} Rule;
 
 typedef struct workflow {
-    Cond cond[WFLEN];
-    int id, len;
+    Rule rule[WFLEN];
+    unsigned id, len;
     char name[4];
 } Workflow;
 
 static const char* cat2char = "xmas_";
-static const int start = 'i' << 8 | 'n';
-static const int accepted = 'A';
-static const int rejected = 'R';
+static const unsigned start = 'i' << 8 | 'n';
+static const unsigned accepted = 'A';
+static const unsigned rejected = 'R';
 static Workflow wf[WFSZ];
 static unsigned part[PARTS][4];
 
@@ -45,7 +60,7 @@ static Cat char2cat(const char c)
         case 'a': return A;
         case 's': return S;
     }
-    return NONE;
+    return NOCAT;
 }
 
 static void show(void)
@@ -53,15 +68,15 @@ static void show(void)
     for (int i = 0 ; i < WFSZ; ++i) {
         printf("%d: %s{", i, wf[i].name);
         for (int j = 0; j < wf[i].len; ++j) {
-            Cat cat = wf[i].cond[j].cat;
-            if (cat != NONE) {  // conditional
+            Cat cat = wf[i].rule[j].cat;
+            if (cat != NOCAT) {  // conditional
                 fputc(cat2char[cat], stdout);
-                if (wf[i].cond[j].lower)
-                    printf(">%d:", wf[i].cond[j].lower);
-                if (wf[i].cond[j].upper)
-                    printf("<%d:", wf[i].cond[j].upper);
+                // if (wf[i].rule[j].lower)
+                //     printf(">%d:", wf[i].rule[j].lower);
+                // if (wf[i].rule[j].upper)
+                //     printf("<%d:", wf[i].rule[j].upper);
             }
-            printf("%s", wf[i].cond[j].nextname);
+            printf("%s", wf[i].rule[j].nextname);
             if (j != wf[i].len - 1)
                 printf(",");
         }
@@ -90,20 +105,20 @@ int main(void)
         for (int c = ','; c == ',' && j < WFLEN && fscanf(f, " %15[^,}]", buf) == 1; ++j, c = fgetc(f)) {
             if ((s = strchr(buf, ':'))) {
                 // conditional node
-                wf[i].cond[j].cat = char2cat(buf[0]);
-                int* val = buf[1] == '>' ? &wf[i].cond[j].lower : &wf[i].cond[j].upper;
-                s = &buf[2];
-                while (*s != ':')
-                    *val = *val * 10 + (*s++ & 15);
-                ++s;  // to start of condition name
+                wf[i].rule[j].cat = char2cat(buf[0]);
+                // int* val = buf[1] == '>' ? &wf[i].rule[j].lower : &wf[i].rule[j].upper;
+                // s = &buf[2];
+                // while (*s != ':')
+                //     *val = *val * 10 + (*s++ & 15);
+                // ++s;  // to start of condition name
             } else {
                 // unconditional node
-                wf[i].cond[j].cat = NONE;
+                wf[i].rule[j].cat = NOCAT;
                 s = buf;  // start of buf is start of condition name
             }
-            strcpy(wf[i].cond[j].nextname, s);
+            strcpy(wf[i].rule[j].nextname, s);
             while (*s)
-                wf[i].cond[j].nextid = wf[i].cond[j].nextid << 8 | *s++;
+                wf[i].rule[j].nextid = wf[i].rule[j].nextid << 8 | *s++;
         }
         wf[i].len = j;
     }
