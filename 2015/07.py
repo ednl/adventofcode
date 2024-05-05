477686ae4765e5f768ca128725fbcbf4e84a5e74
E. Dronkert <ednl@users.noreply.github.com>
2024-05-05 12:00:21 +0200

    comments

--- 2015/07.py
+++ 2015/07.py
@@ -9,31 +9,31 @@ fun = {
     'RSHIFT': lambda x, y: x >> y,
     'PATCH' : lambda x: x}
 
-h = {}
+h = {}  # h = connection diagram: for every output wire list its input wires and gate function
 with open('../aocinput/2015-07-input.txt') as fp:
     for line in fp:
         m = regrule.match(line)
         if m:
             f = fun[m.group(4) if m.group(4) is not None else 'PATCH']
             u = [int(x) if x.isnumeric() else x for x in m.group(2, 5) if x is not None]
-            y = m.group(6)
-            h[y] = {'f': f, 'u': u}
+            y = m.group(6)           # y = output wire
+            h[y] = {'f': f, 'u': u}  # u = input wires, connected via gate function f
 
 def getval(id):
-    if type(id) is int:
+    if type(id) is int:                  # direct value?
         return id
-    if id in val:
+    if id in val:                        # already calculated?
         return val[id]
-    inp = list(map(getval, h[id]['u']))
-    for u, v in zip(h[id]['u'], inp):
-        val[u] = v
-    return h[id]['f'](*inp)
+    inp = list(map(getval, h[id]['u']))  # evaluate input wires u
+    for u, v in zip(h[id]['u'], inp):    # pair input wires with values
+        val[u] = v                       # save input values
+    return h[id]['f'](*inp)              # return output value
 
 # Part 1
-val = {}
-a = getval('a')
-print(a)            # 46065
+val = {}         # cache
+a = getval('a')  # remember value for part 2
+print(a)         # 46065
 
 # Part 2
-val = {'b': a}
+val = {'b': a}      # init with value from part 1
 print(getval('a'))  # 14134
