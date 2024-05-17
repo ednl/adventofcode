7a0026526b37390e16d1633451a18570eab93853
E. Dronkert <ednl@users.noreply.github.com>
2024-05-17 15:22:22 +0200

    ok, it's fairer to include parsing while timing

--- 2022/21.c
+++ 2022/21.c
@@ -12,7 +12,7 @@
  * Minimum runtime:
  *     Mac Mini 2020 (M1 3.2 GHz)          :   ? µs
  *     Raspberry Pi 5 (2.4 GHz)            :   ? µs
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) :  99 µs
+ *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 608 µs
  *     Raspberry Pi 4 (1.8 GHz)            :   ? µs
  */
 
@@ -41,21 +41,6 @@ typedef struct monkey {
 
 static Monkey monkey[N];
 
-static int binsearch(const int32_t id)
-{
-    int l = 0, r = N - 1;
-    if (id == monkey[l].id) return l;
-    if (id == monkey[r].id) return r;
-    // Now always true: monkey[l].id < id < monkey[r].id
-    while (r - l > 1) {  // adjacent means not found
-        int m = ((l + 1) >> 1) + (r >> 1);  // avoid index overflow
-        if      (id > monkey[m].id) l = m;
-        else if (id < monkey[m].id) r = m;
-        else return m;
-    }
-    return -1;  // should not happen for this data (famous last words)
-}
-
 // Much faster than atoi() but only valid for this data because every number is followed by a newline.
 // Slightly more general but still dependent on ASCII: while ((*s & 0xf0) == 0x30) ...;
 // Best and still faster: while (*s >= '0' && *s <= '9') n = n*10 + (*s++ - '0');
@@ -74,39 +59,28 @@ static int32_t hash(const char *s)
     return *(const int32_t *)s;
 }
 
+// Look up monkey by id
+static int binsearch(const int32_t id)
+{
+    int l = 0, r = N - 1;
+    if (id == monkey[l].id) return l;
+    if (id == monkey[r].id) return r;
+    // Now always true: monkey[l].id < id < monkey[r].id
+    while (r - l > 1) {  // adjacent means not found
+        int m = ((l + 1) >> 1) + (r >> 1);  // avoid index overflow
+        if      (id > monkey[m].id) l = m;
+        else if (id < monkey[m].id) r = m;
+        else return m;
+    }
+    return -1;  // should not happen for this data (famous last words)
+}
+
+// Helper for qsort, sort monkies by id
 static int cmp_monkies(const void *p, const void *q)
 {
     if (((const Monkey *)p)->id < ((const Monkey *)q)->id) return -1;
     if (((const Monkey *)p)->id > ((const Monkey *)q)->id) return  1;
     return 0;
-}
-
-static void parse(void)
-{
-    FILE* f = fopen(NAME, "r");
-    if (!f) return;
-    char line[32];
-    Monkey *m = monkey;
-    for (int i = 0; i < N && fgets(line, sizeof line, f); ++i, ++m) {
-        m->id = hash(line);
-        const char *s = line + 6;
-        if (*s >= 'a') {
-            m->a = hash(s);
-            m->b = hash(s + 7);
-            m->op = *(s + 5);
-        } else {
-            m->val = readnum(s);
-            m->op = '=';
-        }
-    }
-    fclose(f);
-    qsort(monkey, sizeof monkey / sizeof *monkey, sizeof *monkey, cmp_monkies);
-    for (int i = 0; i < N; ++i) {
-        if (monkey[i].op != '=') {
-            monkey[i].a = binsearch(monkey[i].a);
-            monkey[i].b = binsearch(monkey[i].b);
-        }
-    }
 }
 
 static int64_t getval(const int index)
@@ -126,14 +100,42 @@ static int64_t getval(const int index)
 
 int main(void)
 {
-    parse();
     starttimer();
 
+    // Parse input
+    FILE* f = fopen(NAME, "r");
+    if (!f) return 1;
+    char line[32];
+    Monkey *m = monkey;
+    for (int i = 0; i < N && fgets(line, sizeof line, f); ++i, ++m) {
+        m->id = hash(line);
+        const char *s = line + 6;
+        if (*s >= 'a') {
+            m->a = hash(s);
+            m->b = hash(s + 7);
+            m->op = *(s + 5);
+        } else {
+            m->val = readnum(s);
+            m->op = '=';
+        }
+    }
+    fclose(f);
+
+    // Transform monkey array, use direct index instead of id
+    qsort(monkey, sizeof monkey / sizeof *monkey, sizeof *monkey, cmp_monkies);
+    for (int i = 0; i < N; ++i)
+        if (monkey[i].op != '=') {
+            monkey[i].a = binsearch(monkey[i].a);
+            monkey[i].b = binsearch(monkey[i].b);
+        }
+
+    // Part 1
     const char *rootname = "root";
     const int32_t rootid = hash(rootname);
     const int rootindex = binsearch(rootid);
     printf("Part 1: %lld\n", getval(rootindex));  // example: 152, input: 21120928600114
 
+    // Part 2
     monkey[rootindex].op = '-';
     const char *humnname = "humn";
     const int32_t humnid = hash(humnname);
