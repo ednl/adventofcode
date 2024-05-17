c5ba1f411d0738b5b9fbeb8513631368ea835343
E. Dronkert <ednl@users.noreply.github.com>
2024-05-17 20:10:38 +0200

    timing

--- 2022/21.c
+++ 2022/21.c
@@ -10,7 +10,7 @@
  * Get minimum runtime:
  *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Mac Mini 2020 (M1 3.2 GHz)          :  388 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)          :  381 µs
  *     iMac 2013 (i5 Haswell 4570 3.2 GHz) :  607 µs
  *     Raspberry Pi 5 (2.4 GHz)            :  715 µs
  *     Raspberry Pi 4 (1.8 GHz)            : 2533 µs
