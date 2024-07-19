1fedf139be6526b0425b1819877536f047023907
E. Dronkert <ednl@users.noreply.github.com>
2024-07-19 15:23:32 +0200

    C version

--- 2015/17.c
+++ 2015/17.c
@@ -10,10 +10,10 @@
  * Get minimum runtime:
  *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
- *     Mac Mini 2020 (M1 3.2 GHz)          :    ? ms
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 5.39 ms
- *     Raspberry Pi 5 (2.4 GHz)            :    ? ms
- *     Raspberry Pi 4 (1.8 GHz)            :    ? ms
+ *     Mac Mini 2020 (M1 3.2 GHz)          :     ? ms
+ *     iMac 2013 (i5 Haswell 4570 3.2 GHz) :  5.39 ms
+ *     Raspberry Pi 5 (2.4 GHz)            :  9.41 ms
+ *     Raspberry Pi 4 (1.8 GHz)            : 55.4  ms
  */
 
 #include <stdio.h>              // fopen, fclose, fscanf, printf
---
+++ 2020/05.c
@@ -0,0 +1,64 @@
+/**
+ * Advent of Code 2020
+ * Day 5: Binary Boarding
+ * https://adventofcode.com/2020/day/5
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *    clang -std=gnu17 -Ofast -march=native -Wall -Wextra 05.c ../startstoptimer.c
+ *    gcc   -std=gnu17 -Ofast -march=native -Wall -Wextra 05.c ../startstoptimer.c
+ * Get minimum runtime:
+ *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
+ * Minimum runtime:
+ *     Mac Mini 2020 (M1 3.2 GHz)          :   ? µs
+ *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 125 µs
+ *     Raspberry Pi 5 (2.4 GHz)            :   ? µs
+ *     Raspberry Pi 4 (1.8 GHz)            :   ? µs
+ */
+
+#include <stdio.h>              // fopen, fclose, fscanf, printf
+#include <stdlib.h>             // qsort
+#include "../startstoptimer.h"  // my own timing function
+
+#define N 1024
+static int seat[N];
+
+// Qsort helper function: sort ints ascending
+static int cmp_int_asc(const void *p, const void *q)
+{
+    const int a = *(int *)p;
+    const int b = *(int *)q;
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+int main(void)
+{
+    starttimer();
+    FILE *f = fopen("../aocinput/2020-05-input.txt", "r");
+    if (!f)
+        return 1;
+    int n = 0, maxid = 0;
+    char boardingpass[16];
+    while (n < N && fgets(boardingpass, sizeof boardingpass, f)) {
+        int id = 0;
+        for (int i = 0; i < 10; ++i)  // 10-bit binary number
+            id = id << 1 | (!(boardingpass[i] & 4));  // B or R is 1, F or L is 0
+        if (id > maxid)
+            maxid = id;
+        seat[n++] = id;
+    }
+    fclose(f);
+    printf("Part 1: %d\n", maxid);  // 861
+
+    qsort(seat, n, sizeof *seat, cmp_int_asc);
+    for (int i = 1; i < n; ++i)  // start at second seat
+        if (seat[i] - 1 != seat[i - 1]) {  // gap before this seat?
+            printf("Part 2: %d\n", seat[i] - 1);  // 633
+            break;
+        }
+
+    printf("Time: %.0f µs\n", stoptimer_us());
+    return 0;
+}
