619c53a317753342aff7454400859b255ea9cc1a
E. Dronkert <ednl@users.noreply.github.com>
2024-07-26 10:36:54 +0200

    reformat

--- 2019/22.c
+++ 2019/22.c
@@ -1,73 +1,61 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>    // atoi
+#include <stdint.h>    // int64_t, INT64_C
+#include <inttypes.h>  // PRId64
 
-const long long DECK = 119315717514047;
-const long long MOVE = 101741582076661;
-const long long START = 2020;
+#define LINES  100
+#define LINELEN 32
 
-long long index = START, count = 0;
-int key[100], val[100];
+#define DECK  INT64_C(119315717514047)
+#define TIMES INT64_C(101741582076661)
+#define START INT64_C(2020)
+
+typedef enum action {
+    SHIFTUP, SHIFTDN, REVERSE, DISTRIB
+} Action;
+
+typedef struct shuffle {
+    Action key;
+    int val;
+} Shuffle;
+
+static Shuffle shuffle[LINES];
 
 int main(void)
 {
-	// const long long DECK = 10007;
-	// const long long MOVE = 1;
-	// const long long START = 2019;
-	FILE *fp;
-	char *s = NULL;
-	size_t t = 0;
-	int i, n;
+    FILE *f = fopen("../aocinput/2019-22-input.txt", "r");
+    if (!f)
+        return 1;
 
-	for (i = 0; i < 100; ++i)
-	{
-		key[i] = 0;
-		val[i] = 0;
-	}
+    char s[LINELEN];
+    for (int i = 0, n; i < LINES && fgets(s, sizeof s, f); ++i)
+        switch (s[5]) {
+        case 'i':  // "deal into new stack"
+            shuffle[i] = (Shuffle){REVERSE, 0};
+            break;
+        case 'w':  // "deal with increment X"
+            shuffle[i] = (Shuffle){DISTRIB, atoi(&s[20])};
+            break;
+        default:  // "cut X" where s[5]='\n' or a digit
+			n = atoi(&s[4]);
+			shuffle[i] = n > 0 ? (Shuffle){SHIFTUP, n} : (Shuffle){SHIFTDN, -n};
+            break;
+        }
+    fclose(f);
 
-	if ((fp = fopen("../aocinput/2019-22-input.txt", "r")) != NULL)
-	{
-		i = 0;
-		while (i < 100 && getline(&s, &t, fp) > 0)
-		{
-			if (s[0] == 'c')
-			{
-				if ((n = atoi(&s[4])) > 0)
-				{
-					key[i] = 1;
-					val[i] = n;
-				} else if (n < 0)
-				{
-					key[i] = 2;
-					val[i] = -n;
-				}
-			} else if (s[5] == 'i')
-			{
-				key[i] = 3;
-				val[i] = 0;
-			} else if (s[5] == 'w')
-			{
-				key[i] = 4;
-				val[i] = atoi(&s[20]);
-			}
-			++i;
-		}
-		free(s);
-		fclose(fp);
-	}
-	do
-	{
-		for (i = 0; i < 100; ++i)
-		{
-			switch (key[i])
-			{
-				case 1: index -= val[i]; if (index < 0) index += DECK; break;
-				case 2: index += val[i]; if (index >= DECK) index -= DECK; break;
-				case 3: index = DECK - 1 - index; break;
-				case 4: index = index * val[i] % DECK; break;
-			}
-		}
-		++count;
-	} while (index != START && count < MOVE);
-	printf("%lld %lld\n", count, index);
-	return 0;
+    int64_t index = START, count = 0;
+    do {
+        for (int i = 0; i < LINES; ++i) {
+            switch (shuffle[i].key) {
+                case SHIFTUP: index -= shuffle[i].val; if (index < 0) index += DECK; break;
+                case SHIFTDN: index += shuffle[i].val; if (index >= DECK) index -= DECK; break;
+                case REVERSE: index = DECK - 1 - index; break;
+                case DISTRIB: index = index * shuffle[i].val % DECK; break;
+            }
+        }
+        ++count;
+    } while (index != START && count < TIMES);
+
+    printf("%"PRId64" %"PRId64"\n", count, index);
+    return 0;
 }
--- 2019/22.py
+++ 2019/22.py
@@ -1,16 +1,20 @@
-m = 119315717514047
-n = 101741582076661
-pos = 2020
-shuffles = { 'deal with increment ': lambda x,m,a,b: (a*x %m, b*x %m),
-        'deal into new stack': lambda _,m,a,b: (-a %m, (m-1-b)%m),
-        'cut ': lambda x,m,a,b: (a, (b-x)%m) }
+deck  = 119315717514047
+times = 101741582076661
+pos   = 2020
+
+shuffle = {
+	'deal with increment ': lambda x,m,a,b: ( a * x % m,  b * x  % m),
+	'deal into new stack' : lambda _,m,a,b: (-a     % m, (m-1-b) % m),
+	'cut '                : lambda x,m,a,b: ( a        , (b-x)   % m)}
+
 a,b = 1,0
-with open('22/22.txt') as f:
-	for s in f.read().strip().split('\n'):
-		for name,fn in shuffles.items():
-			if s.startswith(name):
-				arg = int(s[len(name):]) if name[-1] == ' ' else 0
-				a,b = fn(arg, m, a, b)
+with open('../aocinput/2019-22-input.txt') as f:
+	for line in f.read().strip().split('\n'):
+		for action, func in shuffle.items():
+			if line.startswith(action):
+				arg = int(line[len(action):]) if action[-1] == ' ' else 0
+				a,b = func(arg, deck, a, b)
 				break
-r = (b * pow(1-a, m-2, m)) % m
-print(f"Card at #{pos}: {((pos - r) * pow(a, n*(m-2), m) + r) % m}")
+
+r = (b * pow(1-a, deck-2, deck)) % deck
+print(f"Card at #{pos}: {((pos - r) * pow(a, times * (deck-2), deck) + r) % deck}")
