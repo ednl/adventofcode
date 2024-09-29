7930c73c23e10c2d0888171c9c566b23e916a1fe
E. Dronkert <ednl@users.noreply.github.com>
2024-09-29 13:16:21 +0200

    formatting

--- 2023/05.c
+++ 2023/05.c
@@ -5,8 +5,8 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    clang -std=gnu17 -Ofast -march=native -Wall -Wextra 05.c ../startstoptimer.c
- *    gcc   -std=gnu17 -Ofast -march=native -Wall -Wextra 05.c ../startstoptimer.c
+ *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 05.c ../startstoptimer.c
+ *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 05.c ../startstoptimer.c
  * Get minimum runtime:
  *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
  * Minimum runtime:
@@ -39,9 +39,8 @@
 #define SSIZE 16  // max for my input = 11
 
 // Data structures
-typedef int64_t i64;
 typedef struct Conv {  // a conversion is an offset for range [lo..hi)
-    i64 lo, hi, ofs;
+    int64_t lo, hi, ofs;
 } Conv;
 typedef struct Map {  // a map is a list of conversions
     Conv conv[CONVS];
@@ -49,30 +48,30 @@ typedef struct Map {  // a map is a list of conversions
 } Map;
 typedef struct range {  // seed range to be converted from mapindex onwards
     size_t mapindex;
-    i64 lo, hi;
+    int64_t lo, hi;
 } Range;
 
 // Global data
-static i64 seed[SEEDS];
+static int64_t seed[SEEDS];
 static Map map[MAPS];
 static Range stack[SSIZE];
 static size_t ssize;
 
-static i64 min(const i64 a, const i64 b)
+static int64_t min(const int64_t a, const int64_t b)
 {
     return a < b ? a : b;
 }
 
-static i64 max(const i64 a, const i64 b)
+static int64_t max(const int64_t a, const int64_t b)
 {
     return a > b ? a : b;
 }
 
 // Qsort helper: sort by Conv::lo ascending
-static int lo_asc(const void* p, const void* q)
+static int lo_asc(const void *p, const void *q)
 {
-    const i64 a = ((const Conv*)p)->lo;
-    const i64 b = ((const Conv*)q)->lo;
+    const int64_t a = ((const Conv *)p)->lo;
+    const int64_t b = ((const Conv *)q)->lo;
     if (a < b) return -1;
     if (a > b) return  1;
     return 0;
@@ -80,12 +79,12 @@ static int lo_asc(const void* p, const void* q)
 
 // Convert a single seed number n to a location number
 // by traversing all maps in order.
-static i64 convert1(i64 n)
+static int64_t convert1(int64_t n)
 {
-    const Map* m = map;
+    const Map *m = map;
     for (int i = 0; i < MAPS; ++i, ++m) {
-        const Conv* l = &m->conv[0];
-        const Conv* r = &m->conv[m->len - 1];
+        const Conv *l = &m->conv[0];
+        const Conv *r = &m->conv[m->len - 1];
         while (l <= r) {
             if (n <  l->lo) break;  // outside any range = no conversion
             if (n >= r->hi) break;
@@ -105,25 +104,25 @@ static bool push(const Range r)
     return true;
 }
 
-static bool pop(Range* const r)
+static bool pop(Range *const r)
 {
     if (!ssize) return false;
     *r = stack[--ssize];
     return true;
 }
 
-static i64 convert2(Range range)
+static int64_t convert2(Range range)
 {
-    i64 minloc = INT64_MAX;
+    int64_t minloc = INT64_MAX;
     push(range);
     while (pop(&range)) {
         if (range.mapindex == MAPS) {
             minloc = min(minloc, range.lo);
             continue;
         }
-        const Map* m = &map[range.mapindex];
-        const Conv* l = &m->conv[0];
-        const Conv* r = &m->conv[m->len - 1];
+        const Map *m = &map[range.mapindex];
+        const Conv *l = &m->conv[0];
+        const Conv *r = &m->conv[m->len - 1];
         while (l <= r) {
             if (range.lo < l->lo) {
                 push((Range){range.mapindex + 1, range.lo, min(range.hi, l->lo)});  // no conversion left of l range
@@ -151,7 +150,7 @@ static i64 convert2(Range range)
 int main(void)
 {
     starttimer();
-    FILE* f = fopen(NAME, "r");
+    FILE *f = fopen(NAME, "r");
     if (!f) { fputs("File not found.\n", stderr); return 1; }
 
     while (fgetc(f) != ':');  // skip to first seed number
@@ -159,7 +158,7 @@ int main(void)
         fscanf(f, "%"PRId64, &seed[i]);
     while (fgetc(f) != ':');  // skip to first conversion of first map
     for (int i = 0; i < MAPS; ++i) {
-        i64 dst, src, len;
+        int64_t dst, src, len;
         while (map[i].len < CONVS && fscanf(f, "%"PRId64" %"PRId64" %"PRId64, &dst, &src, &len) == 3)
             map[i].conv[map[i].len++] = (Conv){src, src + len, dst - src};
         qsort(map[i].conv, map[i].len, sizeof (Conv), lo_asc);  // sort to enable l-r search
@@ -167,12 +166,12 @@ int main(void)
     }
     fclose(f);
 
-    i64 part1 = INT64_MAX;
+    int64_t part1 = INT64_MAX;
     for (int i = 0; i < SEEDS; ++i)
         part1 = min(part1, convert1(seed[i]));
     printf("Part 1: %"PRId64"\n", part1);  // example: 35, input: 836040384
 
-    i64 part2 = INT64_MAX;
+    int64_t part2 = INT64_MAX;
     for (int i = 0; i < SEEDS; i += 2)
         part2 = min(part2, convert2((Range){0, seed[i], seed[i] + seed[i + 1]}));
     printf("Part 2: %"PRId64"\n", part2);  // example: 46, input: 10834440
