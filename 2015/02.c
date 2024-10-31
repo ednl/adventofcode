910aa1a2e5cc8741474c03a60a2fccd0556cc381
E. Dronkert <ednl@users.noreply.github.com>
2024-10-31 12:31:15 +0100

    faster C version

--- 2015/02.c
+++ 2015/02.c
@@ -1,8 +1,21 @@
+/**
+ * Advent of Code 2015
+ * Day 2: I Was Told There Would Be No Math
+ * https://adventofcode.com/2015/day/2
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 02.c
+ *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 02.c
+ */
+
 #include <stdio.h>
 
-static unsigned min(const unsigned a, const unsigned b)
+static inline void swap(unsigned *restrict const a, unsigned *restrict const b)
 {
-    return a < b ? a : b;
+    const unsigned tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 int main(void)
@@ -11,13 +24,15 @@ int main(void)
     if (!f) return 1;
     unsigned l, w, h, paper = 0, ribbon = 0;
     while (fscanf(f, "%ux%ux%u", &l, &w, &h) == 3) {
-        const unsigned a = l * w, b = l * h, c = w * h;
-        paper += ((a + b + c) << 1) + min(min(a, b), c);
-        const unsigned p = l + w, q = l + h, r = w + h;
-        ribbon += (min(min(p, q), r) << 1) + l * w * h;
+        // Bubble sort l,w,h ascending
+        if (l > w) swap(&l, &w);
+        if (l > h) swap(&l, &h);
+        if (w > h) swap(&w, &h);
+        const unsigned A = l * w, B = (l + w) << 1;
+        paper  += A * 3 + B * h;  // 2*l*w + 2*l*h + 2*w*h + l*w
+        ribbon += A * h + B;      // l*w*h + 2*l + 2*w
     }
     fclose(f);
-    printf("Part 1: %u\n", paper);   // 1598415
-    printf("Part 2: %u\n", ribbon);  // 3812909
+    printf("%u %u\n", paper, ribbon);  // 1598415 3812909
     return 0;
 }
