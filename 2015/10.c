22b986a624ba1aa837cfbf74d018f4087c37bf38
E. Dronkert <ednl@users.noreply.github.com>
2024-12-01 21:28:09 +0100

    timing

--- 2015/10.c
+++ 2015/10.c
@@ -1,15 +1,35 @@
-// https://en.wikipedia.org/wiki/Look-and-say_sequence
+/**
+ * Advent of Code 2015
+ * Day 10: Elves Look, Elves Say
+ * https://adventofcode.com/2015/day/10
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 10.c ../startstoptimer.c
+ *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 10.c ../startstoptimer.c -lm
+ * Get minimum runtime:
+ *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime:
+ *     Mac Mini 2020 (M1 3.2 GHz)                       :    ? ms
+ *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz)         : 17.6 ms
+ *     Raspberry Pi 5 (2.4 GHz)                         :    ? ms
+ *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) :    ? ms
+ *     Raspberry Pi 4 (1.8 GHz)                         :    ? ms
+ */
+
+// Sequence info: https://en.wikipedia.org/wiki/Look-and-say_sequence
 
 #include <stdio.h>
 #include <stdlib.h>  // calloc, free
 #include <string.h>  // strcpy
 #include <math.h>    // exp, log
+#include "../startstoptimer.h"
 
 // Problem parameters part 1 & 2
 #define ITER1  40
 #define ITER2  50
 
-// https://www.wolframalpha.com/input/?i=conway’s+constant
+// https://www.wolframalpha.com/input/?i=conway%27s+constant
 #define CONWAY 1.303577269034
 
 // Personalised input from AoC
@@ -40,6 +60,7 @@ static size_t looksaytwice(char *const a, char *const b, size_t *const len, cons
 
 int main(void)
 {
+    starttimer();
     size_t len = strlen(seed);
     // Smallest size enough for ITER2=50: floor(CC^57)=3654234
     // This version ceil(CC^58)=4763577, which is fine too
@@ -67,5 +88,6 @@ int main(void)
 
     free(a);
     free(b);
+    printf("Time: %.0f us\n", stoptimer_us());
     return 0;
 }
