1177a0b47009ba554d71a5ac6a946469eb30406d
E. Dronkert <ednl@users.noreply.github.com>
2024-12-02 11:27:45 +0100

    comments

--- 2024/02.c
+++ 2024/02.c
@@ -12,23 +12,27 @@
  * Minimum runtime:
  *     Mac Mini 2020 (M1 3.2 GHz)                       :  92 µs
  *     Raspberry Pi 5 (2.4 GHz)                         : 142 µs
- *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz)         : 182 µs
+ *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz)         : 181 µs
  *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) : 231 µs
  *     Raspberry Pi 4 (1.8 GHz)                         : 449 µs
  */
 
+#define USETIMER 1
+
 #include <stdio.h>
 #include <stdlib.h>  // abs
 #include <stdbool.h>
-#include "../startstoptimer.h"
+#if USETIMER
+    #include "../startstoptimer.h"
+#endif
 
 #define FNAME "../aocinput/2024-02-input.txt"
 #define N 1000  // lines in input file
-#define LEN 32  // line buf size (max needed: 23+2=25)
-#define M 8     // max number of "levels" per line
+#define M 8     // max numbers per line ("levels per report")
+#define LEN (M * 3 + 1)  // line buf size (2 digits + 1 space/newline per number, +'\0')
 
 static int data[N][M];  // N reports with M levels
-static int count[N];    // level count per report
+static int count[N];    // actual levels per report
 
 // Parse 1- or 2-digit number, update char pointer
 static inline int num(const char **const c)
@@ -65,7 +69,11 @@ static bool issafe(const int *const level, const int count, const int skip)
 
 int main(void)
 {
+#if USETIMER
     starttimer();
+#endif
+
+    // Parse input file
     FILE *f = fopen(FNAME, "r");
     if (!f) { fputs("File not found.\n", stderr); return 1; }
     char buf[LEN];
@@ -76,15 +84,19 @@ int main(void)
     }
     fclose(f);
 
+    // Evaluate reports
     int safe = 0, damp = 0;
-    for (int i = 0; i < N; ++i)      // evaluate every report
-        for (int skip = -1; skip < count[i]; ++skip)  // different report versions
+    for (int i = 0; i < N; ++i)      // for every report
+        for (int skip = -1; skip < count[i]; ++skip)  // try different versions
             if (issafe(data[i], count[i], skip)) {
                 safe += skip == -1;  // part 1
                 ++damp;              // part 2
                 break;               // stop at first safe version
             }
     printf("%d %d\n", safe, damp);   // 516 561
+
+#if USETIMER
     printf("Time: %.0f us\n", stoptimer_us());
+#endif
     return 0;
 }
