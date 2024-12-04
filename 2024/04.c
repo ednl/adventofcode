69984014f6b4e4943623101623ace7af92c484ae
E. Dronkert <ednl@users.noreply.github.com>
2024-12-04 15:10:06 +0100

    timing & clean up

--- 2024/04.c
+++ 2024/04.c
@@ -14,9 +14,9 @@
  *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
  *     Mac Mini 2020 (M1 3.2 GHz)                       : 332 µs
- *     Raspberry Pi 5 (2.4 GHz)                         :   ? µs
- *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) :   ? µs
- *     Raspberry Pi 4 (1.8 GHz)                         :   ? µs
+ *     Raspberry Pi 5 (2.4 GHz)                         : 357 µs
+ *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) : 360 µs
+ *     Raspberry Pi 4 (1.8 GHz)                         : 820 µs
  *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz)         :   ? µs
  */
 
@@ -36,8 +36,8 @@
     #define N 140  // square char matrix in input file
 #endif
 #define FSIZE (N * (N + 1))   // input file size in bytes
-#define SLEN 4                // search string length
-#define SKIP (SLEN - 1)       // border
+#define SLEN 4                // search string length ("XMAS" or "SAMX")
+#define SKIP (SLEN - 1)       // safety border
 #define DIAGROWS (N * 2 - 1)  // row count of grid rotated by 45 degrees
 #define DIAGSKIP (DIAGROWS - SKIP * 2)  // rows in diag that can hold search string
 
@@ -48,9 +48,9 @@
 #define SM   INT32_C(0x004d0053)  // *(int *)"S_M_"
 #define MS   INT32_C(0x0053004d)  // *(int *)"M_S_"
 #define SS   INT32_C(0x00530053)  // *(int *)"S_S_"
-#define MASK INT32_C(0x00ff00ff)  // blank out don't-care
+#define MASK INT32_C(0x00ff00ff)  // blank out don't-care bytes
 
-// Tell compiler int pointer is unaligned
+// Unaligned int pointer
 typedef int32_t unaligned_int32_t __attribute__((aligned(1)));
 
 static char input[FSIZE];
@@ -58,8 +58,8 @@ static char grid[N][N];
 static char copy[N][N];
 static char diag[DIAGROWS][N];
 
-// Rotate src (=grid or copy) left by 45 degrees: src(x,y) -> diag(x+y,N-1-x+y)
-// Return pointer to first row with data long enough to hold search string
+// Discrete rotation of 'src' (=grid or copy) left by 45 degrees: src(x,y) -> diag(x+y,N-1-x+y)
+// Return pointer to first row with enough data to hold search string
 static char *rot45(const char *const src)
 {
     for (int y = 0; y < N; ++y) {
@@ -70,7 +70,7 @@ static char *rot45(const char *const src)
     return &diag[SKIP][0];
 }
 
-// Sum of "XMAS" and "SAMX" in rows of arr[rows][cols]
+// Sum of "XMAS" and "SAMX" count in rows of arr[rows][cols]
 static int count(const char *arr, const int rows, const int cols)
 {
     int sum = 0;
@@ -142,7 +142,7 @@ int main(void)
     sum = 0;
     for (int i = 1; i < N - 1; ++i)
         for (int j = 1; j < N - 1; ++j)
-            if (grid[i][j] == 'A') {
+            if (grid[i][j] == 'A') {  // search for central character
                 // Read one byte past total size of 'grid' at last pass, which is
                 // technically global-buffer-overflow but it's masked out anyway.
                 const int32_t top = *(unaligned_int32_t *)&grid[i - 1][j - 1] & MASK;
@@ -152,7 +152,7 @@ int main(void)
                     || (top == SM && btm == SM)
                     || (top == SS && btm == MM);
             }
-    printf("Part 2: %d\n", sum);  // example: 9, input: ?
+    printf("Part 2: %d\n", sum);  // example: 9, input: 1871
 
 #ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
