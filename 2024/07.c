6fb994d3632bcc84357252a29cb13c8fc9b36f5a
E. Dronkert <ednl@users.noreply.github.com>
2024-12-08 05:31:52 +0100

    day 7

--- 2024/07.c
+++ 2024/07.c
@@ -20,7 +20,7 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>    // atoll
+#include <stdlib.h>    // lldiv
 #include <stdint.h>    // int64_t
 #include <inttypes.h>  // PRId64
 #include <stdbool.h>
@@ -36,116 +36,88 @@
     #define FNAME "../aocinput/2024-07-input.txt"
     #define N 850  // lines in input file
 #endif
-#define M 12  // max number count to combine
+#define M 12  // max number count per equation
 
 typedef struct equation {
     int64_t test;
-    int maxlen, count;
-    int num[M], len[M];
-    int64_t maxadd[M], maxmul[M];
+    int count;
+    int num[M];
+    int mag[M];  // order of magnitude of numbers
 } Equation;
-
-static const int pow3[M] = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147};
-static const int64_t pow10[] = {INT64_C(1), INT64_C(10), INT64_C(100), INT64_C(1000),
-    INT64_C(10000), INT64_C(100000), INT64_C(1000000), INT64_C(10000000), INT64_C(100000000),
-    INT64_C(1000000000), INT64_C(10000000000), INT64_C(100000000000), INT64_C(1000000000000),
-    INT64_C(10000000000000), INT64_C(100000000000000), INT64_C(1000000000000000),
-    INT64_C(10000000000000000), INT64_C(100000000000000000), INT64_C(1000000000000000000)};
 
 static Equation equation[N];
 
-static inline int64_t reduce2(const int *const num, const int count, int pat)
+// Parse calibration equation = one line from input file
+static void parseline(Equation *const eq, const char *s)
 {
-    int64_t r = num[0];
-    for (int i = 1; i < count; ++i, pat >>= 1)
-        switch (pat & 1) {
-            case 0: r += num[i]; break;
-            case 1: r *= num[i]; break;
-        }
-    return r;
+    static const int mag[4] = {1, 10, 100, 1000};  // every num is less than 1000
+    int64_t test = *s++ & 15;
+    while (*s != ':')
+        test = test * 10 + (*s++ & 15);
+    ++s;  // skip ':'
+    eq->test = test;
+    int n = 0;
+    for (; *s++ == ' '; ++n) {
+        int num = *s++ & 15, len = 1;
+        for (; *s >= '0' && *s <= '9'; ++len)
+            num = num * 10 + (*s++ & 15);
+        eq->num[n] = num;
+        eq->mag[n] = mag[len];  // order of magnitude
+    }
+    eq->count = n;
 }
 
-static inline int64_t reduce3(const Equation *const eq, int pat)
+// Recursively reduce equation in reverse
+//   res = residue, starts at test value, should end at num[0]
+//   ix = index of num/mag, starts at count - 1, should end at 0
+// Returns true at first solution, false if impossible
+static bool reduce(const Equation *const eq, const int64_t res, const int ix, const int part)
 {
-    int64_t r = eq->num[0];
-    int len = eq->len[0];
-    for (int i = 1; i < eq->count; ++i, pat /= 3)
-        switch (pat % 3) {
-            case 0:
-                if (r > eq->maxadd[i]) return 0;
-                r += eq->num[i];
-                break;
-            case 1:
-                if (r > eq->maxmul[i]) return 0;
-                r *= eq->num[i];
-                break;
-            case 2:
-                len += eq->len[i];
-                if (len > eq->maxlen) return 0;
-                r = r * pow10[eq->len[i]] + eq->num[i];
-                break;
-        }
-    return r;
+    if (ix == 0)
+        return res == eq->num[0];
+    // Addition
+    if (res > eq->num[ix] && reduce(eq, res - eq->num[ix], ix - 1, part))
+        return true;
+    // Multiplication
+    lldiv_t dv = lldiv(res, eq->num[ix]);
+    if (!dv.rem && reduce(eq, dv.quot, ix - 1, part))
+        return true;
+    // Concatenation
+    if (part == 2) {
+        dv = lldiv(res, eq->mag[ix]);  // divide by order of maginitude of num
+        if (dv.rem == eq->num[ix] && reduce(eq, dv.quot, ix - 1, part))
+            return true;
+    }
+    return false;  // this branch failed
 }
 
 int main(void)
 {
+    // Read & parse input file
+    FILE *f = fopen(FNAME, "r");
+    if (!f) return 1;
+    char buf[256];
+    for (int i = 0; i < N && fgets(buf, sizeof buf, f); ++i)
+        parseline(&equation[i], buf);
+    fclose(f);
+
 #ifdef TIMER
     starttimer();
 #endif
 
-    // Parse input file
-    FILE *f = fopen(FNAME, "r");
-    if (!f) { fputs("File not found.\n", stderr); return 1; }
-    char buf[BUFSIZ];
-    for (int i = 0; i < N && fgets(buf, sizeof buf, f); ++i) {
-        Equation *const eq = &equation[i];
-        const char *c = buf;
-        eq->test = *c++ & 15;
-        int len = 1;
-        while (*c != ':') {
-            eq->test = eq->test * 10 + (*c++ & 15);
-            ++len;
-        }
-        eq->maxlen = len;
-        ++c;  // skip ':'
-        while (*c++ == ' ' && eq->count < M) {
-            int x = *c++ & 15;
-            len = 1;
-            while (*c >= '0' && *c <= '9') {
-                x = x * 10 + (*c++ & 15);
-                ++len;
-            }
-            eq->num[eq->count] = x;
-            eq->len[eq->count] = len;
-            eq->maxadd[eq->count] = eq->test - x;
-            eq->maxmul[eq->count] = eq->test / x;
-            eq->count++;
-        }
-    }
-    fclose(f);
-
     int64_t cal1 = 0, cal2 = 0;
     for (int i = 0; i < N; ++i) {
         const Equation *const eq = &equation[i];
-        const int end2 = 1 << (eq->count - 1);
-        for (int pat = 0; pat < end2; ++pat) {
-            if (reduce2(eq->num, eq->count, pat) == eq->test) {
-                cal1 += eq->test;
-                cal2 += eq->test;
-                goto next_i;  // break + continue
-            }
-        }
-        const int end3 = pow3[eq->count - 1];
-        for (int pat = 0; pat < end3; ++pat)
-            if (reduce3(eq, pat) == eq->test)
-                cal2 += eq->test;
-    next_i:;
+        if (reduce(eq, eq->test, eq->count - 1, 1)) {
+            cal1 += eq->test;
+            cal2 += eq->test;
+        } else if (reduce(eq, eq->test, eq->count - 1, 2))
+            cal2 += eq->test;
     }
-    printf("%"PRId64" %"PRId64"\n", cal1, cal2);  // test: 3749 11387, input: 303766880536 (351666462220445 too high)
+    printf("%"PRId64" %"PRId64"\n", cal1, cal2);  // test: 3749 11387, input: 303766880536 337041851384440
 
 #ifdef TIMER
-    printf("Time: %.0f ms\n", stoptimer_ms());
+    printf("Time: %.0f us\n", stoptimer_us());
 #endif
     return 0;
 }
