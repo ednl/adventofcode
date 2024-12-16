65fbb232e5744d052542af2739c16de733611497
E. Dronkert <ednl@users.noreply.github.com>
2024-12-16 11:27:35 +0100

    comments

--- 2024/15.c
+++ 2024/15.c
@@ -1,3 +1,24 @@
+/**
+ * Advent of Code 2024
+ * Day 15: Warehouse Woes
+ * https://adventofcode.com/2024/day/15
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *    clang -std=gnu17 -Wall -Wextra 15.c
+ *    gcc   -std=gnu17 -Wall -Wextra 15.c
+ * Enable timer:
+ *    clang -DTIMER -O3 -march=native 15.c ../startstoptimer.c
+ *    gcc   -DTIMER -O3 -march=native 15.c ../startstoptimer.c
+ * Get minimum runtime from timer output:
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Mac Mini 2020 (M1 3.2 GHz)                       : ? µs
+ *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) : ? µs
+ *     Raspberry Pi 5 (2.4 GHz)                         : ? µs
+ *     Raspberry Pi 4 (1.8 GHz)                         : ? µs
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 
