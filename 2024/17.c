88b40d3056392de5f04b2fa48f94c9e303312fae
E. Dronkert <ednl@users.noreply.github.com>
2024-12-18 20:46:15 +0100

    day 17

--- 2024/17.c
+++ 2024/17.c
@@ -1,6 +1,30 @@
+/**
+ * Advent of Code 2024
+ * Day 17: Chronospatial Computer
+ * https://adventofcode.com/2024/day/17
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *    clang -std=gnu17 -Wall -Wextra 17.c
+ *    gcc   -std=gnu17 -Wall -Wextra 17.c
+ * Enable timer:
+ *    clang -DTIMER -O3 -march=native 17.c ../startstoptimer.c
+ *    gcc   -DTIMER -O3 -march=native 17.c ../startstoptimer.c
+ * Get minimum runtime from timer output:
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Mac Mini 2020 (M1 3.2 GHz)                       : 10 µs
+ *     Raspberry Pi 5 (2.4 GHz)                         :  ? µs
+ *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) :  ? µs
+ *     Raspberry Pi 4 (1.8 GHz)                         :  ? µs
+ */
+
 #include <stdio.h>
-#include <stdint.h>    // uint64_t
+#include <stdint.h>    // uint64_t, UINT64_C
 #include <inttypes.h>  // PRIu64
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
 typedef uint64_t u64;
 typedef struct prog {
@@ -8,82 +32,41 @@ typedef struct prog {
     int len;
 } Prog;
 
-static const char *prog_ex1 = "0,1,5,4,3,0";
-static const char *prog_ex2 = "0,3,5,4,3,0";
-static const char *prog_inp = "2,4,1,2,7,5,4,7,1,3,5,5,0,3,3,0";
+static const char *input = "2,4,1,2,7,5,4,7,1,3,5,5,0,3,3,0";
 
-// Number of 3-bit chunks
-// static inline int chunks(u64 x)
-// {
-//     return x ? (66 - __builtin_clzll(x)) / 3 : 1;
-// }
-
-// static void bin(const u64 x)
-// {
-//     u64 bit = 1ULL << 62;
-//     while (bit) {
-//         putchar(' ');
-//         putchar('0' + (int)(x & bit)); bit >>= 1;
-//         putchar('0' + (int)(x & bit)); bit >>= 1;
-//         putchar('0' + (int)(x & bit)); bit >>= 1;
-//     }
-//     putchar('\n');
-// }
-
-// Return CSV string of 3-bit chunks of p.val, from MSB to LSB
-static void out(Prog p)
+// Output prog.val as octal with digits separated by commas.
+static char *out(Prog p)
 {
-    char csv[64];
-    int i = p.len * 2;
-    csv[--i] = '\0';
-    csv[--i] = "01234567"[p.val & 7];
-    while (i) {
-        csv[--i] = ',';
-        csv[--i] = "01234567"[(p.val >>= 3) & 7];
+    static char csv[64] = {0};
+    char buf[32], *b = buf, *c = csv;
+    snprintf(buf, sizeof buf, "%llo", p.val);
+    if (*b)
+        *c++ = *b++;
+    while (*b) {
+        *c++ = ',';
+        *c++ = *b++;
     }
-    puts(csv);
+    *c = '\0';
+    return csv;
 }
 
+// Interpret CSV line as mumber with big-endian octal digits.
 static Prog inp(const char *csv)
 {
-    Prog p = {0};
+    Prog prog = {0};
     if (csv && *csv) {
-        p.val = *csv++ & 15;
-        p.len = 1;
-        while (*csv++ == ',') {
-            p.val = p.val << 3 | (*csv++ & 15);
-            p.len++;
+        prog.val = *csv++ & 15;
+        prog.len = 1;
+        while (*csv++ == ',' && prog.len < 21) {
+            prog.val = prog.val << 3 | (*csv++ & 15);
+            prog.len++;
         }
     }
-    return p;
-}
-
-// prog: 0,1,5,4,3,0
-static Prog ex1(u64 a)
-{
-    Prog p = {0};
-    do {
-        a >>= 1;                       // 0: adv 1
-        p.val = p.val << 3 | (a & 7);  // 2: out A
-        p.len++;
-    } while (a);                       // 4: jnz 0
-    return p;
-}
-
-// prog: 0,3,5,4,3,0
-static Prog ex2(u64 a)
-{
-    Prog p = {0};
-    do {
-        a >>= 3;                       // 0: adv 3
-        p.val = p.val << 3 | (a & 7);  // 2: out A
-        p.len++;
-    } while (a);                       // 4: jnz 0
-    return p;
+    return prog;
 }
 
 // prog: 2,4,1,2,7,5,4,7,1,3,5,5,0,3,3,0
-static Prog fwd(u64 a)
+static Prog run(u64 a)
 {
     Prog p = {0};
     u64 b = 0, c = 0;
@@ -100,36 +83,40 @@ static Prog fwd(u64 a)
     return p;
 }
 
-static u64 rev(Prog p)
+// Returns minimal A for which program is quine, or zero if not found.
+static u64 rev(const Prog goal)
 {
-    u64 a = 0, b = 0, c = 0;
-    while (p.len--) {
-        b = p.val & 7;
-        p.val >>= 3;
-        b ^= 3;
-        //TODO
+    Prog stack[16] = {(Prog){0, 1}};  // a=0, len=1 (size needed for my input: 10)
+    int sp = 1;
+    u64 min = -1;
+    while (sp) {
+        const Prog a = stack[--sp];
+        const u64 mask = (UINT64_C(1) << (a.len * 3)) - 1;
+        for (unsigned i = 0; i < 8; ++i) {
+            const u64 try = a.val << 3 | i;
+            const Prog res = run(try);
+            if ((goal.val & mask) == res.val) {
+                if (goal.len > res.len)
+                    stack[sp++] = (Prog){try, a.len + 1};
+                else if (goal.len == res.len && try < min)
+                    min = try;
+            }
+        }
     }
-    return a;
+    return min;
 }
 
 int main(void)
 {
-    // Test
-    out(inp(prog_ex1));
-    out(inp(prog_ex2));
-    out(inp(prog_inp));
+#ifdef TIMER
+    starttimer();
+#endif
 
-    // Example 1
-    out(ex1(729));     // 4,6,3,5,6,3,5,2,1,0
+    printf("%s\n", out(run(35200350)));  // 2,7,4,7,2,1,7,5,1
+    printf("%"PRIu64"\n", rev(inp(input)));  // 37221274271220
 
-    // Part 1
-    Prog p = fwd(35200350);
-    out(p);            // 2,7,4,7,2,1,7,5,1
-
-    // Example 2
-    out(ex2(117440));  // 0,3,5,4,3,0
-
-    // Part 2
-    printf("%"PRIu64"\n", rev(p));  // ?
+#ifdef TIMER
+    printf("Time: %.0f us\n", stoptimer_us());
+#endif
     return 0;
 }
