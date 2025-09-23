56298edc00e5892fdaa233619d664a22053b5dcf
E. Dronkert <ednl@users.noreply.github.com>
2025-09-23 10:13:44 +0200

    comments

--- 2023/08.c
+++ 2023/08.c
@@ -5,51 +5,54 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 08.c ../startstoptimer.c
- *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 08.c ../startstoptimer.c
- * Get minimum runtime:
- *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
+ *    clang -std=c17 -Wall -Wextra -pedantic 08alt.c
+ *    gcc   -std=c17 -Wall -Wextra -pedantic 08alt.c
+ * Enable timer:
+ *    clang -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 08alt.c
+ *    gcc   -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 08alt.c
+ * Get minimum runtime from timer output:
+ *     n=10000;m=999999;for((i=0;i<n;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i/$n)";done
  * Minimum runtime:
- *     Macbook Pro 2024 (M4 4.4 GHz)       : 317 µs
- *     Mac Mini 2020 (M1 3.2 GHz)          : 432 µs
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 488 µs
- *     Raspberry Pi 5 (2.4 GHz)            : 556 µs
- *     Raspberry Pi 4 (1.8 GHz)            : 993 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz)       : 121 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)          : 171 µs
+ *     iMac 2013 (i5 Haswell 4570 3.2 GHz) :   ? µs
+ *     Raspberry Pi 5 (2.4 GHz)            : 301 µs
  */
 
-#include <stdio.h>     // fopen, fclose, fgets, fgetc, printf, fputc, puts
-#include <stdlib.h>    // qsort
+#include <stdio.h>     // getline (should perhaps check for availability)
+#include <stdlib.h>    // free
 #include <stdint.h>    // int64_t
 #include <inttypes.h>  // PRId64
-#include "../startstoptimer.h"
+#include <stdbool.h>
+#include <pthread.h>   // pthread_create, pthread_join
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define EXAMPLE 0
+#define EXAMPLE 0  // 0=full input, also available: 1,2,3 = example 1,2,3
 #if EXAMPLE
-    #define DEBUG
     #define STR_HELPER(x) #x
     #define STR(x) STR_HELPER(x)
     #define NAME "../aocinput/2023-08-example"STR(EXAMPLE)".txt"
-    #if EXAMPLE == 1
-        #define LR    2
-        #define NODES 7
-    #elif EXAMPLE == 2
-        #define LR    3
-        #define NODES 3
-    #elif EXAMPLE == 3
-        #define LR    2
-        #define NODES 8
-    #endif
 #else
-    // #define DEBUG  // uncomment or compile with -DDEBUG
-    #define NAME "../aocinput/2023-08-input.txt"
-    #define LR    293  // number of LR instructions
-    #define NODES 786  // number of nodes, each with two neighbours
+    #define NAME ("../aocinput/2023-08-input.txt")
 #endif
+#define BITS  (5)  // range 1..26 fits in 5 bits
+#define BTWO  (BITS << 1)  // twice the bits
+#define WORD  (1 << BITS)  // 32
+#define MASK  (WORD - 1)   // 31
+#define NODES (WORD * WORD * WORD)  // 32768 = simple thus fast array (but very sparse)
+#define START (('A' & MASK) << BTWO | ('A' & MASK) << BITS | ('A' & MASK))
+#define MAXTHREADS 16  // must be bigger than number of xxA nodes in input
 
-static char lr[LR + 1];  // +1 for '\0'
-static int node[NODES][3];  // each node consists of 3 IDs (hashes of node names)
+static char *lr;  // L=left,R=right instructions from first line of input
+static int node[NODES][2];
+static int threadcount;  // number of xxA nodes in input
+static int64_t xxA[MAXTHREADS];  // node index values with names ending in A
+static pthread_t tid[MAXTHREADS];  // thread IDs
 
 // Greatest Common Divisor
+// https://en.wikipedia.org/wiki/Euclidean_algorithm
 static int64_t gcd(int64_t a, int64_t b)
 {
 	int64_t t;
@@ -62,144 +65,94 @@ static int64_t gcd(int64_t a, int64_t b)
 }
 
 // Least Common Multiple
-static int64_t lcm(int64_t a, int64_t b)
+// https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+static int64_t lcm(const int64_t a, const int64_t b)
 {
 	return a / gcd(a, b) * b;
 }
 
-// Qsort helper: sort nodes by node number ascending
-// p,q actually point to int[3] but we need p[0],q[0] anyway, so this works
-static int node_asc(const void *p, const void *q)
+// Hash 3 uppercase letters to 15-bit value
+// 5-bit mask: 'A' = 1, 'Z' = 26
+// "AAA" = (( 1 * 32) +  1) * 32 +  1 =  1024 +  32 +  1 =  1057
+// "ZZZ" = ((26 * 32) + 26) * 32 + 26 = 26624 + 832 + 26 = 27482
+static int hash(const char *s)
 {
-    const int a = *(const int*)p;
-    const int b = *(const int*)q;
-    if (a < b) return -1;
-    if (a > b) return  1;
-    return 0;
+    return (s[0] & MASK) << BTWO | (s[1] & MASK) << BITS | (s[2] & MASK);
 }
 
-// Convert 3 chars to int ID ("hash value")
-static int str2int(const char *s)
+static inline bool end_not_z(const int64_t i)
 {
-    int n = 0;
-    for (int i = 0; i < 3; ++i)
-        n = (n << 8) + *s++;  // store chars in bytes, first char ends up in MSByte
-    return n;
+    return (i & MASK) != ('Z' & MASK);
 }
 
-#ifdef DEBUG
-// Output int as 3 chars to stdout
-static void printnode(int n)
+// Navigate the map from node at index i to first node that ends in Z
+// For part 1, this works because the first xxZ node from AAA is ZZZ
+// For part 2, this is good enough because the first xxZ node from xxA
+// always loops back to the starting point; otherwise loop detection
+// would be needed.
+// Return: number of steps (as a void* because this the thread function)
+static void *navigate(void *arg)
 {
-    for (int shift = 24; shift; )
-        fputc(n >> (shift -= 8) & 0xff, stdout);
-}
-#endif
-
-// Binsearch in sorted node array for name n ("hash table")
-// All data is consistent, so a node will always be found
-// Return: node index i where node[i][0] == n
-static int nodeindex(const int n)
-{
-    int l = 0, r = NODES - 1;
-    if (n == node[l][0]) return l;
-    if (n == node[r][0]) return r;
-    // Now always true: node[l][0] < n < node[r][0]
-    while (r - l != 1) {
-        int m = ((l + 1) >> 1) + (r >> 1);  // avoid index overflow
-        if      (n > node[m][0]) l = m;
-        else if (n < node[m][0]) r = m;
-        else return m;
+    int64_t i = (int64_t)arg, steps = 0;  // both could be int but compiler complains about cast
+    for (const char *dir = lr; end_not_z(i); ++steps) {  // start all searches from beginning of LR instructions
+        i = node[i][(int)*dir];  // *dir is 0 for L, 1 for R, cast to suppress warning
+        if (*++dir == '\n')      // fetch next LR instruction, check for end
+            dir = lr;            // loop around
     }
-    return -1;  // should not happen for this data
-}
-
-// Walk from node at index i to first node that ends in Z
-// For part 1, this works because the first xxZ node is ZZZ
-// For part 2, this is good enough because the first xxZ node always loops
-// back to the starting point; otherwise loop detection would be needed
-// Return: number of steps
-static int walk(int i)
-{
-    const char *go = lr;  // start all searches from beginning of LR instructions
-    int steps = 0;
-    while ((node[i][0] & 0xff) != 'Z') {
-        i = node[i][(int)*go];  // *go is 1 for L, 2 for R
-        if (!*++go)             // fetch next LR instruction, check for end
-            go = lr;            // loop around
-        ++steps;
-    }
-    return steps;
+    return (void *)steps;  // return as thread compatible result
 }
 
 int main(void)
 {
+#ifdef TIMER
     starttimer();
-    FILE *f = fopen(NAME, "r");
-    if (!f) { fputs("File not found.\n", stderr); return 1; }
+#endif
 
-    fgets(lr, sizeof lr, f);  // one long line of LR instructions, excludes '\n'
-    fgetc(f); fgetc(f);  // newline, empty line
-    char buf[32];
-    for (int i = 0; i < NODES; ++i) {
-        fgets(buf, sizeof buf, f);       // read whole line
-        node[i][0] = str2int(buf);       // node name to number
-        node[i][1] = str2int(buf + 7);   // next L node name to number
-        node[i][2] = str2int(buf + 12);  // next R node name to number
+    // Read and convert one line of 'L' and 'R' instructions to index 0 or 1
+    FILE *f = fopen(NAME, "r");
+    if (!f)
+        return 1;
+    size_t lr_size;
+    int LR = getline(&lr, &lr_size, f) - 1;  // subtract 1 for '\n'
+    if (LR < 1)
+        return 2;
+    for (int i = 0; i < LR; ++i)  // leave '\n' intact
+        lr[i] = lr[i] >> 1 & 1;   // 'L'=0b1001100 => 0, 'R'=0b1010010 => 1
+
+    // Skip empty line
+    fgetc(f);
+
+    // Read and convert named nodes to "hashed" index values
+    // Save nodes ending in A
+    char *buf = NULL;
+    size_t bufsize;
+    while (getline(&buf, &bufsize, f) > 1) {  // e.g.: "ABC = (DEF, GHI)\n"
+        const int i = hash(buf);
+        node[i][0] = hash(buf + 7);
+        node[i][1] = hash(buf + 12);
+        if (buf[2] == 'A')  // save nodes ending in 'A'
+            xxA[threadcount++] = i;
     }
     fclose(f);
+    free(buf);
 
-    #ifdef DEBUG
-    printf("%s\n\n", lr);
-    #endif
+    // Launch thread for each map navigation from node xxA to xxZ
+    for (int i = 0; i < threadcount; ++i)
+        pthread_create(&tid[i], NULL, navigate, (void *)xxA[i]);
 
-    // Transform LR instructions from ['L','R'] to [1,2] (=index into node entry)
-    for (int i = 0; i < LR; ++i)
-        lr[i] = (char)(lr[i] >> 3 & 3);  // 'L'=0b1001100 => 1, 'R'=0b1010010 => 2
-
-    #ifdef DEBUG
-    for (int i = 0; i < NODES; ++i) {
-        printf("%3d: ", i);
-        printnode(node[i][0]);
-        printf(" = (");
-        printnode(node[i][1]);
-        fputc(',', stdout);
-        printnode(node[i][2]);
-        puts(")");
+    // Wait for threads to finish, take LCM of results
+    int64_t part2 = 1;  // LCM(x,1) = x
+    void *steps;  // thread return value
+    for (int i = 0; i < threadcount; ++i) {
+        pthread_join(tid[i], &steps);
+        part2 = lcm(part2, (int64_t)steps);
+        if (xxA[i] == START)  // AAA node may be anywhere
+            printf("Part 1: %"PRId64"\n", (int64_t)steps);  // ex1: 2, ex2: 6, ex3: 2, input: 19631
     }
-    printf("\n");
-    #endif
-
-    // Sort nodes by node number (=node[][0])
-    qsort(node, NODES, sizeof *node, node_asc);  // *node is an int[3] so size is 12
-
-    // Transform next node numbers to index of node array
-    for (int i = 0; i < NODES; ++i) {
-        node[i][1] = nodeindex(node[i][1]);
-        node[i][2] = nodeindex(node[i][2]);
-    }
-
-    #ifdef DEBUG
-    printf("\n");
-    for (int i = 0; i < NODES; ++i)
-        printf("%3d: %7d = (%3d,%3d)\n", i, node[i][0], node[i][1], node[i][2]);
-    printf("\n");
-    #endif
-
-    const int part1 = walk(0);  // node AAA is at index 0
-    printf("Part 1: %d\n", part1);  // ex1: 2, ex2: 6, ex3: 2, input: 19631
-
-    // Part 2
-    // For every xxA node, determine no. of steps to first xxZ node, then take LCM of those
-    int64_t part2 = part1;  // node AAA from part 1
-    for (int i = 1; i < NODES; ++i)  // skip node AAA at index 0
-        if ((node[i][0] & 0xff) == 'A') {
-            part2 = lcm(part2, walk(i));
-            #ifdef DEBUG
-            printf("%d: len=%d lcm=%"PRId64"\n", i, walk(i), part2);
-            #endif
-        }
     printf("Part 2: %"PRId64"\n", part2);  // ex1: 2, ex2: 6, ex3: 6, input: 21003205388413
+    free(lr);
+
+#ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
-    return 0;
+#endif
 }
