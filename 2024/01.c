3024f03ae1a6d3de1a44cb9ae7c3dfa7b22b02ec
E. Dronkert <ednl@users.noreply.github.com>
2025-11-15 18:51:19 +0100

    optional timer

--- 2024/01.c
+++ 2024/01.c
@@ -5,22 +5,25 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 01.c ../startstoptimer.c
- *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 01.c ../startstoptimer.c
+ *    cc -std=c17 -Wall -Wextra -pedantic 01.c
+ * Enable timer:
+ *    cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 01.c
  * Get minimum runtime:
- *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime:
- *     Macbook Pro 2024 (M4 4.4 GHz)                    :  64 µs
- *     Mac Mini 2020 (M1 3.2 GHz)                       : 108 µs
- *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz)         : 181 µs
- *     Raspberry Pi 5 (2.4 GHz)                         : 189 µs
- *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) : 237 µs
- *     Raspberry Pi 4 (1.8 GHz)                         : 504 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz)                    :   ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)                       :  92 µs
+ *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz)         :   ? µs
+ *     Raspberry Pi 5 (2.4 GHz)                         :   ? µs
+ *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) :   ? µs
+ *     Raspberry Pi 4 (1.8 GHz)                         :   ? µs
  */
 
 #include <stdio.h>
 #include <stdlib.h>  // qsort, abs
-#include "../startstoptimer.h"
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
 #define FNAME "../aocinput/2024-01-input.txt"
 #define FSIZE 16384  // >= input file size in bytes
@@ -52,13 +55,16 @@ static int num5(const char **const c)
 
 int main(void)
 {
-    starttimer();
-
     // Read input file
     FILE *f = fopen(FNAME, "rb");  // fread requires binary mode
-    if (!f) { fputs("File not found.\n", stderr); return 1; }
+    if (!f) { fputs("File not found.\n", stderr); return EXIT_FAILURE; }
     fread(input, sizeof input, 1, f);  // read whole file at once
     fclose(f);
+
+#ifdef TIMER
+    // Excludes reading from disk
+    starttimer();
+#endif
 
     // Read numbers into columns
     const char *c = input;
@@ -87,7 +93,8 @@ int main(void)
     }
     printf("Part 2: %d\n", simil);  // 26859182
 
-    // Time including reading/parsing of input
+#ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
-    return 0;
+#endif
+    return EXIT_SUCCESS;
 }
