6f2b4c8782515ab5fe10ffee4691c981d7ade89b
E. Dronkert <ednl@users.noreply.github.com>
2025-11-25 22:56:19 +0100

    2021 day 24 finally

--- 2021/24.c
+++ 2021/24.c
@@ -1,147 +1,131 @@
+/**
+ * Advent of Code 2021
+ * Day 24: Arithmetic Logic Unit
+ * https://adventofcode.com/2021/day/24
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Validate 14-digit numbers where each digit is from 1 to 9.
+ * Part 1: find largest valid number.
+ * Part 2: find smallest valid number.
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 24.c
+ * Enable timer:
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 24.c
+ * Get minimum runtime from timer output in bash:
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 13 µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 22 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  ? µs
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <stdint.h>
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define REGCOUNT  ('z' - 'w' + 1)
-#define STACKSIZE 14
-#define MEMSIZE   256
+#define FNAME "../aocinput/2021-24-input.txt"
+#define N 14
+#define M (N >> 1)
 
-static const char *lang[] = {"nop", "inp", "add", "mul", "div", "mod", "eql"};
-typedef enum {
-    NOP = 0, INP, ADD, MUL, DIV, MOD, EQL
-} OpCode;
+static int rule[N];
+static int index[M];
+static int value[M];
+char max[N + 1];  // part 1
+char min[N + 1];  // part 2
 
-typedef struct {
-    OpCode op;
-    int    arg[2];
-    bool   reg;
-} Instr;
+// Manually translated from input file:
+// int z = w[0] + 2;          // (w0+2)
+// z = z * 26 + w[1] + 13;    // (w0+2)(w1+13)
+// z = z * 26 + w[2] + 13;    // (w0+2)(w1+13)(w2+13)
+// if (z % 26 - 13 == w[3])   // w2 + 13 - 13 == w3
+//     z /= 26;               // (w0+2)(w1+13)
+// z = z * 26 + w[4] + 15;    // (w0+2)(w1+13)(w4+15)
+// if (z % 26 - 13 == w[5])   // w4 + 15 - 13 == w5
+//     z /= 26;               // (w0+2)(w1+13)
+// if (z % 26 - 7 == w[6])    // w1 + 13 - 7 == w6
+//     z /= 26;               // (w0+2)
+// z = z * 26 + w[7] + 5;     // (w0+2)(w7+5)
+// z = z * 26 + w[8] + 16;    // (w0+2)(w7+5)(w8+16)
+// z = z * 26 + w[9] + 1;     // (w0+2)(w7+5)(w8+16)(w9+1)
+// if (z % 26 - 4 == w[10])   // w9 + 1 - 4 == w10
+//     z /= 26;               // (w0+2)(w7+5)(w8+16)
+// if (z % 26 - 9 == w[11])   // w8 + 16 - 9 == w11
+//     z /= 26;               // (w0+2)(w7+5)
+// if (z % 26 - 13 == w[12])  // w7 + 5 - 13 == w12
+//     z /= 26;               // (w0+2)
+// if (z % 26 - 9 == w[13])   // w0 + 2 - 9 == w13
+//     z /= 26;               // (0)
+// return z;
 
-typedef struct {
-    Instr *ip;
-    int   *sp;
-    int    reg[REGCOUNT];
-    int    stack[STACKSIZE];
-    Instr  mem[MEMSIZE];
-} VM;
+// Rule summary:
+// w2 + 13 - 13 == w3    =>   w2     == w3
+// w4 + 15 - 13 == w5    =>   w4 + 2 == w5
+// w1 + 13 -  7 == w6    =>   w1 + 6 == w6
+// w9 +  1 -  4 == w10   =>   w9 - 3 == w10
+// w8 + 16 -  9 == w11   =>   w8 + 7 == w11
+// w7 +  5 - 13 == w12   =>   w7 - 8 == w12
+// w0 +  2 -  9 == w13   =>   w0 - 7 == w13
 
-static VM vm = {0};
+//  index: 01234567890123
+// Part 1: 93997999296912 (largest)
+// Part 2: 81111379141811 (smallest)
 
-static bool run(int64_t model)
+int main(void)
 {
-    vm.sp = vm.stack;
-    for (int i = 0; i < STACKSIZE; ++i) {
-        *(vm.sp++) = model % 9 + 1;
-        model /= 9;
-    }
-    for (int i = 0; i < REGCOUNT; ++i)
-        vm.reg[i] = 0;
-    for (vm.ip = vm.mem;; vm.ip++) {
-        int r = vm.ip->arg[0], val = vm.ip->reg ? vm.reg[vm.ip->arg[1]] : vm.ip->arg[1];
-        switch (vm.ip->op) {
-            case NOP: return vm.reg['z' - 'w'] == 0;
-            case INP: vm.reg[r] = *--vm.sp; break;
-            case ADD: vm.reg[r] += val; break;
-            case MUL: vm.reg[r] *= val; break;
-            case DIV: vm.reg[r] /= val; break;
-            case MOD: vm.reg[r] %= val; break;
-            case EQL: vm.reg[r] = vm.reg[r] == val; break;
+#ifdef TIMER
+    starttimer();
+#endif
+
+    FILE *f = fopen(FNAME, "r");
+    if (!f)
+        return 1;
+    char buf[16];  // very short lines in input file
+    for (int i = 0, add = 0; i < N && fgets(buf, sizeof buf, f); )
+        if (buf[0] == 'i')  // new digit
+            add = 0;        // counter reset
+        else if (add < 4 && buf[0] == 'a' && (buf[4] == 'x' || buf[4] == 'y') && buf[6] < 'w') {
+            // For every digit (under "inp w") there are 4 lines "add [xy] [0-9-]+".
+            // If the first is negative, put it in the rules.
+            // Otherwise, put the 4th in the rules.
+            if ((add == 0 && buf[6] == '-') || add == 3) {
+                rule[i++] = atoi(&buf[6]);
+                continue;
+            }
+            ++add;
         }
-    }
-}
+    fclose(f);
 
-static int64_t model(int64_t base10)
-{
-    static const int64_t dec[STACKSIZE] = {1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000,10000000000,100000000000,1000000000000,10000000000000};
-    int64_t n = 0;
-    for (int i = 0; i < STACKSIZE; ++i) {
-        n += dec[i] * (base10 % 9 + 1);
-        base10 /= 9;
-    }
-    return n;
-}
+    // My input:
+    // rule[] = {2, 13, 13, -13, 15, -13, -7, 5, 16, 1, -4, -9, -13, -9};
 
-static int64_t base10(int64_t model)
-{
-    static const int64_t non[STACKSIZE] = {1,9,81,729,6561,59049,531441,4782969,43046721,387420489,3486784401,31381059609,282429536481,2541865828329};
-    int64_t n = 0;
-    for (int i = 0; i < STACKSIZE; ++i) {
-        n += non[i] * (model % 10 - 1);
-        model /= 10;
-    }
-    return n;
-}
-
-int main(int argc, char *argv[])
-{
-    FILE *f = fopen("../aocinput/2021-24-input.txt", "r");
-    int c, ip = 0;
-    for (; ip < MEMSIZE && (c = fgetc(f)) != EOF; ++ip) {
-        printf("%d:", ip);
-
-        OpCode op = NOP;
-        int arg0 = 0, arg1 = 0;
-        bool reg = false;
-
-        int d = fgetc(f);
-        switch (c) {
-            case 'a': op = ADD; break;
-            case 'd': op = DIV; break;
-            case 'e': op = EQL; break;
-            case 'i': op = INP; break;
-            case 'm': op = d == 'u' ? MUL : MOD; break;
-        }
-        fgetc(f);
-        printf(" %s", lang[op]);
-
-        if ((c = fgetc(f)) == ' ') {  // there's more?
-            arg0 = fgetc(f) - 'w';
-            printf(" %c", arg0 + 'w');
-
-            if ((c = fgetc(f)) == ' ') {  // there's more?
-                c = fgetc(f);
-                if (c >= 'w' && c <= 'z') {
-                    arg1 = c - 'w';
-                    reg = true;
-                    printf(" %c", arg1 + 'w');
-                    while ((c = fgetc(f)) != '\n');
-                } else if (c >= '-' && c <= '9') {
-                    char buf[4] = {(char)c};
-                    int i = 1;
-                    while ((c = fgetc(f)) != '\n')
-                        buf[i++] = (char)c;
-                    buf[i] = '\0';
-                    arg1 = atoi(buf);
-                    printf(" %d", arg1);
-                }
+    for (int i = 0, sp = 0; i < N; ++i) {
+        if (rule[i] > 0) {
+            index[sp] = i;
+            value[sp++] = rule[i];
+        } else {
+            const int j = index[--sp];
+            const int k = value[sp] + rule[i];
+            // num[j] + k == num[i];
+            if (k >= 0) {
+                max[j] = '9' - k;
+                max[i] = '9';
+                min[j] = '1';
+                min[i] = '1' + k;
+            } else {
+                max[j] = '9';
+                max[i] = '9' + k;
+                min[j] = '1' - k;
+                min[i] = '1';
             }
         }
-        vm.mem[ip] = (Instr){op, {arg0, arg1}, reg};
-        printf("\n");
     }
-    fclose(f);
-    if (ip < MEMSIZE) {
-        vm.mem[ip].op = NOP;
-        printf("%d: %s\n", ip, lang[NOP]);
-    }
+    printf("Part 1: %s\n", max);  // 93997999296912
+    printf("Part 2: %s\n", min);  // 81111379141811
 
-    int64_t minmodel = 0, maxmodel = 0;
-    for (int i = 0; i < STACKSIZE; ++i) {
-        minmodel = minmodel * 10 + 1;
-        maxmodel = maxmodel * 10 + 9;
-    }
-
-    if (argc > 1 && argv && argv[1])
-        minmodel = atoll(argv[1]);
-    printf("min=%lld\nmax=%lld\n", minmodel, maxmodel);
-
-    // int64_t val = base10(maxmodel) + 1;
-    // while (!run(--val));
-    // printf("Part 1: %lld\n", model(val));
-
-    int64_t val = base10(minmodel) - 1;
-    while (!run(++val));
-    printf("Part 2: %lld\n", model(val));
-    return 0;
+#ifdef TIMER
+    printf("Time: %.0f us\n", stoptimer_us());
+#endif
 }
--- 2024/01.c
+++ 2024/01.c
@@ -11,9 +11,9 @@
  * Get minimum runtime from timer output in bash:
  *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime:
- *     Macbook Pro 2024 (M4 4.4 GHz)                    :  56 µs
- *     Mac Mini 2020 (M1 3.2 GHz)                       :  92 µs
- *     Raspberry Pi 5 (2.4 GHz)                         : 164 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  56 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  92 µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 164 µs
  */
 
 #include <stdio.h>
