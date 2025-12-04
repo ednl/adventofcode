12e6bccd281aee3ff8dffbdb2d8b83fc5a7ea9a6
E. Dronkert <ednl@users.noreply.github.com>
2025-12-04 15:09:37 +0100

    rpi timing

--- 2025/04alt.c
+++ 2025/04alt.c
@@ -11,9 +11,9 @@
  * Get minimum runtime from timer output in bash:
  *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :  250 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    :    ? µs
- *     Raspberry Pi 5 (2.4 GHz)      :    ? µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 250 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :   ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 603 µs
  */
 
 #include <stdio.h>
@@ -116,7 +116,7 @@ int main(void)
             check(i, j);
     printf("%d\n", qlen);  // example: 13, input: 1495
 
-    // Part 2: remove & check neighbours until no more paper rolls removable
+    // Part 2: remove, check neighbours, repeat until no more paper rolls removable
     Vec pos;
     int removed = 0;
     while (dequeue(&pos)) {
