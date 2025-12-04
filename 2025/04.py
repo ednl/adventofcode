ed8aa5ee6ef33e03b5a247c5dd7e9bb2881d8e49
E. Dronkert <ednl@users.noreply.github.com>
2025-12-04 22:36:43 +0100

    python timing

--- 2025/04.py
+++ 2025/04.py
@@ -5,8 +5,13 @@ https://adventofcode.com/2025/day/4
 By: E. Dronkert https://github.com/ednl
 '''
 
+from time import monotonic_ns
+t0 = monotonic_ns()
+
 import numpy as np
 from scipy.signal import convolve2d
+
+t1 = monotonic_ns()
 
 # convert to array filled with 0 (=floor/space '.') or 1 (=paper roll '@')
 # with open('../aocinput/2025-04-example.txt') as f:
@@ -22,10 +27,14 @@ def forklift():
     state = state & (nb > 3)  # leave rolls that have more than 3 neighbours
     return np.sum(state)      # new number of paper rolls
 
+t2 = monotonic_ns()
+
 init = np.sum(state)  # initial number of paper rolls
 count = forklift()    # number of paper rolls after 1 iteration
 print(f"Part 1: {init - count}")  # number of paper rolls removed after 1 iteration
 # example: 13, input: 1495
+
+t3 = monotonic_ns()
 
 prev = 0
 while count != prev:
@@ -33,3 +42,13 @@ while count != prev:
     count = forklift()
 print(f"Part 2: {init - count}")  # total number of paper rolls removed
 # example: 43, input: 8768
+
+t4 = monotonic_ns()
+
+print('import libs : {:5.1f} ms'.format((t1 - t0)/1000000))
+print('parse input : {:5.1f} ms'.format((t2 - t1)/1000000))
+print('part 1      : {:5.1f} ms'.format((t3 - t2)/1000000))
+print('part 2      : {:5.1f} ms'.format((t4 - t3)/1000000))
+print('part 1+2    : {:5.1f} ms'.format((t4 - t2)/1000000))
+print('no import   : {:5.1f} ms'.format((t4 - t1)/1000000))
+print('all         : {:5.1f} ms'.format((t4 - t0)/1000000))
