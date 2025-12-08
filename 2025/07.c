2ea887293e13eb8b741c96a82c9fedda82cc7154
E. Dronkert <ednl@users.noreply.github.com>
2025-12-08 06:50:36 +0100

    comments

--- 2025/07.c
+++ 2025/07.c
@@ -64,14 +64,14 @@ int main(void)
     int splits = 0;  // part 1: number of splitters hit with a beam
     int col = HALF, end = HALF + 1;  // start/stop columns
     for (int i = 2; i < M; i += 2, --col, ++end)  // peg row on grid
-        for (int j = col; j < end; ++j)
+        for (int j = col; j < end; ++j)  // only look at triangle, not whole square
             if (grid[i][j] == SPLIT && galton[j]) { // splitter and beam in this column?
                 ++splits;  //  part 1: beam has hit a splitter
                 galton[j - 1] += galton[j];  // may already have value
                 galton[j + 1] += galton[j];  // may already have value
                 galton[j] = 0;  // peg shadow
             }
-    int64_t worlds = 0;  // part 2
+    int64_t worlds = 0;  // part 2: all possible tachyon beam paths
     for (int j = 0; j < N; ++j)
         worlds += galton[j];
     printf("%d %"PRId64"\n", splits, worlds);  // example: 21 40, input: 1598 4509723641302
