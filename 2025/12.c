dea7142fddae2676acde1fd0788dbebc0d1bd5d0
E. Dronkert <ednl@users.noreply.github.com>
2025-12-12 11:56:32 +0100

    only for input is faster

--- 2025/12.c
+++ 2025/12.c
@@ -11,9 +11,9 @@
  * Get minimum runtime from timer output in bash:
  *     m=9999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :  8.3 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 20.1 µs (strangely slow?)
- *     Raspberry Pi 5 (2.4 GHz)      : 21.5 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  5.4 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : ? µs
  */
 
 #include <stdio.h>
@@ -21,33 +21,17 @@
     #include "../startstoptimer.h"
 #endif
 
-#define EXAMPLE 0
-#if EXAMPLE == 1
-    #define FNAME "../aocinput/2025-12-example.txt"
-    #define FSIZE 149
-    #define TREES 3
-#else
-    #define FNAME "../aocinput/2025-12-input.txt"
-    #define FSIZE 25096
-    #define TREES 1000
-#endif
+#define FNAME "../aocinput/2025-12-input.txt"
+#define FSIZE 25096
 #define SHAPES 6
-
-// Threshold percentage of area for shapes to fit trees
-// I mean, c'mon!!
-#define BAR 85
+#define BASE (('.' * 3 + '\n') * 3)  // shape with zero parts
+#define DIFF ('.' - '#')  // one part
+#define TREES 1000
 
 static char input[FSIZE];
 static int shape[SHAPES];
-
-// Read 1- or 2-digit number, advance char pointer
-static int readnum(const char **s)
-{
-    int x = *(*s)++ & 15;
-    if (**s >= '0' && **s <= '9')
-        x = x * 10 + (*(*s)++ & 15);
-    return x;
-}
+static int area[TREES];
+static int list[TREES][SHAPES];
 
 int main(void)
 {
@@ -61,25 +45,33 @@ int main(void)
     starttimer();
 #endif
 
-    int fits = 0;
+    // Parse input
     const char *c = input;
     for (int i = 0; i < SHAPES; ++i) {
         c += 3;  // skip digit, ':', newline
+        int sum = 0;
         for (int j = 0; j < 12; ++j)
-            shape[i] += *c++ == '#';
+            sum += *c++;
+        shape[i] = (BASE - sum) / DIFF;
         c++;  // skip blank line
     }
     for (int i = 0; i < TREES; ++i) {
-        int avail = readnum(&c);
-        c++;  // skip 'x'
-        avail *= readnum(&c);
-        c += 2;  // skip ':' and space
-        int need = 0;
-        for (int j = 0; j < SHAPES; ++j, ++c)  // also skip space or newline
-            need += readnum(&c) * shape[j];
-        fits += need * 100 / avail < BAR;
+        area[i] = ((*c & 15) * 10 + (*(c + 1) & 15)) * ((*(c + 3) & 15) * 10 + (*(c + 4) & 15));
+        c += 7;  // skip "00x00: "
+        for (int j = 0; j < SHAPES; ++j, c += 3)
+            list[i][j] = (*c & 15) * 10 + (*(c + 1) & 15);
     }
-    printf("%d\n", fits);  // example: 2, input: 521
+
+    //
+    int fits = 0;
+    for (int i = 0; i < TREES; ++i) {
+        int sum = 0;
+        for (int j = 0; j < SHAPES; ++j)
+            sum += list[i][j] * shape[j];
+        // I mean, c'mon!!
+        fits += sum < area[i];
+    }
+    printf("%d\n", fits);  // 521
 
 #ifdef TIMER
     printf("Time: %.0f ns\n", stoptimer_ns());
