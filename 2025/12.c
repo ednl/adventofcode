ca35d8c9cb37f6cdbf9d9e2649e533a2062e7a3f
E. Dronkert <ednl@users.noreply.github.com>
2025-12-15 00:52:47 +0100

    timing

--- 2025/12.c
+++ 2025/12.c
@@ -13,7 +13,7 @@
  * Minimum runtime measurements:
  *     Macbook Pro 2024 (M4 4.4 GHz) :  4.04 µs
  *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
- *     Raspberry Pi 5 (2.4 GHz)      : ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 17.4  µs
  */
 
 #include <stdio.h>
