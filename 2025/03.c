a391190199c2a49c6c739a4b5475c3d8a1787b8a
E. Dronkert <ednl@users.noreply.github.com>
2025-12-21 11:54:04 +0100

    optimised loop

--- 2025/03.c
+++ 2025/03.c
@@ -11,9 +11,9 @@
  * Get minimum runtime from timer output in bash:
  *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :  49 µs
- *     Raspberry Pi 5 (2.4 GHz)      : 130 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 171 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  41 µs
+ *     Raspberry Pi 5 (2.4 GHz)      :   ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :   ? µs
  */
 
 #include <stdio.h>
@@ -26,50 +26,47 @@
 #define EXAMPLE 0
 #if EXAMPLE
     #define FNAME "../aocinput/2025-03-example.txt"
-    #define BANKS     4
-    #define BANKLEN  15
+    #define ROWS 4
+    #define COLS 15
 #else
     #define FNAME "../aocinput/2025-03-input.txt"
-    #define BANKS   200
-    #define BANKLEN 100
+    #define ROWS 200
+    #define COLS 100
 #endif
 
-static char battery[BANKS][BANKLEN + 1];  // +'\n'
+static char input[ROWS][COLS + 1];  // +'\n'
 
-static int64_t joltage(const int batteries)
+static int64_t joltage(const int len)
 {
-    const int firstend = BANKLEN + 1 - batteries;  // one past last index of first window
-    int64_t total = 0;
-    for (int bank = 0; bank < BANKS; ++bank) {
-        int64_t jolt = 0;
-        for (int i = 0; i < batteries; ++i) {
-            const int end = firstend + i;  // one past last index of current window
-            int maxkey = -1;
-            char maxval = 0;
-            for (int k = maxkey + 1; maxval < '9' && k < end; ++k)  // search window and shortcut
-                if (battery[bank][k] > maxval)
-                    maxval = battery[bank][(maxkey = k)];
-            jolt = jolt * 10 + (maxval & 15);
+    int64_t sum = 0;
+    for (int i = 0; i < ROWS; ++i) {  // process all rows separately
+        int64_t num = 0;  // len-digit number on this row
+        for (int key = -1, last = COLS - len; last < COLS; ++last) {  // count digits 1..len
+            char max = 0;  // best value found
+            for (int j = key + 1; j <= last; ++j)  // leave room for remaining digits
+                if (input[i][j] > max && (max = input[i][(key = j)]) == '9')
+                    break;  // can't get higher than '9'
+            num = num * 10 + (max & 15);
         }
-        total += jolt;
+        sum += num;
     }
-    return total;
+    return sum;
 }
 
 int main(void)
 {
     FILE *f = fopen(FNAME, "rb");  // fread requires binary mode
     if (!f) { fprintf(stderr, "File not found: %s\n", FNAME); return 1; }
-    fread(battery, sizeof battery, 1, f);  // read whole file at once
+    fread(input, sizeof input, 1, f);  // read whole file at once
     fclose(f);
 
 #ifdef TIMER
     starttimer();
 #endif
 
-    // example:   357   3121910778619
-    // input  : 17324 171846613143331
-    printf("%"PRId64" %"PRId64"\n", joltage(2), joltage(12));
+    printf("%"PRId64" %"PRId64"\n",
+        joltage(2),    // example: 357, input: 17324
+        joltage(12));  // example: 3121910778619, input: 171846613143331
 
 #ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
