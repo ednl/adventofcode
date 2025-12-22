20d7bdfb69358ab5fb3ee0a5c4d84b8bbff47cb3
E. Dronkert <ednl@users.noreply.github.com>
2025-12-22 11:02:51 +0100

    repo cleanup

--- 2025/04.c
+++ 2025/04.c
@@ -5,15 +5,15 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    cc -std=c17 -Wall -Wextra -pedantic 04.c
+ *    cc -std=c17 -Wall -Wextra -pedantic 04alt.c
  * Enable timer:
- *    cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 04.c
+ *    cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 04alt.c
  * Get minimum runtime from timer output in bash:
  *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :  709 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    :    ? µs
- *     Raspberry Pi 5 (2.4 GHz)      : 2970 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 244 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : 360 µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 601 µs
  */
 
 #include <stdio.h>
@@ -23,20 +23,60 @@
 #endif
 
 #define EXAMPLE 0
-#if EXAMPLE
+#if EXAMPLE == 1
     #define FNAME "../aocinput/2025-04-example.txt"
     #define N 10
+    #define QSIZE 16
+#elif EXAMPLE == 2
+    #define FNAME "04_extra.txt"
+    #define N 258
+    #define QSIZE 10000  // max needed for extra input: 8638
 #else
     #define FNAME "../aocinput/2025-04-input.txt"
     #define N 140
+    #define QSIZE 2048  // max needed for my input: 1495 (=first iteration)
 #endif
-#define ROLL  '@'
-#define SPACE '.'
-#define BOXED 4
+
+#define ROLL  '@'   // paper roll
+#define SPACE '.'   // free space/floor
+#define BOXED 4     // 4 or more neighbours = unreachable for the forklift
+
+typedef struct vec {
+    int row, col;
+} Vec;
 
 static char grid[N + 2][N + 3];  // +border, +'\0'
+static bool seen[N + 2][N + 3];  // marked for removal (=queued)
+static Vec queue[QSIZE];
+static int qhead, qtail, qlen;
 
-#if EXAMPLE
+// Add to head of queue (FIFO)
+// Return false if queue was full
+static bool enqueue(const Vec pos)
+{
+    if (qlen == QSIZE)
+        return false;
+    queue[qhead++] = pos;
+    if (qhead == QSIZE)
+        qhead = 0;
+    qlen++;
+    return true;
+}
+
+// Remove from tail of queue (FIFO)
+// Return false if queue was empty
+static bool dequeue(Vec *const v)
+{
+    if (qlen == 0)
+        return false;
+    *v = queue[qtail++];
+    if (qtail == QSIZE)
+        qtail = 0;
+    qlen--;
+    return true;
+}
+
+#if EXAMPLE == 1
 static void show(void)
 {
     for (int i = 1; i <= N; ++i)
@@ -44,25 +84,23 @@ static void show(void)
 }
 #endif
 
-static int forklift(const bool remove)
+// Neigbour count low enough?
+static bool reachable(const int row, const int col)
 {
-    int count = 0;
-    for (int i = 1; i < N + 1; ++i)
-        for (int j = 1; j < N + 1; ++j)
-            if (grid[i][j] == ROLL) {
-                const int y0 = i - 1, y1 = i + 2;
-                const int x0 = j - 1, x1 = j + 2;
-                int sum = 0;
-                for (int y = y0; y < y1; ++y)
-                    for (int x = x0; x < x1; ++x)
-                        sum += grid[y][x] == ROLL;
-                if (sum < BOXED + 1) {  // includes centre roll
-                    ++count;
-                    if (remove)
-                        grid[i][j] = SPACE;
-                }
-            }
-    return count;
+    int nb = 0;
+    for (int i = row - 1; i < row + 2; ++i)
+        for (int j = col - 1; j < col + 2; ++j)
+            nb += grid[i][j] == ROLL;  // count self, too
+    return nb < BOXED + 1;  // +1 because self is always '@'
+}
+
+// Is it a paper roll, and not in the queue yet, and doesn't have too many neighbours?
+static void check(const int row, const int col)
+{
+    if (grid[row][col] == ROLL && !seen[row][col] && reachable(row, col)) {
+        enqueue((Vec){row, col});
+        seen[row][col] = true;  // marked for removal = in the queue
+    }
 }
 
 int main(void)
@@ -71,7 +109,7 @@ int main(void)
     if (!f) { fprintf(stderr, "File not found: %s\n", FNAME); return 1; }
     for (int i = 1; i <= N && fgets(&grid[i][1], N + 2, f); ++i);  // +"\n\0"
     fclose(f);
-#if EXAMPLE
+#if EXAMPLE == 1
     show();
 #endif
 
@@ -79,21 +117,24 @@ int main(void)
     starttimer();
 #endif
 
-    // Part 1
-    // Count accessible rolls (=fewer than 4 neighbours)
-    printf("%d\n", forklift(false));  // example: 13, input: 1495
+    // Part 1: fill queue with all paper rolls to be removed
+    for (int i = 1; i < N + 1; ++i)
+        for (int j = 1; j < N + 1; ++j)
+            check(i, j);
+    printf("%d\n", qlen);  // example: 13, input: 1495
 
-    // Part 2
-    // Remove accessible rolls until no moves possible
-    int removed = 0, prev;
-    do {
-        prev = removed;
-        removed += forklift(true);
-    } while (removed != prev);
-#if EXAMPLE
+    // Part 2: remove, check neighbours, repeat until no more paper rolls removable
+    int removed = 0;
+    for (Vec pos; dequeue(&pos); ++removed) {
+        grid[pos.row][pos.col] = SPACE;  // forklift action
+        for (int i = -1; i < 2; ++i)
+            for (int j = -1; j < 2; ++j)
+                check(pos.row + i, pos.col + j);  // neighbours might be reachable now
+    }
+#if EXAMPLE == 1
     show();
 #endif
-    printf("%d\n", removed);  // example: 43, input: 8768
+    printf("%d\n", removed); // example: 43, input: 8768
 
 #ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
