d5b73bd7c0c1e8796eef4410c4b8d162b4a889f6
E. Dronkert <ednl@users.noreply.github.com>
2026-01-06 22:48:11 +0100

    a little faster but the algo still scuks

--- 2015/06.c
+++ 2015/06.c
@@ -5,37 +5,42 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 06.c ../startstoptimer.c
- *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 06.c ../startstoptimer.c
- * Get minimum runtime:
- *     m=999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
- * Minimum runtime:
- *     Mac Mini 2020 (M1 3.2 GHz)                       :     ? µs
- *     Macbook Air 2013 (Core i5 Haswell 4250U 1.3 GHz) :  4843 µs
- *     iMac 2013 (Core i5 Haswell 4570 3.2 GHz)         :  4895 µs
- *     Raspberry Pi 5 (2.4 GHz)                         :  5343 µs
- *     Raspberry Pi 4 (1.8 GHz)                         : 50957 µs
+ *    cc -std=c17 -Wall -Wextra -pedantic 06.c
+ * Enable timer:
+ *    cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 06.c
+ * Get minimum runtime from timer output in bash:
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 1052 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :    ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      :    ? µs
  */
 
 #include <stdio.h>
 #include <stdint.h>
-#include "../startstoptimer.h"
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
+#define FNAME "../aocinput/2015-06-input.txt"
+#define FSIZE 9531
 #define N 1000
 
 typedef struct range {
     int x0, y0, x1, y1;
 } Range;
 
-static int8_t lights1[N][N];
-static int8_t lights2[N][N];
+static char input[FSIZE];
+static const char *const end = input + FSIZE;
+static int_fast8_t lights1[N][N];
+static int_fast8_t lights2[N][N];
 
 static int8_t notneg(const int8_t a)
 {
     return a > 0 ? a : 0;
 }
 
-static int readnum(const char** s)
+static int readnum(const char **s)
 {
     int n = 0;
     while (**s > ',')  // until ',' or ' ' or '\n'
@@ -57,16 +62,20 @@ static Range parse(const char *s)
 
 int main(void)
 {
+    FILE *f = fopen(FNAME, "rb");  // fread requires binary mode
+    if (!f) { fprintf(stderr, "File not found: %s\n", FNAME); return 1; }
+    fread(input, sizeof input, 1, f);  // read whole file at once
+    fclose(f);
+
+#ifdef TIMER
     starttimer();
-    FILE *f = fopen("../aocinput/2015-06-input.txt", "r");
-    if (!f) { fputs("File not found.\n", stderr); return 1; }
+#endif
 
     Range r;
-    char buf[64];
-    while (fgets(buf, sizeof buf, f))
-        switch (buf[6]) {  // first unique char for each action
+    for (const char *c = input; c != end; ++c) {
+        switch (*(c + 6)) {  // first unique char for each action
             case 'n':  // turn on
-                r = parse(buf + 8);
+                r = parse(c + 8);
                 for (int i = r.x0; i < r.x1; ++i)
                     for (int j = r.y0; j < r.y1; ++j) {
                         lights1[i][j] = 1;
@@ -74,7 +83,7 @@ int main(void)
                     }
                 break;
             case 'f':  // turn off
-                r = parse(buf + 9);
+                r = parse(c + 9);
                 for (int i = r.x0; i < r.x1; ++i)
                     for (int j = r.y0; j < r.y1; ++j) {
                         lights1[i][j] = 0;
@@ -82,7 +91,7 @@ int main(void)
                     }
                 break;
             case ' ':  // toggle
-                r = parse(buf + 7);
+                r = parse(c + 7);
                 for (int i = r.x0; i < r.x1; ++i)
                     for (int j = r.y0; j < r.y1; ++j) {
                         lights1[i][j] ^= 1;
@@ -90,15 +99,17 @@ int main(void)
                     }
                 break;
         }
-    fclose(f);
+        for (c += 26; *c != '\n'; ++c);  // shortest line is 26 chars + newline
+    }
 
     int sum1 = 0, sum2 = 0;
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j) {
-            sum1 += lights1[i][j];
-            sum2 += lights2[i][j];
-        }
+    for (int i = 0; i < N * N; ++i) {
+        sum1 += *((int_fast8_t *)lights1 + i);
+        sum2 += *((int_fast8_t *)lights2 + i);
+    }
     printf("%d %d\n", sum1, sum2);  // 377891 14110788
+
+#ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
-    return 0;
+#endif
 }
