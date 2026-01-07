a4814f827c5cdafd971a1f5e3a9c48ae0e8792f0
E. Dronkert <ednl@users.noreply.github.com>
2026-01-07 09:34:14 +0100

    timing

--- 2015/07.c
+++ 2015/07.c
@@ -5,23 +5,27 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 07.c ../startstoptimer.c
- *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 07.c ../startstoptimer.c
- * Get minimum runtime:
- *     m=999999;for((i=0;i<2000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
- * Minimum runtime:
- *     Mac Mini 2020 (M1 3.2 GHz)          :  30 µs
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) :  67 µs
- *     Raspberry Pi 5 (2.4 GHz)            :  47 µs
- *     Raspberry Pi 4 (1.8 GHz)            : 129 µs
+ *    cc -std=c17 -Wall -Wextra -pedantic 07.c
+ * Enable timer:
+ *    cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 07.c
+ * Get minimum runtime from timer output in bash:
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  5.75 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  ?    µs
+ *     Raspberry Pi 5 (2.4 GHz)      :  ?    µs
  */
 
 #include <stdio.h>
+#include <string.h>  // memset
 #include <stdint.h>  // uint16_t
 #include <stdbool.h>
-#include <string.h>  // memset
-#include "../startstoptimer.h"
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
+#define FNAME "../aocinput/2015-07-input.txt"
+#define FSIZE 5303
 #define CHARVAL(C) ((C) - 'a' + 1)  // +1 to tell apart "a" and "aa"
 #define BASE CHARVAL('z')           // 'z' - 'a' + 1 = 26 letters
 
@@ -44,12 +48,14 @@ typedef struct wire {
     bool iswire[2];   // whether arg[i] is another wire or a direct value
 } Wire;
 
+static char input[FSIZE];
+static const char *const end = input + FSIZE;
 static Wire wire[N];
 static bool incache[N];  // separate array for easy reset
 
 // Convert wire name to array index while advancing char pointer
 // E.g.: a=1, aa=27, zz=702 (max. index for my input: ma=339)
-static uint16_t wireindex(char *wirename[])
+static uint16_t wireindex(const char **wirename)
 {
     int id = CHARVAL(*(*wirename)++);
     if (**wirename >= 'a' && **wirename <= 'z')
@@ -59,7 +65,7 @@ static uint16_t wireindex(char *wirename[])
 
 // Read unsigned int value from string while advancing char pointer
 // Pre: **digits is a valid digit 0..9
-static uint16_t parseint(char *digits[])
+static uint16_t parseint(const char **digits)
 {
     int n = *(*digits)++ - '0';
     while (**digits >= '0' && **digits <= '9')
@@ -84,46 +90,46 @@ static uint16_t eval(const uint16_t index)
         case SHL: x <<= y; break;
         case SHR: x >>= y; break;
     }
-    w->val = x;  // save as shortcut for next evaluation of same wire
     incache[index] = true;
-    return x;
+    return (w->val = x);  // save as shortcut for next evaluation of same wire
 }
 
 int main(void)
 {
+    FILE *f = fopen(FNAME, "rb");  // fread requires binary mode
+    if (!f) { fprintf(stderr, "File not found: %s\n", FNAME); return 1; }
+    fread(input, sizeof input, 1, f);  // read whole file at once
+    fclose(f);
+
+#ifdef TIMER
     starttimer();
-    FILE *f = fopen("../aocinput/2015-07-input.txt", "r");
-    if (!f) return 1;
+#endif
 
     // Parse input file
-    char line[32];  // max line length in my input = 18 (+'\n\0' = 20)
-    while (fgets(line, sizeof line, f)) {  // read with newline
+    for (const char *c = input; c != end; ++c) {  // skip newline
         Wire w = {0};  // .func = EQ, .val = 0, .arg[] = 0, .iswire[] = false
         uint16_t args = 0;  // argument count
-        char *s = line;
-        while (*s) {
-            if (*s >= 'a' && *s <= 'z') {         // argument is a wire name
+        for (;; ++c) {  // skip space
+            if (*c >= 'a' && *c <= 'z') {         // argument is a wire name
                 w.iswire[args] = true;
-                w.arg[args++] = wireindex(&s);
-            } else if (*s >= '0' && *s <= '9') {  // argument is a number
-                w.arg[args++] = parseint(&s);     // already set: .iswire[] = false
-            } else if (*s >= 'A' && *s <= 'Z') {  // input function name
-                switch (*s) {
-                    case 'N': w.func = NOT; break;
-                    case 'A': w.func = AND; break;
-                    case 'O': w.func = OR;  break;
-                    case 'L': w.func = SHL; break;
-                    case 'R': w.func = SHR; break;
+                w.arg[args++] = wireindex(&c);
+            } else if (*c >= '0' && *c <= '9') {  // argument is a number
+                w.arg[args++] = parseint(&c);     // already set: .iswire[] = false
+            } else if (*c >= 'A' && *c <= 'Z') {  // input function name
+                switch (*c) {
+                    case 'N': w.func = NOT; c += 3; break;
+                    case 'A': w.func = AND; c += 3; break;
+                    case 'O': w.func = OR;  c += 2; break;
+                    case 'L': w.func = SHL; c += 3; break;
+                    case 'R': w.func = SHR; c += 3; break;
                 }
-                while (*s != ' ') ++s;  // skip rest of function name
-            } else if (*s == '-') {
-                s += 3;  // skip arrow and space (patch / direct assignment already set: EQ=0)
-                wire[wireindex(&s)] = w;
+            } else if (*c == '-') {
+                c += 3;  // skip arrow and space (patch / direct assignment already set: EQ=0)
+                wire[wireindex(&c)] = w;
+                break;  // *c=='\n': EOL
             }
-            ++s;  // skip space or newline
         }
     }
-    fclose(f);
 
     // Part 1
     const uint16_t a = eval(CHARVAL('a'));
@@ -134,6 +140,7 @@ int main(void)
     wire[CHARVAL('b')] = (Wire){.arg[0] = a};    // set rule: "<value_from_part1> -> b"
     printf("Part 2: %u\n", eval(CHARVAL('a')));  // 14134
 
-    printf("Time: %.0f us\n", stoptimer_us());
-    return 0;
+#ifdef TIMER
+    printf("Time: %.0f ns\n", stoptimer_ns());
+#endif
 }
