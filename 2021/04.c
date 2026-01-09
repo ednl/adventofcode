840ace69c7291235728b4560faacfb20e6298e1e
E. Dronkert <ednl@users.noreply.github.com>
2026-01-09 11:43:22 +0100

    switch to static data

--- 2021/04.c
+++ 2021/04.c
@@ -1,108 +1,108 @@
+/**
+ * Advent of Code 2021
+ * Day 4: Giant Squid
+ * https://adventofcode.com/2021/day/4
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *    cc -std=c17 -Wall -Wextra -pedantic 04.c
+ * Enable timer:
+ *    cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 04.c
+ * Get minimum runtime from timer output in bash:
+ *     m=999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  7.71 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :     ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 29.5  µs
+ */
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include <stdbool.h>
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
-#define DRAW 100  // how many numbers drawn, range 0..DRAW-1
-#define CARD 100  // number of cards in the game
-#define DIM    5  // number of rows and columns on every card
+#define FNAME "../aocinput/2021-04-input.txt"
+#define FSIZE 7890 // input file size in bytes
+static char input[FSIZE];
 
-static unsigned char draw[DRAW] = {0};            // numbers drawn
-static unsigned char card[CARD][DIM][DIM] = {0};  // bingo cards
-static unsigned char crossrow[CARD][DIM] = {0};   // count crosses in rows
-static unsigned char crosscol[CARD][DIM] = {0};   // count crosses in cols
+#define CALL 100  // numbers called
+#define CARD 100  // number of bingo cards
+#define SIZE 5    // bingo cards are 5x5
+#define MULT 40   // each number called appears on how many cards max (my input: 13..37)
 
-// Reverse index to quickly locate a number on any card
-// For every number, a (single linked) list keeps track of where it is on which cards
-typedef struct Index {
-    struct Index *next;
-    unsigned char card, row, col;
-} Index;
-static Index *index[DRAW] = {0};
+// Location of a number on a card by row/col
+typedef struct pos {
+    int card, row, col;
+} Pos;
+
+static int call[CALL];              // 100 numbers called
+static int card[CARD][SIZE][SIZE];  // 100 bingo cards of size 5x5
+static int markrow[CARD][SIZE];     // count of marked numbers for every 5 rows per card
+static int markcol[CARD][SIZE];     // count of marked numbers for every 5 columns per card
+static int mult[CALL];              // each number called is on how many cards ("multiplicity")
+static Pos pos[CALL][MULT];         // called numbers are where
+static bool bingo[CARD];            // did a card get bingo yet?
+
+static int score(const int cardindex, const int lastdrawn)
+{
+    int sum = 0;
+    for (int i = 0; i < SIZE; ++i)
+        for (int j = 0; j < SIZE; ++j)
+            sum += card[cardindex][i][j];
+    return sum * lastdrawn;
+}
 
 int main(void)
 {
+    FILE *f = fopen(FNAME, "rb");  // fread requires binary mode
+    if (!f) { fprintf(stderr, "File not found: %s\n", FNAME); return 1; }
+    fread(input, sizeof input, 1, f);  // read whole file at once
+    fclose(f);
+
 #ifdef TIMER
     starttimer();
 #endif
 
-    FILE *f = fopen("../aocinput/2021-04-input.txt", "r");
-    unsigned int i = 0;
-    unsigned char a;
-    if (fscanf(f, "%hhu", &a) == 1)
-        draw[i++] = a;  // first number drawn
-    while (i < DRAW && fscanf(f, ",%hhu", &a) == 1)
-        draw[i++] = a;  // 99 more numbers drawn with a comma separator
-
-    unsigned char b = 0, r = 0, c = 0;  // b = bingo card, r = row, c = column
-    Index *p, *q;
-    while (b < CARD && fscanf(f, " %hhu", &a) == 1) {  // read next number with any whitespace separation
-        // Fill in the cards
-        card[b][r][c] = a;
-
-        // Enter a reference in the reverse index
-        p = malloc(sizeof(Index));
-        *p = (Index){NULL, b, r, c};
-        if ((q = index[a])) {  // already an entry for this number?
-            while (q->next)
-                q = q->next;
-            q->next = p;  // add to end of list (order not important)
-        } else {
-            index[a] = p;  // first entry into the array of linked list base pointers
-        }
-
-        // Next position on the card, or next card
-        if (++c == DIM) {
-            c = 0;
-            if (++r == DIM) {
-                r = 0;
-                ++b;
-            }
-        }
+    // Parse input
+    const char *c = input;
+    for (int i = 0; i < CARD; ++i, ++c) {
+        int x = *c++ & 15;
+        if (*c >= '0')
+            x = x * 10 + (*c++ & 15);
+        call[i] = x;
     }
-    fclose(f);
-
-    unsigned char wins = 0;   // total number of cards that won
-    bool won[CARD] = {false};
-    for (i = 0; i < DRAW; ++i) {
-        p = index[draw[i]];  // first entry in reverse index of number drawn
-        while (p) {          // as long as there are entries
-            // Only process cards that have not yet won
-            if (!won[p->card]) {
-                // Cross out = set number on card to 100 (bigger than any number used)
-                card[p->card][p->row][p->col] = DRAW;
-                crossrow[p->card][p->row]++;  // can't do this in the "if" because of short-circuit evaluation
-                crosscol[p->card][p->col]++;
-                // Full row or column?
-                if (crossrow[p->card][p->row] == DIM || crosscol[p->card][p->col] == DIM) {
-                    ++wins;
-                    won[p->card] = true;
-                    // Calculate score for first and last wins
-                    if (wins == 1 || wins == DRAW) {
-                        unsigned int cardsum = 0;
-                        for (unsigned int j = 0; j < DIM; ++j)
-                            for (unsigned int k = 0; k < DIM; ++k)
-                                if (card[p->card][j][k] < DRAW)  // not crossed out?
-                                    cardsum += card[p->card][j][k];
-                        // Part 1: 28082
-                        // Part 2: 8224
-                        printf("Part %d: %u\n", 1 + (wins == DRAW), draw[i] * cardsum);
-                    }
-                }
+    for (int k = 0; k < CARD; ++k) {  // 100 cards
+        c++;  // blank line
+        for (int i = 0; i < SIZE; ++i)  // 5 rows
+            for (int j = 0; j < SIZE; ++j, c += 3) {  // 5 cols
+                const int x = (*c & 15) * 10 + (*(c + 1) & 15);
+                card[k][i][j] = (uint8_t)x;
+                pos[x][mult[x]++] = (Pos){k, i, j};
             }
-            p = p->next;  // next entry in reverse index of number drawn
-        }
     }
 
-    // Free allocated memory
-    for (i = 0; i < DRAW; ++i) {
-        p = index[i];
-        while (p) {
-            q = p->next;
-            free(p);
-            p = q;
+    // Call out numbers until all cards have bingo
+    int bingocount = 0;
+    for (int i = 0; bingocount < CARD; ++i) {
+        const int n = call[i];
+        const int count = mult[n];
+        // Mark on every card where this number appears
+        for (int j = 0; j < count; ++j) {
+            const Pos *const p = &pos[n][j];  // convenience pointer
+            const int k = p->card;
+            const int r = p->row;
+            const int c = p->col;
+            card[k][r][c] = 0;  // called numbers don't count in the score
+            markrow[k][r]++;    // one more number called in this row
+            markcol[k][c]++;    // one more number called in this column
+            if (!bingo[k] && (markrow[k][r] == SIZE || markcol[k][c] == SIZE)) {
+                bingo[k] = true;
+                ++bingocount;
+                if (bingocount == 1 || bingocount == CARD)
+                    printf("%d\n", score(k, n));  // 28082 8224
+            }
         }
     }
 
@@ -110,4 +110,3 @@ int main(void)
     printf("Time: %.0f ns\n", stoptimer_ns());
 #endif
 }
-
