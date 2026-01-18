b2a4635c8fe8baac5c4b3b5c9ca0e08b8b84ddc7
E. Dronkert <ednl@users.noreply.github.com>
2026-01-18 10:58:28 +0100

    py timing

--- 2015/18.py
+++ 2015/18.py
@@ -1,5 +1,10 @@
+from time import monotonic_ns
+t0 = monotonic_ns()
+
 import numpy as np
 from scipy.signal import convolve2d
+
+t1 = monotonic_ns()
 
 with open('../aocinput/2015-18-input.txt') as f:
     data = (np.array([list(i.strip()) for i in f]) == '#').astype(np.uint8)
@@ -7,12 +12,16 @@ with open('../aocinput/2015-18-input.txt') as f:
 # Leave centre bit on, so change neighbour check from 3,2 to 4,3
 kernel = np.ones((3, 3), dtype=np.uint8)
 
+t2 = monotonic_ns()
+
 # Part 1
 state = np.copy(data)
 for _ in range(100):
     nb = convolve2d(state, kernel, mode='same')
     state = state & (nb == 4) | (nb == 3)
-print(np.sum(state))
+print(np.sum(state))  # 821
+
+t3 = monotonic_ns()
 
 # Part 2
 state = np.copy(data)
@@ -21,4 +30,13 @@ for _ in range(100):
     nb = convolve2d(state, kernel, mode='same')
     state = state & (nb == 4) | (nb == 3)
     state[0, 0] = state[0, 99] = state[99, 0] = state[99, 99] = 1
-print(np.sum(state))
+print(np.sum(state))  # 886
+
+t4 = monotonic_ns()
+print()
+print('Import     : {:4.0f} ms'.format((t1 - t0) / 1_000_000))
+print('Read+parse : {:4.0f} ms'.format((t2 - t1) / 1_000_000))
+print('Part 1     : {:4.0f} ms'.format((t3 - t2) / 1_000_000))
+print('Part 2     : {:4.0f} ms'.format((t4 - t3) / 1_000_000))
+print('All        : {:4.0f} ms'.format((t4 - t0) / 1_000_000))
+print('No import  : {:4.0f} ms'.format((t4 - t1) / 1_000_000))
