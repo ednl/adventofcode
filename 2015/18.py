621e20afdac6c547441286b39a4a3db97feb2c22
E. Dronkert <ednl@users.noreply.github.com>
2026-01-18 11:11:11 +0100

    generalise

--- 2015/18.py
+++ 2015/18.py
@@ -6,6 +6,11 @@ from scipy.signal import convolve2d
 
 t1 = monotonic_ns()
 
+# From puzzle description
+STEP = 100
+SIZE = 100
+MAX = SIZE - 1  # zero-based array
+
 with open('../aocinput/2015-18-input.txt') as f:
     data = (np.array([list(i.strip()) for i in f]) == '#').astype(np.uint8)
 
@@ -16,7 +21,7 @@ t2 = monotonic_ns()
 
 # Part 1
 state = np.copy(data)
-for _ in range(100):
+for _ in range(STEP):
     nb = convolve2d(state, kernel, mode='same')
     state = state & (nb == 4) | (nb == 3)
 print(np.sum(state))  # 821
@@ -25,11 +30,11 @@ t3 = monotonic_ns()
 
 # Part 2
 state = np.copy(data)
-state[0, 0] = state[0, 99] = state[99, 0] = state[99, 99] = 1
-for _ in range(100):
+state[0, 0] = state[0, MAX] = state[MAX, 0] = state[MAX, MAX] = 1
+for _ in range(STEP):
     nb = convolve2d(state, kernel, mode='same')
     state = state & (nb == 4) | (nb == 3)
-    state[0, 0] = state[0, 99] = state[99, 0] = state[99, 99] = 1
+    state[0, 0] = state[0, MAX] = state[MAX, 0] = state[MAX, MAX] = 1
 print(np.sum(state))  # 886
 
 t4 = monotonic_ns()
