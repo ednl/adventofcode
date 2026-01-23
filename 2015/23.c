8202db7edbeb4122d4e4606cc0d10a2a3b8d919d
E. Dronkert <ednl@users.noreply.github.com>
2026-01-23 14:14:55 +0100

    comments

--- 2015/23.c
+++ 2015/23.c
@@ -16,17 +16,17 @@
  *     Raspberry Pi 5 (2.4 GHz)      : 15.2  µs
  */
 
-#include <stdio.h>     // fopen, getline, printf
-#include <stdlib.h>    // atoi, free
-#include <stdint.h>    // int64_t
+#include <stdio.h>
+#include <stdint.h>  // uint32_t, uint64_t
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
-#define VALIDATE 0
-#define MEMSIZE 64
+#define VALIDATE 0  // change to 1 for a direct Collatz computation
+#define MEMSIZE 64  // needed for my input: 48
+#define BUFSIZE 16  // needed for my input: 12
 
-// JIO = jump if one, JIE = jump if even
+// JIO = jump if ONE (not odd), JIE = jump if even
 typedef enum opcode {
     INCB, INC, HLF, TPL, JMP, JIO, JIE
 } OpCode;
@@ -45,48 +45,47 @@ static int parse(const char *fname)
     FILE *f = fopen(fname, "r");
     if (!f)
         return 0;
+    char buf[BUFSIZE];
     int n = 0;
-    char *line = NULL;
-    size_t size;
-    while (n < MEMSIZE && getline(&line, &size, f) > 1) {
+    while (n < MEMSIZE && fgets(buf, sizeof buf, f)) {
         OpCode op;
-        switch (line[2]) {
-            case 'c': op = line[4] == 'a' ? INC : INCB; break;
+        switch (buf[2]) {
+            case 'c': op = buf[4] == 'a' ? INC : INCB; break;
             case 'e': op = JIE; break;
             case 'f': op = HLF; break;
             case 'l': op = TPL; break;
             case 'o': op = JIO; break;
             case 'p': op = JMP; break;
-            default: return 0;
+            default: return 0;  // illegal instruction
         }
         int jmp;
-        // -1 to compensate for ip++
         switch (op) {
-            case JMP: jmp = atoi(&line[4]) - 1; break;
+            case JMP: jmp = atoi(&buf[4]) - 1; break;  // -1 to compensate for ip++
             case JIO: /* fallthrough */
-            case JIE: jmp = atoi(&line[7]) - 1; break;
-            default : jmp = 0;
+            case JIE: jmp = atoi(&buf[7]) - 1; break;
+            default : jmp = 0;  // keep compiler happy because op is enum
         }
         mem[n++] = (Instr){op, jmp};
     }
     fclose(f);
-    free(line);
     return n;
 }
 
-static int run(uint64_t a)
+// For my input, the value in register A never exceeds 30 bits
+static int run(uint32_t a)
 {
-    int b = 0;
+    int b = 0;  // counter of Collatz steps
+    // Assume program is well-formed and reaches 'end' exactly
     const Instr *const end = mem + progsize;
     for (const Instr *ip = mem; ip != end; ++ip)
         switch (ip->op) {
-            case INCB: b++;     break;
-            case INC : a++;     break;
-            case HLF : a >>= 1; break;
-            case TPL : a *= 3;  break;
-            case JMP : ip += ip->jmp; break;
-            case JIO : if (a == 1)   ip += ip->jmp; break;
-            case JIE : if (!(a & 1)) ip += ip->jmp; break;
+            case INCB: b++;                         break;  // increment register B
+            case INC : a++;                         break;  // increment register A
+            case HLF : a >>= 1;                     break;  // halve register A
+            case TPL : a *= 3;                      break;  // triple register A
+            case JMP : ip += ip->jmp;               break;  // unconditional jump
+            case JIO : if (a == 1) ip += ip->jmp;   break;  // jump if register A is one (not odd!)
+            case JIE : if (!(a & 1)) ip += ip->jmp; break;  // jump if register A is even
         }
     return b;
 }
