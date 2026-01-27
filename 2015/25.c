d2ce3f9012ff35381d2f5cd8c67ba86291adf1f3
E. Dronkert <ednl@users.noreply.github.com>
2026-01-27 03:18:41 +0100

    custom printf

--- 2015/25.c
+++ 2015/25.c
@@ -11,63 +11,53 @@
  * Get minimum runtime from timer output in bash:
  *     m=9999999;for((i=0;i<10000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 0.63 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 0.46 µs
  *     Mac Mini 2020 (M1 3.2 GHz)    : 1.08 µs
  *     Raspberry Pi 5 (2.4 GHz)      : 5.17 µs
  */
 
-#include <stdio.h>
+#include <stdio.h>     // fopen, fclose, FILE
+#include <unistd.h>    // write, STDOUT_FILENO
+#include <stdlib.h>    // div, div_t
 #include <stdint.h>    // uint64_t, UINT64_C
-#include <inttypes.h>  // PRIu64
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
+// Input file
 #define FNAME "../aocinput/2015-25-input.txt"
-#define FSIZE 128
+#define FSIZE 100
+#define ROW 80
+#define COL 93
 static char input[FSIZE];
 
 // From puzzle description
 // Ref. https://adventofcode.com/2015/day/25
-#define VAL 20151125  // starting value at grid pos (1,1) = index 0
-#define MUL 252533    // multiplier (base of the modular exponentiation)
-#define MOD 33554393  // modulus of the modular exponentiation
+#define MUL UINT64_C(252533)    // multiplier (base of the modular exponentiation)
+#define VAL UINT64_C(20151125)  // starting value at triangle pos (1,1) = index 0
+#define MOD UINT64_C(33554393)  // modulus of the modular exponentiation
 
-// Skip to next digit, read unsigned int, advance char pointer
-static int readnum(const char **s)
+// Read unsigned int, minus 1 because row/col are one-based
+static unsigned readnum(const char *s)
 {
-    int x = 0;
-    for (; **s && (**s < '0' || **s > '9'); ++(*s));
-    for (; **s >= '0' && **s <= '9'; ++(*s))
-        x = x * 10 + (**s & 15);
-    return x;
+    unsigned x = 0;
+    while (*s >= '0')
+        x = x * 10 + (*s++ & 15);
+    return x - 1;
 }
 
-// In the puzzle, row/col are one-based
-static int getindex(const int row, const int col)
+static void printint(unsigned x)
 {
-    const int tri = row + col - 2;  // triangle base is zero-based
-    return col - 1 + tri * (tri + 1) / 2;  // number of spots in de triangle
+    char buf[sizeof x * 4], *end = buf + sizeof buf, *pc = end;
+    *--pc = '\n';
+	do {
+        const div_t qr = div(x, 10);
+		*--pc = '0' + qr.rem;  // remainder of negative number is negative
+		x = qr.quot;
+	} while (x);
+    for (ssize_t n; pc < end && (n = write(STDOUT_FILENO, pc, end - pc)) != -1; pc += n);
 }
 
-// Modular exponentiation: calculate remainder r = b^e mod m
-// Ref. https://en.wikipedia.org/wiki/Modular_exponentiation
-// Return u64 instead of u32 to avoid overflow in next calculation
-static uint64_t modpow(uint64_t base, uint64_t exponent, const uint64_t modulus)
-{
-    if (modulus == 1)
-        return 0;  // shortcut
-    if (modulus == 0 || modulus > (UINT64_C(1) << 32))
-        return -1;  // error (but also maximum unsigned value)
-    uint64_t rem = 1;
-    base %= modulus;  // now: base <= modulus-1
-    for (; exponent; exponent >>= 1) {
-        if (exponent & 1)  // current LSB set?
-            rem = (rem * base) % modulus;  // parentheses for clarity
-        base = (base * base) % modulus;  // overflow if base >= 1<<32
-    }
-    return rem;  // 0 <= rem < 1<<32 because modulus <= 1<<32
-}
 
 int main(void)
 {
@@ -82,13 +72,19 @@ int main(void)
 #endif
 
     // Parse input
-    const char *c = input;
-    const int row = readnum(&c);
-    const int col = readnum(&c);
+    const uint64_t row = readnum(input + ROW);
+    const uint64_t col = readnum(input + COL);
 
-    // Part 1
-    const int pos = getindex(row, col);  // destination pos (exponent of the mod. exp.)
-    printf("%"PRIu64"\n", modpow(MUL, pos, MOD) * VAL % MOD);  // 19980801
+    // https://en.wikipedia.org/wiki/Modular_exponentiation
+    const uint64_t tri = row + col;
+    uint64_t exp = col + (tri * (tri + 1) >> 1);  // index into the triangle
+    uint64_t rem = VAL, base = MUL;
+    for (; exp; exp >>= 1) {
+        if (exp & 1)
+            rem = rem * base % MOD;
+        base = base * base % MOD;
+    }
+    printint(rem);  // 19980801
 
 #ifdef TIMER
     printf("Time: %.0f ns\n", stoptimer_ns());
