ffcdd66a51cb40834ac03584507efcf4147e57ab
E. Dronkert <ednl@users.noreply.github.com>
2026-01-27 12:41:36 +0100

    include timing in main file

--- 2015/25.c
+++ 2015/25.c
@@ -6,33 +6,37 @@
  *
  * Compile with warnings:
  *     cc -std=c17 -Wall -Wextra -pedantic 25.c
- * Enable timer with optimisations:
- *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 25.c
+ * Compile for speed:
+ *     cc -O3 -march=native -mtune=native 25.c
  * Run program:
  *     ./a.out                  read input file from internal file name
  *     ./a.out < input.txt      read input file using redirected input
  *     cat input.txt | ./a.out  read input file using piped input
  * Get minimum runtime from timer output in Bash:
  *     m=9999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ *     (optionally replace './a.out' with 2nd or 3rd run command above)
  * Minimum runtime measurements:
  *     Macbook Pro 2024 (M4 4.4 GHz) : 0.417 µs
  *     Mac Mini 2020 (M1 3.2 GHz)    :     ? µs
  *     Raspberry Pi 5 (2.4 GHz)      : 2.96  µs
  */
 
-#include <stdio.h>   // fopen, fclose, fread, FILE
-#include <unistd.h>  // isatty, fileno, write, STDOUT_FILENO
-#include <stdlib.h>  // div, div_t
-#include <stdint.h>  // uint64_t, UINT64_C
-#ifdef TIMER
-    #include "../startstoptimer.h"
+#include <stdio.h>     // fopen, fclose, fread, FILE
+#include <unistd.h>    // isatty, fileno, write, STDOUT_FILENO
+#include <stdlib.h>    // div, div_t
+#include <stdint.h>    // uint64_t, UINT64_C, int64_t, INT64_C
+#include <inttypes.h>  // PRId64
+#include <time.h>      // clock_gettime, struct timespec, CLOCK_MONOTONIC/_RAW
+
+#ifndef CLOCK_MONOTONIC_RAW
+#define CLOCK_MONOTONIC_RAW CLOCK_MONOTONIC
 #endif
 
 // Input file
 #define FNAME "../aocinput/2015-25-input.txt"
-#define FSIZE 100      // input file size in bytes, assumes 2x 4-digit numbers
-#define ROW   80       // index of start of row number in input file
-#define COL  (89 + 4)  // same for col, assumes row has 4 digits
+#define FSIZE 100  // input file size in bytes, assumes 2x 4-digit numbers
+#define ROW 80  // index of start of row number in input file
+#define COL 93  // same for col, assumes row number has 4 digits
 
 // From puzzle description
 // Ref. https://adventofcode.com/2015/day/25
@@ -41,6 +45,28 @@
 #define MOD UINT64_C(33554393)  // modulus of the modular exponentiation
 
 static char input[FSIZE];
+static struct timespec t0, t1;
+
+static void starttimer(void)
+{
+    // Warn on Raspberry Pi if not running at max performance
+    FILE *f = fopen("/sys/devices/system/cpu/cpufreq/policy0/scaling_governor", "r");
+    if (f) {
+        if (fgetc(f) != 'p' || fgetc(f) != 'e')
+            fprintf(stderr,
+                "Warning: CPU not optimised for performance.\n"
+                "  Resolve: echo performance | sudo tee /sys/devices/system/cpu/cpufreq/policy0/scaling_governor\n"
+                "  Setting will be restored to default 'ondemand' at reboot.\n");
+        fclose(f);
+    }
+    clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+}
+
+static int64_t stoptimer_ns(void)
+{
+    clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+    return INT64_C(1000000000) * (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec);
+}
 
 // Read unsigned int, minus 1 because row/col are one-based
 static unsigned readnum(const char *s)
@@ -64,7 +90,6 @@ static void printint(unsigned x)
     for (ssize_t n; pc < end && (n = write(STDOUT_FILENO, pc, end - pc)) != -1; pc += n);
 }
 
-
 int main(void)
 {
     if (isatty(fileno(stdin))) {
@@ -77,9 +102,7 @@ int main(void)
         // Read input or example file from pipe or redirected stdin
         fread(input, sizeof input, 1, stdin);
 
-#ifdef TIMER
     starttimer();
-#endif
 
     // Parse input
     const unsigned row = readnum(input + ROW);
@@ -97,8 +120,5 @@ int main(void)
         base = base * base % MOD;
     }
     printint(rem);  // 19980801
-
-#ifdef TIMER
-    printf("Time: %.0f ns\n", stoptimer_ns());
-#endif
+    printf("Time: %"PRId64" ns\n", stoptimer_ns());
 }
