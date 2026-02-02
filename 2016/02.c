f0be61544a6a9a3b4456afe652a96a6aaa76480e
E. Dronkert <ednl@users.noreply.github.com>
2026-02-02 05:10:06 +0100

    new version

--- 2016/02.c
+++ 2016/02.c
@@ -1,48 +1,110 @@
-#include <stdio.h>
+/**
+ * Advent of Code 2016
+ * Day 2: Bathroom Security
+ * https://adventofcode.com/2016/day/2
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile with warnings:
+ *     cc -std=c17 -Wall -Wextra -pedantic 02.c
+ * Compile for speed, with timer:
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 02.c
+ * Get minimum runtime from timer output in bash:
+ *     m=9999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements including result output which is redirected to /dev/null in shell:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  3.61 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :     ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 12.0  µs
+ */
 
-static const int pad1[3][3] = {{1,4,7},{2,5,8},{3,6,9}};
-static const int pad2[5][5] = {{0,0,5,0,0},{0,2,6,10,0},{1,3,7,11,13},{0,4,8,12,0},{0,0,9,0,0}};
+#include <stdio.h>
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
+
+#define FNAME "../aocinput/2016-02-input.txt"
+#define FSIZE 2481
+
+static char input[FSIZE];
+static const char *const end = input + FSIZE;
+
+typedef struct vec2 {
+    int x, y;
+} Vec2;
+
+// Must be same order as Vec2 step[]
+typedef enum dir {
+    UP = 0, DOWN, LEFT, RIGHT
+} Dir;
+
+// Must be same order as enum dir
+static const Vec2 step[] = {{0,-1},{0,1},{-1,0},{1,0}};  // UDLR (y=-y)
+
+// Keypads for part 1 and 2 from puzzle description
+// with zero-padding on all sides
+static const int pad[2][7][7] = {{
+    {0},
+    {0},
+    {0, 0, 1, 2, 3},
+    {0, 0, 4, 5, 6},
+    {0, 0, 7, 8, 9},
+    {0},
+    {0}},{
+    {0},
+    {0, 0, 0, 1},
+    {0, 0, 2, 3, 4},
+    {0, 5, 6, 7, 8, 9},
+    {0, 0,10,11,12},
+    {0, 0, 0,13},
+    {0}}};
+
+static Vec2 pos[2];
+
+static Vec2 add(const Vec2 a, const Vec2 b)
+{
+    return (Vec2){a.x + b.x, a.y + b.y};
+}
+
+static void move(const Dir dir)
+{
+    for (int i = 0; i < 2; ++i) {
+        const Vec2 tmp = add(pos[i], step[dir]);
+        if (pad[i][tmp.y][tmp.x])
+            pos[i] = tmp;
+    }
+}
 
 int main(void)
 {
-    FILE *f = fopen("../aocinput/2016-02-input.txt", "r");
-    if (!f)
-        return 1;
-    int ch, b1 = 0, b2 = 0, x1 = 1, y1 = 1, x2 = 0, y2 = 2;
-    while ((ch = fgetc(f)) > 0) {
-        switch (ch) {
-            case 'U':
-                if (y1 > 0)
-                    --y1;
-                if (y2 > 0 && pad2[x2][y2 - 1])
-                    --y2;
-                break;
-            case 'D':
-                if (y1 < 2)
-                    ++y1;
-                if (y2 < 4 && pad2[x2][y2 + 1])
-                    ++y2;
-                break;
-            case 'L':
-                if (x1 > 0)
-                    --x1;
-                if (x2 > 0 && pad2[x2 - 1][y2])
-                    --x2;
-                break;
-            case 'R':
-                if (x1 < 2)
-                    ++x1;
-                if (x2 < 4 && pad2[x2 + 1][y2])
-                    ++x2;
-                break;
-            case 10:
-                b1 = b1 * 10 + pad1[x1][y1];
-                b2 = b2 * 16 + pad2[x2][y2];
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) return 1;
+    fread(input, sizeof input, 1, f);
+    fclose(f);
+
+#ifdef TIMER
+    starttimer();
+    for (int timingloop = 0; timingloop < 1000; ++timingloop) {
+#endif
+
+    Vec2 code = {0};
+    // Initial positions at key "5"
+    pos[0] = (Vec2){3, 3};
+    pos[1] = (Vec2){1, 3};
+    for (const char *c = input; c != end; ++c) {
+        switch (*c) {
+            case 'U': move(UP   ); break;
+            case 'D': move(DOWN ); break;
+            case 'L': move(LEFT ); break;
+            case 'R': move(RIGHT); break;
+            case '\n':  // newline: record current keypad position
+                code.x = code.x * 10 + pad[0][pos[0].y][pos[0].x];
+                code.y = code.y << 4 | pad[1][pos[1].y][pos[1].x];
                 break;
         }
     }
-    fclose(f);
-    printf("Part 1: %d\n", b1);
-    printf("Part 2: %X\n", b2);
-    return 0;
+    printf("%d %X\n", code.x, code.y);  // 95549 D87AD
+
+#ifdef TIMER
+    }
+    fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());
+#endif
 }
