c8cde3cb36d69b22d7ecdd145eff4fe30e819581
E. Dronkert <ednl@users.noreply.github.com>
2026-02-09 20:21:41 +0100

    style update

--- 2016/09.c
+++ 2016/09.c
@@ -1,17 +1,19 @@
 #include <stdio.h>   // fopen, getline, sscanf, printf, fclose
 #include <stdlib.h>  // free
+#include <stdbool.h>
+#include "../startstoptimer.h"
 
-static size_t decompress(const char *c, const char * const end, const char recurse)
+static size_t decompress(const char *beg, const char *const end, const bool recurse)
 {
     size_t size = 0;
     unsigned int len, rep;
-    while (c < end) {
-        while (c < end && *c++ != '(')
+    while (beg < end) {
+        while (beg < end && *beg++ != '(')
             ++size;
-        if (c < end && sscanf(c, "%ux%u", &len, &rep) == 2) {
-            while (*c++ != ')');  // skip over the marker
-            size += recurse ? decompress(c, c + len, 1) * rep : len * rep;
-            c += len;
+        if (beg < end && sscanf(beg, "%ux%u", &len, &rep) == 2) {
+            while (*beg++ != ')');  // skip over the marker
+            size += rep * (recurse ? decompress(beg, beg + len, true) : len);
+            beg += len;
         }
     }
     return size;
@@ -27,8 +29,10 @@ int main(void)
     ssize_t len;
     if ((len = getline(&line, &size, f)) > 0) {
         line[--len] = '\0';  // strip EOL
-        printf("Part 1: %zu\n", decompress(line, line + len, 0));  // 123908
-        printf("Part 2: %zu\n", decompress(line, line + len, 1));  // 10755693147
+        starttimer();
+        printf("Part 1: %zu\n", decompress(line, line + len, false));  // 123908
+        printf("Part 2: %zu\n", decompress(line, line + len, true));   // 10755693147
+        printf("Time: %.0f us\n", stoptimer_ms());
         free(line);
     }
     fclose(f);
