377d6bc385da5dafad06c95eccd8adf5c1b8e76d
E. Dronkert <ednl@users.noreply.github.com>
2026-02-13 05:36:06 +0100

    Fibonacci

--- 2016/12.c
+++ 2016/12.c
@@ -1,15 +1,41 @@
+/**
+ * Advent of Code 2016
+ * Day 12: Leonardo's Monorail
+ * https://adventofcode.com/2016/day/12
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile with warnings:
+ *     cc -std=c17 -Wall -Wextra -pedantic 12.c -lm
+ * Compile for speed, with timer:
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 12.c -lm
+ * Get minimum runtime from timer output in bash:
+ *     m=9999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements including result output:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  2.21 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  ?    µs
+ *     Raspberry Pi 5 (2.4 GHz)      :  ?    µs
+ */
+
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
+#include <stdint.h>    // int64_t
+#include <inttypes.h>  // PRId64
+#include <string.h>    // memset
+#include <math.h>      // pow, llround
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define MEMSIZE 64
+#define FNAME "../aocinput/2016-12-input.txt"
+#define MEMSIZE 32  // needed for my input: 23
 #define REGCOUNT 4
+#define PHI   1.618033988749894
+#define SQRT5 2.23606797749979
 
-typedef enum Op {
-    NOP, INC, DEC, STO, CPY, JMP, JNZ
+typedef enum op {
+    NOP, INC, DEC, STO, CPY, JMP, JNZ, FIB
 } Op;
 
-typedef struct Assembunny {
+typedef struct assembunny {
     Op op;
     int a, b;
 } Assembunny;
@@ -18,65 +44,79 @@ static Assembunny mem[MEMSIZE];
 static int64_t reg[REGCOUNT];
 static int memsize, ip;
 
+static int64_t fib(const int n)
+{
+    return llround(pow(PHI, n) / SQRT5);
+}
+
+// reg d is countdown for consecutive Fibonacci numbers in reg a
+// reg a goes: 1,2,3,5,... so starts with Fib[2] (skips Fib[0]=0, Fib[1]=1)
+// which means reg d starts with n-2 for n'th Fib number ending up in reg a
+// => instructions in mem[9..15] can be replaced by: a=fib(d+2), jmp +6
 static int parse(void)
 {
-    FILE *f = fopen("../aocinput/2016-12-input.txt", "r");
-    if (!f)
-        return 0;
+    FILE *f = fopen(FNAME, "r");
+    if (!f) {
+        fprintf(stderr, "File not found: "FNAME"\n");
+        return 0;  // return linecount=0
+    }
     char buf[16];
-    int i = 0, a, b;
+    int n = 0, a, b;
     char c, d;
-    while (i < MEMSIZE && fgets(buf, sizeof buf, f)) {
+    while (n < MEMSIZE && fgets(buf, sizeof buf, f)) {
         switch (buf[0]) {
             case 'i':
             case 'd':
-                mem[i++] = (Assembunny){buf[0] == 'i' ? INC : DEC, buf[4] - 'a', 0};
+                mem[n++] = (Assembunny){buf[0] == 'i' ? INC : DEC, buf[4] - 'a', 0};
                 break;
             case 'c':
                 if (buf[4] >= 'a') {
                     sscanf(&buf[4], "%c %c", &c, &d);
-                    mem[i++] = (Assembunny){CPY, c - 'a', d - 'a'};
+                    mem[n++] = (Assembunny){CPY, c - 'a', d - 'a'};
                 } else {
                     sscanf(&buf[4], "%d %c", &a, &c);
-                    mem[i++] = (Assembunny){STO, a, c - 'a'};
+                    mem[n++] = (Assembunny){STO, a, c - 'a'};
                 }
                 break;
             case 'j':
                 if (buf[4] >= 'a') {
                     sscanf(&buf[4], "%c %d", &c, &b);
-                    mem[i++] = (Assembunny){JNZ, c - 'a', b - 1};  // compensate -1 for ip incr
+                    mem[n++] = (Assembunny){JNZ, c - 'a', b};
                 } else {
                     sscanf(&buf[4], "%d %d", &a, &b);
-                    if (a)
-                        mem[i++] = (Assembunny){JMP, b - 1, 0};  // compensate -1 for ip incr
-                    else
-                        mem[i++] = (Assembunny){NOP, 0, 0};
+                    mem[n++] = a ? (Assembunny){JMP, b, 0} : (Assembunny){NOP, 0, 0};
                 }
                 break;
         }
     }
     fclose(f);
-    return i;
+    // Reverse engineered
+    mem[ 9] = (Assembunny){FIB, 'd' - 'a', 2};  // a=fib(d+2) (a is implied)
+    mem[10] = (Assembunny){JMP, 6, 0};  // jump over old Fib calculation
+    return n;
 }
 
+#if DEBUG
 static void list(void)
 {
-    static const char *ins[] = {"nop", "inc", "dec", "sto", "cpy", "jmp", "jnz"};
+    static const char *ins[] = {"nop", "inc", "dec", "sto", "cpy", "jmp", "jnz", "fib"};
     for (int i = 0; i < memsize; ++i) {
         printf("%s ", ins[mem[i].op]);
         switch (mem[i].op) {
-            case NOP:                                                  break;
-            case INC:
+            case NOP: break;
+            case INC: /* fall-through */
             case DEC: printf("%c", 'a' + mem[i].a);                    break;
             case STO: printf("%d %c", mem[i].a, 'a' + mem[i].b);       break;
             case CPY: printf("%c %c", 'a' + mem[i].a, 'a' + mem[i].b); break;
-            case JMP: printf("%d", mem[i].a + 1);                      break;
-            case JNZ: printf("%c %d", 'a' + mem[i].a, mem[i].b + 1);   break;
+            case JMP: printf("%d", mem[i].a);                          break;
+            case JNZ: printf("%c %d", 'a' + mem[i].a, mem[i].b);       break;
+            case FIB: printf("%c+%d", 'a' + mem[i].a, mem[i].b);       break;
         }
         putchar('\n');
     }
 
 }
+#endif
 
 static int64_t run(const int regc)
 {
@@ -85,24 +125,34 @@ static int64_t run(const int regc)
     ip = 0;
     while (ip < memsize) {
         switch (mem[ip].op) {
-            case NOP:                                      break;
-            case INC: ++reg[mem[ip].a];                    break;
-            case DEC: --reg[mem[ip].a];                    break;
-            case STO: reg[mem[ip].b] = mem[ip].a;          break;
-            case CPY: reg[mem[ip].b] = reg[mem[ip].a];     break;
-            case JMP: ip += mem[ip].a;                     break;
-            case JNZ: if (reg[mem[ip].a]) ip += mem[ip].b; break;
+            case NOP: ip++; break;
+            case INC: reg[mem[ip].a]++; ip++; break;
+            case DEC: reg[mem[ip].a]--; ip++; break;
+            case STO: reg[mem[ip].b] = mem[ip].a;      ip++; break;
+            case CPY: reg[mem[ip].b] = reg[mem[ip].a]; ip++; break;
+            case JMP: ip += mem[ip].a;                      break;
+            case JNZ: ip += reg[mem[ip].a] ? mem[ip].b : 1; break;
+            case FIB: reg[0] = fib(reg[mem[ip].a] + mem[ip].b); ip++; break;  // reverse engineered
         }
-        ++ip;
     }
-    return reg[0];
+    return reg[0];  // reg a
 }
 
 int main(void)
 {
     memsize = parse();
+#if DEBUG
     list();
-    printf("Part 1: %lld\n", run(0));  // 318009
-    printf("Part 2: %lld\n", run(1));  // 9227663
-    return 0;
+#endif
+
+#ifdef TIMER
+    starttimer();
+#endif
+
+    printf("%"PRId64"\n", run(0));  // 318009
+    printf("%"PRId64"\n", run(1));  // 9227663
+
+#ifdef TIMER
+    printf("Time: %.0f ns\n", stoptimer_ns());
+#endif
 }
