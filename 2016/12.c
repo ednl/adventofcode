31102db94b449c62d48912544941867af65dc20a
E. Dronkert <ednl@users.noreply.github.com>
2026-02-13 20:46:33 +0100

    timing

--- 2016/12.c
+++ 2016/12.c
@@ -15,9 +15,13 @@
  * Get minimum runtime from timer output in bash:
  *     m=9999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements with result output but without reading/parsing input file:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 2.04 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 3.17 µs
- *     Raspberry Pi 5 (2.4 GHz)      : 7.39 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  2.04 µs (same as NOFPU!)
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  3.46 µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 11.6  µs
+ * Minimum runtime measurements with result output but without reading/parsing input file, NOFPU:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  2.04 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  3.17 µs
+ *     Raspberry Pi 5 (2.4 GHz)      :  7.33 µs
  */
 
 #include <stdio.h>
