8259300778ddf7921486e88dbcd237941e4e0ec6
E. Dronkert <ednl@users.noreply.github.com>
2026-02-24 12:38:07 +0100

    style update

--- 2016/24.c
+++ 2016/24.c
@@ -1,37 +1,59 @@
+/**
+ * Advent of Code 2016
+ * Day 24: Air Duct Spelunking
+ * https://adventofcode.com/2016/day/24
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile with warnings:
+ *     cc -std=c17 -Wall -Wextra -pedantic 24.c
+ * Compile for speed, with timer:
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 24.c
+ * Get minimum runtime from timer output in bash:
+ *     m=9999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements including result output:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 284 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :   ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      :   ? µs
+ */
+
 #include <stdio.h>    // fopen, fclose, printf, putchar, puts
 #include <string.h>   // memset, strcpy
 #include <stdbool.h>  // bool
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
+#define FNAME "../aocinput/2016-24-input.txt"
 #define W 179      // width
 #define H 43       // height
 #define N 8        // points of interest 0..7
 #define FAC7 5040  // 7! = 5040
-#define QSIZE 100  // for my input: max=69 (nice)
+#define QSIZE 100  // needed for my input: max=69 (nice)
 
-typedef struct {
+typedef struct vec2 {
     int x, y;
-} vec2_t;
+} Vec2;
 
-typedef struct {
-    vec2_t pos;
+typedef struct state {
+    Vec2 pos;
     int dist;
-} state_t;
+} State;
 
-typedef struct {
+typedef struct queue {
     size_t len, pop, ins;
-    state_t q[QSIZE];
-} queue_t;
+    State q[QSIZE];
+} Queue;
 
-const static vec2_t dir[4] = {{-1,0},{1,0},{0,-1},{0,1}};
-static queue_t queue;
-static int grid[H][W];
+const static Vec2 dir[4] = {{-1,0},{1,0},{0,-1},{0,1}};
+static Queue queue;
+static char grid[H][W];
 static bool seen[H][W];
-static vec2_t poi[N];       // position of POI 0..7
+static Vec2 poi[N];       // position of POI 0..7
 static int dist[N][N];      // distance between POI i and j
 static char perm[FAC7][N];  // all permutations of "1234567" (always start at 0)
 
 // Dequeue = pop off the tail of the queue
-static bool deq(queue_t * const q, state_t * const val)
+static bool deq(Queue *const q, State *const val)
 {
     if (!q || !q->len)
         return false;
@@ -43,7 +65,7 @@ static bool deq(queue_t * const q, state_t * const val)
 }
 
 // Enqueue = push onto the head of the queue
-static bool enq(queue_t * const q, const state_t val)
+static bool enq(Queue *const q, const State val)
 {
     if (!q || q->len == QSIZE)
         return false;
@@ -54,37 +76,37 @@ static bool enq(queue_t * const q, const state_t val)
     return true;
 }
 
-static vec2_t add(const vec2_t a, const vec2_t b)
+static Vec2 add(const Vec2 a, const Vec2 b)
 {
-    return (vec2_t){a.x + b.x, a.y + b.y};
+    return (Vec2){a.x + b.x, a.y + b.y};
 }
 
 static void flood(const int start)
 {
     memset(seen, 0, sizeof seen);
     seen[poi[start].y][poi[start].x] = true;
-    state_t s = {poi[start], 0};
+    State s = {poi[start], 0};
     enq(&queue, s);
     while (deq(&queue, &s)) {
-        int c = grid[s.pos.y][s.pos.x];
+        const char c = grid[s.pos.y][s.pos.x];
         if (c >= '0' && c < '0' + N)
             dist[start][c - '0'] = dist[c - '0'][start] = s.dist;
         for (int i = 0; i < (int)(sizeof dir / sizeof *dir); ++i) {
-            vec2_t next = add(s.pos, dir[i]);
+            Vec2 next = add(s.pos, dir[i]);
             if (grid[next.y][next.x] != '#' && !seen[next.y][next.x]) {
                 seen[next.y][next.x] = true;
-                if (!enq(&queue, (state_t){next, s.dist + 1}))
+                if (!enq(&queue, (State){next, s.dist + 1}))
                     puts("***** QUEUE OVERFLOW *****");
             }
         }
     }
 }
 
-static void swap(char *a, char *b)
+static void swap(char *const a, char *const b)
 {
-    char t = *a;
+    const char tmp = *a;
     *a = *b;
-    *b = t;
+    *b = tmp;
 }
 
 static void permute(char *s, int l, int r)
@@ -101,6 +123,7 @@ static void permute(char *s, int l, int r)
         }
 }
 
+#ifdef DEBUG
 static void showdist(void)
 {
     printf("\n           |");
@@ -116,6 +139,7 @@ static void showdist(void)
     }
     putchar('\n');
 }
+#endif
 
 static void mindist(const int part)
 {
@@ -136,14 +160,17 @@ static void mindist(const int part)
     int i = 0, j = N - 2;
     while (i < j)
         swap(s + i++, s + j--);
+#ifdef DEBUG
     printf("Part %d: \"0%s%s\" = %d\n", part, s, part == 2 ? "0" : "", min);  // 490, 744
+#else
+    printf("%d\n", min);
+#endif
 }
 
 int main(void)
 {
-    FILE *f = fopen("../aocinput/2016-24-input.txt", "r");
-    if (!f)
-        return 1;
+    FILE *f = fopen(FNAME, "r");
+    if (!f) { fprintf(stderr, "File not found: "FNAME"\n"); return 1; }
     int c, x = 0, y = 0;
     while (y < H && (c = fgetc(f)) != EOF) {
         if (c == '\n') {
@@ -151,23 +178,32 @@ int main(void)
             ++y;
         } else {
             if (c >= '0' && c < '0' + N)
-                poi[c - '0'] = (vec2_t){x, y};
+                poi[c - '0'] = (Vec2){x, y};
             if (x < W)
-                grid[y][x++] = (char)c;
+                grid[y][x++] = c;
         }
     }
     fclose(f);
 
+#ifdef TIMER
+    starttimer();
+#endif
+
     for (int i = 0; i < N; ++i)
         flood(i);
+#ifdef DEBUG
     showdist();
+#endif
 
     char s[] = "1234567";
     for (int i = 0; i < N - 1; ++i)
         s[i] -= '0';
     permute(s, 0, N - 2);
 
-    mindist(1);
-    mindist(2);
-    return 0;
+    mindist(1);  // 490
+    mindist(2);  // 744
+
+#ifdef TIMER
+    printf("Time: %.0f us\n", stoptimer_us());
+#endif
 }
