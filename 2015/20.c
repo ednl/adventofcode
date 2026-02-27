129901f45a4de3f2c222f230b2cf6a6a49b17fbc
E. Dronkert <ednl@users.noreply.github.com>
2026-02-27 10:54:48 +0100

    style update

--- 2015/20.c
+++ 2015/20.c
@@ -1,52 +1,60 @@
 #include <stdio.h>
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define P  33100000U  // puzzle input
-#define N   1000000U  // number of houses (arbitrary)
-#define M1       10U  // elf multiplier (part 1)
-#define M2       11U  // elf multiplier (part 2)
-#define Q        50U  // max house visits (part 2)
+#define N  (1000 * 1000)  // number of houses (arbitrary)
+#define M1 10  // elf multiplier (part 1)
+#define M2 11  // elf multiplier (part 2)
+#define Q  50  // max house visits (part 2)
 
-unsigned int presents[N] = {M1};  // init for part 1
+static int presents[N];
 
 int main(void)
 {
-    unsigned int elf, house, npres, visit;
+    FILE *f = fopen("../aocinput/2015-20-input.txt", "r");
+    if (!f) return 1;
+    int P;  // puzzle input, presents threshold
+    if (fscanf(f, "%d", &P) != 1) return 2;
+    if (P < 1 || P > (1u << 31) - 1) return 3;
 
-    // Part 1: distribute presents (elf 1 already visited by init)
-    for (elf = 2; elf < N; ++elf) {
-        npres = elf * M1;
-        for (house = elf; house < N; house += elf) {
+#ifdef TIMER
+    starttimer();
+#endif
+
+    // Part 1: distribute presents
+    for (int house = 1; house < N; ++house)
+        presents[house] = M1;
+    for (int elf = 2; elf < N; ++elf) {
+        const int npres = elf * M1;
+        for (int house = elf; house < N; house += elf)
             presents[house] += npres;
-        }
     }
 
     // Part 1: find lowest house number where number of presents >= puzzle input
-    for (house = 1; house < N; ++house) {
+    for (int house = 1; house < N; ++house)
         if (presents[house] >= P) {
-            printf("Part 1: %u\n", house);  // 776160
+            printf("Part 1: %d (%d >= %d)\n", house, presents[house], P);  // 776160
             break;
         }
-    }
 
     // Part 2: distribute presents
-    for (house = 1; house < N; ++house) {
+    for (int house = 1; house < N; ++house)
         presents[house] = M2;
-    }
-    for (elf = 2; elf < N; ++elf) {
-        npres = elf * M2;
-        visit = 0;
-        for (house = elf; visit++ < Q && house < N; house += elf) {
+    for (int elf = 2; elf < N; ++elf) {
+        const int npres = elf * M2;
+        for (int visit = 0, house = elf; visit < Q && house < N; ++visit, house += elf)
             presents[house] += npres;
-        }
     }
 
     // Part 2: find lowest house number where number of presents >= puzzle input
-    for (house = 1; house < N; ++house) {
+    for (int house = 1; house < N; ++house)
         if (presents[house] >= P) {
-            printf("Part 2: %u\n", house);  // 786240
+            printf("Part 2: %d (%d >= %d)\n", house, presents[house], P);  // 786240
             break;
         }
-    }
 
-    return 0;
+#ifdef TIMER
+    printf("Time: %.0f us\n", stoptimer_us());
+#endif
 }
