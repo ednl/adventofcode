530cb3e82a4283e44f2d8e50babd29195432e140
E. Dronkert <ednl@users.noreply.github.com>
2026-02-27 10:54:05 +0100

    simplify step test

--- 2015/18.py
+++ 2015/18.py
@@ -14,8 +14,9 @@ MAX = SIZE - 1  # zero-based array
 with open('../aocinput/2015-18-input.txt') as f:
     data = (np.array([list(i.strip()) for i in f]) == '#').astype(np.uint8)
 
-# Leave centre bit on, so change neighbour check from 3,2 to 4,3
+# Count only neighbours, so reset centre bit
 kernel = np.ones((3, 3), dtype=np.uint8)
+kernel[1, 1] = 0
 
 t2 = monotonic_ns()
 
@@ -23,7 +24,7 @@ t2 = monotonic_ns()
 state = np.copy(data)
 for _ in range(STEP):
     nb = convolve2d(state, kernel, mode='same')
-    state = state & (nb == 4) | (nb == 3)
+    state = (state | nb) == 3
 print(np.sum(state))  # 821
 
 t3 = monotonic_ns()
@@ -33,7 +34,7 @@ state = np.copy(data)
 state[0, 0] = state[0, MAX] = state[MAX, 0] = state[MAX, MAX] = 1
 for _ in range(STEP):
     nb = convolve2d(state, kernel, mode='same')
-    state = state & (nb == 4) | (nb == 3)
+    state = (state | nb) == 3
     state[0, 0] = state[0, MAX] = state[MAX, 0] = state[MAX, MAX] = 1
 print(np.sum(state))  # 886
 
