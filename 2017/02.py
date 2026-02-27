93ed95c7fc4b239504734076769d519b2a6625ad
E. Dronkert <ednl@users.noreply.github.com>
2026-02-27 11:50:57 +0100

    style update

--- 2017/02.py
+++ 2017/02.py
@@ -5,16 +5,19 @@ a = np.loadtxt("../aocinput/2017-02-input.txt", dtype=np.uint32)
 a.sort(axis=1)
 
 # Part 1
-print((a[:,-1] - a[:,0]).sum())
+print((a[:,-1] - a[:,0]).sum())  # 34925
 
 # Part 2
-def finddiv(a):
+type Vector = list[int]
+def finddiv(a: Vector) -> int:
     for i, y in enumerate(a[:-1]):
         for x in a[i + 1:]:
             z = divmod(x, y)
             if z[1] == 0:
                 return z[0]
+    return 0
+
 k = 0
 for b in a:
     k += finddiv(b)
-print(k)
+print(k)  # 221
