76a1a1a64fba95a18a56a8f964c0dabe56f06a63
E. Dronkert <ednl@users.noreply.github.com>
2026-03-04 04:29:44 +0100

    less cheating

--- 2017/05.c
+++ 2017/05.c
@@ -11,9 +11,9 @@
  * Get minimum runtime from timer output:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :  31.9 ms
- *     Mac Mini 2020 (M1 3.2 GHz)    :     ? ms
- *     Raspberry Pi 5 (2.4 GHz)      :     ? ms
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 31.9 ms
+ *     Mac Mini 2020 (M1 3.2 GHz)    :    ? ms
+ *     Raspberry Pi 5 (2.4 GHz)      : 63.3 ms
  */
 
 #include <stdio.h>
@@ -32,17 +32,21 @@ int main(void)
     if (!f) return 1;
     for (int i = 0; i < N && fscanf(f, "%d", &vm1[i]) == 1; ++i);
     fclose(f);
-    memcpy(vm2, vm1, N * sizeof *vm1);
 
 #ifdef TIMER
     starttimer();
 #endif
 
+    // Second copy to start fresh in part 2
+    memcpy(vm2, vm1, N * sizeof *vm1);
+
+    // Part 1
     unsigned tick = 0;
     for (unsigned ip = 0; ip < N; ++tick)
         ip += vm1[ip]++;
     printf("%u\n", tick);  // 388611
 
+    // Part 2
     tick = 0;
     for (unsigned ip = 0; ip < N; ++tick)
         ip += vm2[ip] < 3 ? vm2[ip]++ : vm2[ip]--;
