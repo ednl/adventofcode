49d7e95e3fdf09dea02ab9cb1c6b9404126a1482
E. Dronkert <ednl@users.noreply.github.com>
2026-03-05 10:38:28 +0100

    input redirect option

--- 2017/05.c
+++ 2017/05.c
@@ -6,8 +6,13 @@
  *
  * Compile:
  *     cc -std=c17 -Wall -Wextra -pedantic 05.c
+ * Enable debug output:
+ *     cc -DDEBUG 05.c
  * Enable timer:
  *     cc -std=gnu17 -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 05.c
+ * Get input from other input file:
+ *     ./a.out < input.txt
+ *     cat input.txt | ./a.out
  * Get minimum runtime from timer output:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
@@ -17,67 +22,100 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>  // isatty, fileno
 #include <string.h>  // memcpy
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
 #define FNAME "../aocinput/2017-05-input.txt"
-#define N 1090
+#define N 1200  // needed for my input: 1090
 
-static int inp[N];  // keep original for comparison ifdef DEBUG
+static int n;
+static int inp[N];
 static int vm1[N];
 static int vm2[N];
 #ifdef DEBUG
-    static int count1[N];
-    static int count2[N];
+    static int add[N];
+    static int sub[N];
 #endif
+
+static int readinput(FILE *f)
+{
+    int i = 0;
+    while (i < N && fscanf(f, "%d", &inp[i]) == 1)
+        i++;
+    return i;
+}
+
+static int part1(void)
+{
+    int tick = 0;
+    for (int ip = 0; ip < n; ++tick)
+        ip += vm1[ip]++;
+    return tick;
+}
+
+static int part2(void)
+{
+    int tick = 0;
+    for (int ip = 0; ip < n; ++tick)
+        ip += vm2[ip] < 3 ? (
+        #ifdef DEBUG
+            add[ip]++,
+        #endif
+            vm2[ip]++) : (
+        #ifdef DEBUG
+            sub[ip]--,
+        #endif
+            vm2[ip]--);
+    return tick;
+}
 
 int main(void)
 {
-    FILE *f = fopen(FNAME, "r");
-    if (!f) { fprintf(stderr, "File not found: "FNAME"\n"); return 1; }
-    for (int i = 0; i < N && fscanf(f, "%d", &inp[i]) == 1; ++i);
-    fclose(f);
+    if (isatty(fileno(stdin))) {
+        FILE *f = fopen(FNAME, "r");
+        if (!f) {
+            fprintf(stderr, "File not found: "FNAME"\n");
+            return 1;
+        }
+        n = readinput(f);
+        fclose(f);
+    } else
+        n = readinput(stdin);
 
-    // Two copies to start fresh in part 2
-    memcpy(vm1, inp, N * sizeof *inp);
-    memcpy(vm2, inp, N * sizeof *inp);
+    if (!n) {
+        fprintf(stderr, "Invalid input\n");
+        return 2;
+    }
+#ifdef DEBUG
+    printf("input length: %d\n", n);
+#endif
+
+    // Two copies to keep original for comparison
+    memcpy(vm1, inp, n * sizeof *inp);
+    memcpy(vm2, inp, n * sizeof *inp);
 
 #ifdef TIMER
     starttimer();
 #endif
 
-    // Part 1
-    unsigned tick = 0;
-    for (unsigned ip = 0; ip < N; ++tick) {
-    #ifdef DEBUG
-        count1[ip]++;
-    #endif
-        ip += vm1[ip]++;
-    }
-    printf("%u\n", tick);  // 388611
+    printf("%d\n", part1());  // 388611
+    printf("%d\n", part2());  // 27763113
 
-    // Part 2
-    tick = 0;
-    for (unsigned ip = 0; ip < N; ++tick) {
-    #ifdef DEBUG
-        count2[ip]++;
-    #endif
-        ip += vm2[ip] < 3 ? vm2[ip]++ : vm2[ip]--;
-    }
-    printf("%u\n", tick);  // 27763113
+#ifdef TIMER
+    const double us = stoptimer_us();
+#endif
 
 #ifdef DEBUG
-    for (int i = 0; i < N; ++i) {
-        printf("    %4d: %5d", i, inp[i]);
-        if (inp[i] != vm1[i])
-            printf(" %5d (%3d) %5d (%5d)", vm1[i], count1[i], vm2[i], count2[i]);
-        putchar('\n');
+    for (int i = 0; i < n; ++i) {
+        printf("    %4d: %5d | %5d %+4d | %5d %+6d %+6d\n",
+            i, inp[i], vm1[i], vm1[i] - inp[i], vm2[i], add[i], sub[i]);
     }
 #endif
 
 #ifdef TIMER
-    printf("Time: %.0f us\n", stoptimer_us());
+    printf("Time: %.0f us\n", us);
 #endif
 }
