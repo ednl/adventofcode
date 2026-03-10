1ddb0c64061321b57bb568ab1e78a2ad42765a7f
E. Dronkert <ednl@users.noreply.github.com>
2026-03-10 02:01:16 +0100

    timing

--- 2017/09.c
+++ 2017/09.c
@@ -14,8 +14,8 @@
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
  *     Macbook Pro 2024 (M4 4.4 GHz) : 13.9 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
- *     Raspberry Pi 5 (2.4 GHz)      : ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :    ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 78.2 µs
  */
 
 #include <stdio.h>
