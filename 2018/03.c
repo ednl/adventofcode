ed718063b1a22a11e3e08d67b7bd2a7f0e6d4f7e
E. Dronkert <ednl@users.noreply.github.com>
2026-04-03 13:29:05 +0200

    style update

--- 2018/03.c
+++ 2018/03.c
@@ -4,93 +4,76 @@
  * https://adventofcode.com/2018/day/3
  * By: E. Dronkert https://github.com/ednl
  *
- * Benchmark on a Mac Mini M1, compiler Apple clang 14.0.3 with -O3 -march=native:
- *
- *     [TBD]
- *
- * Benchmark on an iMac (Late 2013, 3.2 GHz quad-core Core i5 "Haswell"),
- * compiler Apple clang 12.0.0 with -O3 -march=native:
- *
- *     $ hyperfine -N -w 200 -r 1000 ./a.out
- *     Benchmark 1: ./a.out
- *       Time (mean ± σ):       4.1 ms ±   0.5 ms    [User: 2.1 ms, System: 1.0 ms]
- *       Range (min … max):     3.4 ms …   6.2 ms    1000 runs
- *
- * Benchmark on a Raspberry Pi 4, compiler Debian gcc 10.2.1-6 with -O3 -march=native
- * and the CPU in performance mode:
- *
- *     echo performance | sudo tee /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
- *     (to reset, replace performance with ondemand)
- *     /boot/config.txt: arm_boost=1, no overclock
- *
- *     $ hyperfine -N -w 100 -r 500 ./a.out
- *     Benchmark 1: ./a.out
- *       Time (mean ± σ):      11.3 ms ±   0.1 ms    [User: 5.8 ms, System: 5.2 ms]
- *       Range (min … max):    11.1 ms …  12.3 ms    500 runs
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 03.c
+ * Enable timer:
+ *     cc -std=gnu17 -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 03.c
+ * Get minimum runtime from timer output:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 179 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :   ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      :   ? µs
 */
+
 #include <stdio.h>   // fopen, fclose, fscanf, printf
-#include <stdint.h>  // uint16_t, uint32_t
 #include <stdbool.h>
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define N 1293  // number of claims (lines) in input file
-#define M 1000  // fabric dimension MxM
+#define FNAME "../aocinput/2018-03-input.txt"
+#define N 1293  // number of claims (lines in input file)
+#define M 1000  // fabric dimension MxM "at least 1000x1000"
 
-typedef struct {
-    uint16_t x, y;
+typedef struct pos {
+    int x, y;
 } Pos;
 
-typedef struct {
-    uint16_t id;
+typedef struct claim {
+    int id;
     Pos a, b;  // a = top left corner, b = 1 outside bottom right corner
 } Claim;
 
 static Claim claim[N];
-static uint16_t fabric[M][M];
-
-static size_t parse(void)
-{
-    FILE *f = fopen("../aocinput/2018-03-input.txt", "r");
-    if (!f)
-        return 0;
-
-    size_t n = 0;
-    uint16_t id, x, y, w, h;
-    while (n < N && fscanf(f, "#%hu @ %hu,%hu: %hux%hu ", &id, &x, &y, &w, &h) == 5)
-        claim[n++] = (Claim){id, {x, y}, {x + w, y + h}};
-    fclose(f);
-    return n;
-}
+static unsigned char fabric[M][M];
 
 int main(void)
 {
-    // Input
-    size_t n = parse();
+    FILE *f = fopen(FNAME, "r");
+    if (!f) { fprintf(stderr, "File not found: "FNAME"\n"); return 1; }
+    for (int i = 0, id, x, y, w, h; i < N &&
+        fscanf(f, "#%d @ %d,%d: %dx%d ", &id, &x, &y, &w, &h) == 5; ++i)
+        claim[i] = (Claim){id, {x, y}, {x + w, y + h}};
+    fclose(f);
+
+#ifdef TIMER
+    starttimer();
+#endif
 
     // Lay claim
+    int overlap = 0;
     const Claim *c = claim;
-    for (size_t i = 0; i < n; ++i, ++c)
-        for (size_t x = c->a.x; x < c->b.x; ++x)
-            for (size_t y = c->a.y; y < c->b.y; ++y)
-                fabric[x][y]++;
-    // Count all overlap
-    uint32_t overlap = 0;
-    for (size_t x = 0; x < M; ++x)
-        for (size_t y = 0; y < M; ++y)
-            if (fabric[x][y] > 1)
-                overlap++;
-    printf("Part 1: %u\n", overlap);  // 110827
+    for (int i = 0; i < N; ++i, ++c)
+        for (int x = c->a.x; x < c->b.x; ++x)
+            for (int y = c->a.y; y < c->b.y; ++y)
+                if (fabric[x][y]++ == 1)
+                    overlap++;
+    printf("%d\n", overlap);  // 110827
 
     // Find claim without overlap
     c = claim;
-    for (size_t i = 0; i < n; ++i, ++c) {
-        bool intact = true;  // extra variable to break twice
-        for (size_t x = c->a.x; intact && x < c->b.x; ++x)
-            for (size_t y = c->a.y; y < c->b.y && (intact = fabric[x][y] == 1); ++y)
-                ;
-        if (intact) {
-            printf("Part 2: %u\n", c->id);  // 116
-            return 0;
-        }
+    for (int i = 0; i < N; ++i, ++c) {
+        for (int x = c->a.x; x < c->b.x; ++x)
+            for (int y = c->a.y; y < c->b.y; ++y)
+                if (fabric[x][y] > 1)
+                    goto nexti;
+        printf("%d\n", c->id);  // 116
+    #ifdef TIMER
+        printf("Time: %.0f us\n", stoptimer_us());
+    #endif
+        return 0;
+    nexti:;
     }
-    return 1;
+    return 1;  // claim without overlap not found
 }
