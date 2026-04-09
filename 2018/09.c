e657be91a9f6b16c50cb9c0438bdedfb0a46ae42
E. Dronkert <ednl@users.noreply.github.com>
2026-04-09 13:47:17 +0200

    cleanup

--- 2018/09.c
+++ 2018/09.c
@@ -1,34 +1,54 @@
-#include <stdio.h>
-#include "../startstoptimer.h"
+/**
+ * Advent of Code 2018
+ * Day 9: Marble Mania
+ * https://adventofcode.com/2018/day/9
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 09.c
+ * Enable timer:
+ *     cc -std=gnu17 -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 09.c
+ * Get minimum runtime from timer output:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 24 ms
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  ? ms
+ *     Raspberry Pi 5 (2.4 GHz)      :  ? ms
+*/
 
-// Values from my input file
-#define PLAYERS      471U
-#define MARBLES1   72026U
-#define MARBLES2 7202600U  // x100
+#include <stdio.h>
+#include <stdlib.h>  // calloc, free
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
+
+#define FNAME "../aocinput/2018-09-input.txt"
+#define KEEP 23U  // puzzle rule
+
+typedef unsigned int u32;
 
 typedef struct Marble {
     struct Marble *prev, *next;
-    unsigned int val;
+    u32 val;
 } Marble;
 
-static Marble circle[MARBLES2] = {0};
-static unsigned int score[PLAYERS] = {0};
+static Marble *circle;
+static u32 *score;
 
 // Play a complete game
 // ret: high score
-static unsigned int play(unsigned int marbles)
+static u32 play(const u32 players, const u32 marbles)
 {
     Marble *cur, *p, *q;
-    unsigned int i, freeindex = 0, num = 0, player = PLAYERS - 1;  // start with player = -1
+    u32 i, freeindex = 0, num = 0, player = players - 1;  // start with player = -1
 
     // Init zeroth marble
     cur = &circle[freeindex++];
     *cur = (Marble){cur, cur, num++};
 
     // Reset scores
-    for (i = 0; i < PLAYERS; ++i) {
+    for (i = 0; i < players; ++i)
         score[i] = 0;
-    }
 
     // Use all the marbles
     while (num <= marbles) {
@@ -56,7 +76,7 @@ static unsigned int play(unsigned int marbles)
             q = p->prev;
             // Give points
             player += 23U;
-            score[player % PLAYERS] += num++ + p->val;
+            score[player % players] += num++ + p->val;
             // Remove marble;
             q->next = cur;
             cur->prev = q;
@@ -64,19 +84,40 @@ static unsigned int play(unsigned int marbles)
     }
 
     // Determine hiscore
-    unsigned int hi = 0;
-    for (i = 0; i < PLAYERS; ++i) {
-        if (score[i] > hi) {
+    u32 hi = 0;
+    for (i = 0; i < players; ++i)
+        if (score[i] > hi)
             hi = score[i];
-        }
-    }
     return hi;
 }
 
 int main(void)
 {
+    FILE *f = fopen(FNAME, "r");
+    if (!f)
+        return 1;
+    u32 inp1 = 0, inp2 = 0;
+    if (fscanf(f, "%u players; last marble is worth %u", &inp1, &inp2) != 2)
+        return 2;
+    fclose(f);
+
+    const u32 players = inp1;
+    const u32 marbles1 = inp2;
+    const u32 marbles2 = marbles1 * 100;  // part 2
+
+    circle = calloc(marbles2, sizeof *circle);
+    score = calloc(players, sizeof *score);
+
+#ifdef TIMER
     starttimer();
-    printf("Part 1: %u\n", play(MARBLES1));  // 390093
-    printf("Part 2: %u\n", play(MARBLES2));  // 3150377341
-    printf("Time: %.0f us\n", stoptimer_us());
+#endif
+
+    printf("Part 1: %u\n", play(players, marbles1));  // 390093
+    printf("Part 2: %u\n", play(players, marbles2));  // 3150377341
+
+#ifdef TIMER
+    printf("Time: %.0f ms\n", stoptimer_ms());
+#endif
+    free(score);
+    free(circle);
 }
--- 2018/09alt.c
+++ 2018/09alt.c
@@ -1,41 +1,74 @@
+/**
+ * Advent of Code 2018
+ * Day 9: Marble Mania
+ * https://adventofcode.com/2018/day/9
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 09alt.c
+ * Enable timer:
+ *     cc -std=gnu17 -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 09alt.c
+ * Get minimum runtime from timer output:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  3.12 ms
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  ?    ms
+ *     Raspberry Pi 5 (2.4 GHz)      :  ?    ms
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
-#include "../startstoptimer.h"
+#include <stdlib.h>  // calloc, free
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define KEEP 23U
-// Values from my input file
-#define PLAYERS    471U
-#define MARBLES1 72026U
-// Derived values
-#define MARBLES2 (MARBLES1 * 100U)
-#define LEN      (MARBLES2 * 2U)
+#define FNAME "../aocinput/2018-09-input.txt"
+#define KEEP 23U  // puzzle rule
 
-static unsigned int score[PLAYERS] = {0};
-static unsigned int line[LEN] = {0};
+typedef unsigned int u32;
 
 int main(void)
 {
-    starttimer();
-    unsigned int i = 0, j = 1, n = 1, m = KEEP + 1, k = KEEP, hi = 0;
+    FILE *f = fopen(FNAME, "r");
+    if (!f)
+        return 1;
+    u32 inp1 = 0, inp2 = 0;
+    if (fscanf(f, "%u players; last marble is worth %u", &inp1, &inp2) != 2)
+        return 2;
+    fclose(f);
 
-    for (unsigned int t = 0; t < 6U; ++t) {
+    const u32 players = inp1;
+    const u32 marbles1 = inp2;
+    const u32 marbles2 = marbles1 * 100U;  // part 2
+    const u32 len = marbles2 * 37U / 23U;  // needed for my input: 11586785
+
+    u32 *score = calloc(players, sizeof *score);
+    u32 *line = calloc(len, sizeof *line);
+
+#ifdef TIMER
+    starttimer();
+#endif
+
+    u32 i = 0, j = 1, n = 1, m = KEEP + 1, k = KEEP, hi = 0;
+
+    for (u32 t = 0; t != 6U; ++t) {
         line[j++] = line[i++];      // skip one
         line[j++] = n++;            // 1-6
     }
 
-    while (k <= MARBLES1) {
-        for (unsigned int t = 0; t < 12U; ++t) {
+    for (; k <= marbles1; k += KEEP) {
+        for (u32 t = 0; t != 12U; ++t) {
             line[j++] = line[i++];  // skip one
             line[j++] = n++;        // 7-18
         }
 
         // Keep multiple of 23 + take marble at index i
-        score[k % PLAYERS] += k + line[i];
+        score[k % players] += k + line[i];
 
         line[i] = n++;              // 19
         line[j++] = line[i++];      // skip one
 
-        for (unsigned int t = 0; t < 3U; ++t) {
+        for (u32 t = 0; t != 3U; ++t) {
             line[j++] = line[i++];  // skip one
             line[j++] = m++;        // 24, 26, 28
             line[j++] = n++;        // 20, 21, 22
@@ -44,28 +77,26 @@ int main(void)
 
         n += KEEP - 16U;  // 16 already counted, need 7 more to make 23
         m += KEEP - 6U;   // 6 already counted, need 17 more to make 23
-        k += KEEP;
     }
 
-    for (unsigned int t = 0; t < PLAYERS; ++t) {
+    for (u32 t = 0; t != players; ++t)
         if (score[t] > hi)
             hi = score[t];
-    }
     printf("Part 1: %u\n", hi);  // 390093
 
-    while (k <= MARBLES2) {
-        for (unsigned int t = 0; t < 12U; ++t) {
+    for (; k <= marbles2; k += KEEP) {
+        for (u32 t = 0; t < 12U; ++t) {
             line[j++] = line[i++];  // skip one
             line[j++] = n++;        // 7-18
         }
 
         // Keep multiple of 23 + take marble at index i
-        score[k % PLAYERS] += k + line[i];
+        score[k % players] += k + line[i];
 
         line[i] = n++;              // 19
         line[j++] = line[i++];      // skip one
 
-        for (unsigned int t = 0; t < 3U; ++t) {
+        for (u32 t = 0; t < 3U; ++t) {
             line[j++] = line[i++];  // skip one
             line[j++] = m++;        // 24, 26, 28
             line[j++] = n++;        // 20, 21, 22
@@ -74,14 +105,16 @@ int main(void)
 
         n += KEEP - 16U;  // 16 already counted, need 7 more to make 23
         m += KEEP - 6U;   // 6 already counted, need 17 more to make 23
-        k += KEEP;
     }
 
-    for (unsigned int t = 0; t < PLAYERS; ++t) {
+    for (u32 t = 0; t < players; ++t)
         if (score[t] > hi)
             hi = score[t];
-    }
     printf("Part 2: %u\n", hi);  // 3150377341
-    printf("j=%u N/23*37=%u\n", j, MARBLES2 / 23 * 37);
+
+#ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
+#endif
+    free(line);
+    free(score);
 }
