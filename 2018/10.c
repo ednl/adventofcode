cbff5fbb4f45e086c9cde18428cc283cbe9f895c
E. Dronkert <ednl@users.noreply.github.com>
2026-04-10 12:25:25 +0200

    style update

--- 2018/10.c
+++ 2018/10.c
@@ -1,70 +1,87 @@
+/**
+ * Advent of Code 2018
+ * Day 10:
+ * https://adventofcode.com/2018/day/10
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 10.c
+ * Enable timer:
+ *     cc -std=gnu17 -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 10.c
+ * Get minimum runtime from timer output:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 1.83 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : ?    µs
+ *     Raspberry Pi 5 (2.4 GHz)      : ?    µs
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>  // memset
+#include <limits.h>  // INT_MIN, INT_MAX
 #include <stdbool.h>
-#include <limits.h>
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
 // Number of points in my input file
-#define N 324U
+#define N 324
+
+// Matrix display dimensions with hindsight
+#define W (8 * (6 + 2))  // 8 characters of 6 dots wide with 2 spaces inbetween, +\n\0
+#define H 10  // 10 dots high
 
 typedef struct {
     int x, y;
 } Vec;
 
 // Old and new positions, constant velocity
-static Vec pos0[N] = {0}, pos1[N] = {0}, vel[N] = {0};
+static Vec pos0[N];
+static Vec pos1[N];
+static Vec vel[N];
 
-// Read input file
-static bool read(void)
+// Matrix display
+static char matrix[H][W];
+
+// Read input file, return number of lines read
+static int read(void)
 {
     FILE *f = fopen("../aocinput/2018-10-input.txt", "r");
-    if (!f) {
-        return false;
-    }
-
-    size_t n = 0;
+    if (!f)
+        return 0;  // error: zero lines read
+    int n = 0;
     Vec p, v;
-    while (n < N && fscanf(f, "position=< %d, %d> velocity=< %d, %d> ", &p.x, &p.y, &v.x, &v.y) == 4) {
+    for (; n < N && fscanf(f, "position=< %d, %d> velocity=< %d, %d> ", &p.x, &p.y, &v.x, &v.y) == 4; n++) {
         pos0[n] = p;
         vel[n] = v;
-        n++;
     }
     fclose(f);
-    return n == N;
-}
-
-// Sort first by y then by x coordinate
-static int cmp_pos(const void *const a, const void *const b)
-{
-    const Vec *const p = a;
-    const Vec *const q = b;
-    if (p->y < q->y) return -1;
-    if (p->y > q->y) return  1;
-    if (p->x < q->x) return -1;
-    if (p->x > q->x) return  1;
-    return 0;
+    return n;
 }
 
 int main(void)
 {
     // Read input file into globals pos0[] and vel[]
-    if (!read()) {
+    if (read() < N)
         return 1;
-    }
+
+#ifdef TIMER
+    starttimer();
+#endif
 
     // Intersection: x0 + vx0.t = x1 + vx1.t
     //   => t = (x0 - x1) / (vx1 - vx0)
     // will be close approx of final t
-    size_t i = 1;
-    while (pos0[i].x == pos0[0].x || vel[i].x == vel[0].x) {
+    int i = 1;
+    while (pos0[i].x == pos0[0].x || vel[i].x == vel[0].x)
         i++;  // skip parallel lines or (unlikely) already overlapping points
-    }
     int t = (pos0[0].x - pos0[i].x) / (vel[i].x - vel[0].x);
 
     // Go to initial position in one big step
     // and determine spread of the points
     Vec pmin = {INT_MAX, INT_MAX};
     Vec pmax = {INT_MIN, INT_MIN};
-    for (i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
         pos0[i].x += vel[i].x * t;
         pos0[i].y += vel[i].y * t;
         if (pos0[i].x < pmin.x) pmin.x = pos0[i].x;
@@ -76,7 +93,7 @@ int main(void)
 
     // First try going forward (single time steps)
     // p keeps track of the best coordinates, q is the next set
-    Vec *p = pos0, *q = pos1, *temp;
+    Vec *p = pos0, *q = pos1, *tmp;
     bool better;
     do {
         dim0 = dim1;
@@ -93,9 +110,9 @@ int main(void)
         dim1 = (Vec){pmax.x - pmin.x, pmax.y - pmin.y};
         if ((better = dim1.x <= dim0.x && dim1.y <= dim0.y)) {
             t++;
-            temp = p;
+            tmp = p;
             p = q;
-            q = temp;
+            q = tmp;
         }
     } while (better);
 
@@ -104,10 +121,10 @@ int main(void)
     Vec pmin0;
     do {
         dim0 = dim1;
-        pmin0 = pmin;
+        pmin0 = pmin;  // save minimum
         pmin = (Vec){INT_MAX, INT_MAX};
         pmax = (Vec){INT_MIN, INT_MIN};
-        for (i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i) {
             q[i].x = p[i].x - vel[i].x;
             q[i].y = p[i].y - vel[i].y;
             if (q[i].x < pmin.x) pmin.x = q[i].x;
@@ -118,36 +135,29 @@ int main(void)
         dim1 = (Vec){pmax.x - pmin.x, pmax.y - pmin.y};
         if ((better = dim1.x <= dim0.x && dim1.y <= dim0.y)) {
             t--;
-            temp = p;
+            tmp = p;
             p = q;
-            q = temp;
+            q = tmp;
         }
     } while (better);
 
     // Rebase to zero using the saved minimal coordinates
-    // and sort by y,x coordinates
-    for (i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
         p[i].x -= pmin0.x;
         p[i].y -= pmin0.y;
     }
-    qsort(p, N, sizeof(Vec), cmp_pos);
 
-    printf("Part 1:\n\n");
-    i = 0;  // point index
-    for (int y = 0; y <= dim0.y; ++y) {
-        for (int x = 0; x <= dim0.x; ++x) {
-            if (i < N && x == p[i].x && y == p[i].y) {
-                printf("#");
-                do {  // there can be multiple overlapping points
-                    ++i;
-                } while (i < N && x == p[i].x && y == p[i].y);
-            } else {
-                printf(" ");
-            }
-        }
-        printf("\n");
+    memset(matrix, ' ', sizeof matrix);
+    for (int i = 0; i < N; ++i)
+        matrix[p[i].y][p[i].x] = '#';  // ERCXLAJL
+    for (int i = 0; i < H; ++i) {
+        matrix[i][W - 2] = '\n';
+        matrix[i][W - 1] = '\0';
+        printf("%s", matrix[i]);
     }
-    printf("\nPart 2: %u\n", t);
+    printf("%d\n", t);  // 10813
 
-    return 0;
+#ifdef TIMER
+    printf("Time: %.0f ns\n", stoptimer_ns());
+#endif
 }
