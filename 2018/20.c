9cf5da489cca9175e11e75c923d0f587095c1acc
E. Dronkert <ednl@users.noreply.github.com>
2026-04-20 14:47:22 +0200

    style update

--- 2018/20.c
+++ 2018/20.c
@@ -4,43 +4,40 @@
  * https://adventofcode.com/2018/day/20
  * By: E. Dronkert https://github.com/ednl
  *
- * Benchmark on an iMac (Late 2013, 3.2 GHz quad-core Core i5 "Haswell"),
- * compiler Apple clang 12.0.0 with -O3 -march=native:
- *
- *     $ hyperfine -N -w 100 -r 500 ./a.out
- *     Benchmark 1: ./a.out
- *       Time (mean ± σ):       4.5 ms ±   0.1 ms    [User: 3.2 ms, System: 0.6 ms]
- *       Range (min … max):     4.4 ms …   5.3 ms    500 runs
- *
- * Benchmark on a Raspberry Pi 4, compiler Debian gcc 10.2.1-6 with -O3 -march=native
- * and the CPU in performance mode:
- *
- *     echo performance | sudo tee /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
- *     (to reset, replace performance with ondemand)
- *     /boot/config.txt: arm_boost=1, no overclock
- *
- *     $ hyperfine -N -w 50 -r 200 ./a.out
- *     Benchmark 1: ./a.out
- *       Time (mean ± σ):      15.3 ms ±   0.7 ms    [User: 13.7 ms, System: 1.4 ms]
- *       Range (min … max):    14.0 ms …  17.6 ms    200 runs
-*/
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 20.c
+ * Enable timer:
+ *     cc -std=gnu17 -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 20.c
+ * Get minimum runtime from timer output:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements, includes all parsing but not reading from disk:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 1.40 ms
+ *     Mac Mini 2020 (M1 3.2 GHz)    :    ? ms
+ *     Raspberry Pi 5 (2.4 GHz)      :    ? ms
+ */
+
 #include <stdio.h>    // fopen, fclose, fgetc, printf, putchar
 #include <string.h>   // memmove
 #include <stdint.h>   // int8_t, int16_t
 #include <stdbool.h>  // bool, true, false
+#include "../startstoptimer.h"
 
+#define FNAME "../aocinput/2018-20-input.txt"
+#define FSIZE     16384  // needed for my input: 14185
 #define STACKSIZE   256  // needed for my input: 225
 #define ROOMCOUNT 16384  // needed for my input: 10000 (grid 100x100)
+#define BIG        1000  // distance >=1000 is big
 
-typedef struct {
+typedef struct pos {
     int8_t x, y;
 } Pos;
 
-typedef struct {
+typedef struct room {
     Pos pos;
     int16_t dist;
 } Room;
 
+static char input[FSIZE];
 static Room stack[STACKSIZE];
 static size_t stacksize;
 // static size_t maxstacksize;
@@ -51,11 +48,12 @@ static size_t roomcount;
 // Sort rooms by position
 static int cmp_pos(const void *a, const void *b)
 {
-    const Pos p = ((const Room *)a)->pos, q = ((const Room *)b)->pos;
-    if (p.x < q.x) return -1;
-    if (p.x > q.x) return  1;
-    if (p.y < q.y) return -1;
-    if (p.y > q.y) return  1;
+    const Pos *const p = &((const Room *)a)->pos;
+    const Pos *const q = &((const Room *)b)->pos;
+    if (p->x < q->x) return -1;
+    if (p->x > q->x) return  1;
+    if (p->y < q->y) return -1;
+    if (p->y > q->y) return  1;
     return 0;
 }
 
@@ -63,10 +61,10 @@ static int cmp_pos(const void *a, const void *b)
 // Return false if failed, or not needed, to insert or update.
 static bool binsert(const Room a)
 {
-    if (roomcount >= ROOMCOUNT) {
-        putchar('b');  // increase ROOMCOUNT
-        return false;
-    }
+    // if (roomcount >= ROOMCOUNT) {
+    //     putchar('b');  // increase ROOMCOUNT
+    //     return false;
+    // }
 
     // First element in empty array.
     if (!roomcount) {
@@ -128,8 +126,6 @@ static bool push(const Room a)
 {
     if (stacksize < STACKSIZE) {
         stack[stacksize++] = a;
-        // if (stacksize > maxstacksize)
-        //     maxstacksize = stacksize;
         return true;
     }
     putchar('u');  // increase STACKSIZE
@@ -160,31 +156,35 @@ static bool pop(Room * const a)
 
 int main(void)
 {
-    FILE *f = fopen("../aocinput/2018-20-input.txt", "r");
-    if (!f)
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) {
+        fputs("File not found: "FNAME, f);
         return 1;
-    int c;
+    }
+    fread(input, 1, FSIZE, f);
+    fclose(f);
+
+    starttimer();
+
     Room a = {0};
     binsert(a);
-    while ((c = fgetc(f)) != '$')
-        switch (c) {
+    for (const char *c = input; *c; ++c)
+        switch (*c) {
             case '(': push(a);  break;
-            case '|': peek(&a); break;
             case ')': pop(&a);  break;
-            case 'W': --a.pos.x; ++a.dist; binsert(a); break;
-            case 'N': --a.pos.y; ++a.dist; binsert(a); break;
-            case 'E': ++a.pos.x; ++a.dist; binsert(a); break;
-            case 'S': ++a.pos.y; ++a.dist; binsert(a); break;
+            case 'E': a.pos.x++; a.dist++; binsert(a); break;
+            case 'N': a.pos.y--; a.dist++; binsert(a); break;
+            case 'S': a.pos.y++; a.dist++; binsert(a); break;
+            case 'W': a.pos.x--; a.dist++; binsert(a); break;
+            case '|': peek(&a); break;
         }
-    fclose(f);
-    // printf("maxstacksize=%zu roomcount=%zu\n", maxstacksize, roomcount);
 
     int max = 0, big = 0;
     for (size_t i = 0; i < roomcount; ++i) {
-        if (room[i].dist > max) max = room[i].dist;
-        if (room[i].dist >= 1000) ++big;
+        if (room[i].dist >  max) max = room[i].dist;
+        if (room[i].dist >= BIG) big++;
     }
-    printf("Part 1: %d\n", max);  // 3810
-    printf("Part 2: %d\n", big);  // 8615
-    return 0;
+    printf("%d\n", max);  // part 1: 3810
+    printf("%d\n", big);  // part 2: 8615
+    printf("Time: %.0f us\n", stoptimer_us());
 }
