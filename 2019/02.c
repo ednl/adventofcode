a255f33a02285eb1c64c5390e2349b09954d90f5
E. Dronkert <ednl@users.noreply.github.com>
2026-05-04 11:55:04 +0200

    optimised timing

--- 2019/02.c
+++ 2019/02.c
@@ -8,26 +8,27 @@
  *     cc -std=c17 -Wall -Wextra -pedantic 02.c
  * Enable timer:
  *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 02.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n2
  * Get minimum runtime from timer output in bash:
- *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 1.50 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 2.54 µs
- *     Raspberry Pi 5 (2.4 GHz)      : 7.91 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 0.35 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :    ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      :    ? µs
  */
 
 #include <stdio.h>
-#include <stdlib.h>    // calloc, free
-#include <string.h>    // memcpy
-#include <stdint.h>    // int64_t
-#include <inttypes.h>  // PRId64
+#include <stdlib.h>  // div
+#include <string.h>  // memcpy
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
-#define FNAME "../aocinput/2019-02-input.txt"
-#define VMSIZE 128
-#define TARGET 19690720
+#define FNAME  "../aocinput/2019-02-input.txt"
+#define FSIZE  288       // needed for my input: 273
+#define VMSIZE 128       // needed for my input: 109
+#define TARGET 19690720  // date of the first moon landing
 
 typedef enum opcode {
     ADD = 1,  // add params and store
@@ -36,77 +37,62 @@ typedef enum opcode {
 } OpCode;
 
 typedef struct vm {
-    int64_t *app;  // ROM code
-    int64_t *mem;  // RAM
-    int cap, len;
+    int app[VMSIZE];  // ROM code, save for next run
+    int mem[VMSIZE];  // RAM, run self-modifying program
+    int len;
 } VM;
 
-static int csv(const char *fname, int64_t *const list, const int size)
-{
-    FILE *f = fopen(fname, "r");
-    if (!f) return 0;
-    const int64_t *const end = list + size;
-    int64_t *x = list;
-    *x = 0;
-    int s = 1;
-    for (int c = fgetc(f); c != EOF && x != end; c = fgetc(f)) {
-        if (c >= '0' && c <= '9')
-            *x = *x * 10 + (c & 15);
-        else if (c == ',') {
-            *x++ *= s;
-            *x = 0;
-            s = 1;
-        } else if (c == '-')
-            s = -1;
-    }
-    fclose(f);
-    *x++ *= s;
-    return x - list;
-}
+static char input[FSIZE];
+static VM vm;
 
-static int64_t run(VM *vm, const int noun, const int verb)
+static int run(const int noun, const int verb)
 {
-    int64_t *const m = vm->mem;  // short name
-    memcpy(m, vm->app, vm->len * sizeof *m);
-    m[1] = noun;
-    m[2] = verb;
-    for (int ip = 0;; ip += 4)
-        switch (m[ip]) {
-            case ADD: m[m[ip + 3]] = m[m[ip + 1]] + m[m[ip + 2]]; break;
-            case MUL: m[m[ip + 3]] = m[m[ip + 1]] * m[m[ip + 2]]; break;
-            case RET: return m[0];
+    memcpy(vm.mem, vm.app, vm.len * sizeof *vm.mem);
+    vm.mem[1] = noun;
+    vm.mem[2] = verb;
+    for (int *m = vm.mem;; m += 4)
+        switch (*m) {
+            case ADD: vm.mem[*(m + 3)] = vm.mem[*(m + 1)] + vm.mem[*(m + 2)]; break;
+            case MUL: vm.mem[*(m + 3)] = vm.mem[*(m + 1)] * vm.mem[*(m + 2)]; break;
+            case RET: return vm.mem[0];
         }
 }
 
 int main(void)
 {
-    VM vm = {
-        .app = calloc(VMSIZE * 2, sizeof *vm.app),
-        .cap = VMSIZE
-    };
-    vm.mem = vm.app + VMSIZE;
-    vm.len = csv(FNAME, vm.app, vm.cap);
+    // Read from disk
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) return 1;
+    const int fsize = fread(input, 1, FSIZE, f);
+    input[fsize - 1] = '\0';  // remove newline
+    fclose(f);
 
 #ifdef TIMER
-    starttimer();
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
+    vm.len = 0;
 #endif
 
-    // Part 1
-    printf("%"PRId64"\n", run(&vm, 12, 2));  // 3085697
-
-    // Part 2
-    const int base  = run(&vm, 0, 0);
-    const int dnoun = run(&vm, 1, 0) - base;
-    const int dverb = run(&vm, 0, 1) - base;
-    if (dverb == 1) {
-        const int target = TARGET - base;
-        const int noun = target / dnoun;
-        const int verb = target % dnoun;
-        printf("%d\n", noun * 100 + verb);  // 9425
+    // Parse CSV to vm.app
+    for (const char *c = input; *c; ++c) {
+        int x = *c++ & 15;
+        while (*c >= '0')
+            x = x * 10 + (*c++ & 15);
+        vm.app[vm.len++] = x;
     }
 
+    // Part 1
+    printf("%d\n", run(12, 2));  // part 1: 3085697
+
+    // Part 2
+    const int base  = run(0, 0);
+    const int dnoun = run(1, 0) - base;
+    // const int dverb = run(0, 1) - base;  // assume dverb==1
+    const div_t sol = div(TARGET - base, dnoun);
+    printf("%d\n", sol.quot * 100 + sol.rem);  // part 2: 9425
+
 #ifdef TIMER
-    printf("Time: %.0f ns\n", stoptimer_ns());
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());
 #endif
-    free(vm.app);
 }
