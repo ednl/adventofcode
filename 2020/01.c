0ff9683f961dd717bddcea4b20aebd52dacf8eab
E. Dronkert <ednl@users.noreply.github.com>
2026-05-27 15:45:37 +0200

    new version

--- 2020/01.c
+++ 2020/01.c
@@ -1,43 +1,93 @@
+/**
+ * Advent of Code 2020
+ * Day 1: Report Repair
+ * https://adventofcode.com/2020/day/1
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 01.c
+ * Enable timer:
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 01.c
+ * Get minimum runtime from timer output in bash:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :    ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  9.4 µs
+ *     Raspberry Pi 5 (2.4 GHz)      :    ? µs
+ */
+
 #include <stdio.h>
+#include <stdlib.h>  // qsort
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define N   200
-#define SUM 2020
+#define FNAME "../aocinput/2020-01-input.txt"
+#define FSIZE 1024  // needed for my input: 990
+#define N      200  // numbers in input file
+#define SUM   2020  // from puzzle description
 
+static char input[FSIZE];
 static int data[N];
 
-static int find2(const int n)
+static int parseint(const char **s)
 {
-    for (int i = 0; i < n - 1; ++i)
-        for (int j = i + 1; j < n; ++j)
-            if (data[i] + data[j] == SUM)
-                return data[i] * data[j];
+    int x = 0;
+    while (**s & '0')
+        x = x * 10 + (*(*s)++ & 15);
+    (*s)++;  // skip newline
+    return x;
+}
+
+static int sort_int_asc(const void *p, const void *q)
+{
+    const int a = *(const int *)p;
+    const int b = *(const int *)q;
+    if (a < b) return -1;
+    if (a > b) return +1;
     return 0;
 }
 
-static int find3(const int n)
+// Part 1: find product of two numbers that sum to 2020
+static int twosum(const int *a, const int *b, const int sum)
 {
-    for (int i = 0; i < n - 2; ++i)
-        for (int j = i + 1; j < n - 1; ++j) {
-            const int part = data[i] + data[j];
-            if (part >= SUM)
-                continue;
-            for (int k = j + 1; k < n; ++k)
-                if (part + data[k] == SUM)
-                    return data[i] * data[j] * data[k];
-        }
+    while (a < b) {
+        if (*a + *b == sum)
+            return *a * *b;
+        while (a < b && *a + *b > sum)
+            b--;
+        while (a < b && *a + *b < sum)
+            a++;
+    }
     return 0;
 }
 
 int main(void)
 {
-    FILE *f = fopen("../aocinput/2020-01-input.txt", "r");
-    if (!f)
-        return 1;
-    int n = 0;
-    while (n < N && fscanf(f, "%d", &data[n]) == 1)
-        ++n;
+    // Read input file
+    FILE *f = fopen(FNAME, "rb");
+    fread(input, 1, FSIZE, f);
     fclose(f);
-    printf("Part 1: %d\n", find2(n));  // 802011
-    printf("Part 2: %d\n", find3(n));  // 248607374
-    return 0;
+
+#ifdef TIMER
+    starttimer();
+#endif
+
+    // Parse and sort input
+    const int *a = data;
+    int *b = data;
+    for (const char *c = input; *c; *b++ = parseint(&c));
+    qsort(data, N, sizeof *data, sort_int_asc);
+
+    // Part 1
+    printf("%d\n", twosum(a, --b, SUM));  // 802011
+
+    // Part 2
+    int p;
+    for (; !(p = twosum(a + 1, b, SUM - *a)); ++a);
+    printf("%d\n", *a * p);  // 248607374
+
+#ifdef TIMER
+    printf("Time: %.0f ns\n", stoptimer_ns());
+#endif
 }
