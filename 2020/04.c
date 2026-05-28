407289575e2fa7c61ea2f29c9cb243f928d55f1f
E. Dronkert <ednl@users.noreply.github.com>
2026-05-28 14:06:11 +0200

    C version

---
+++ 2020/04.c
@@ -0,0 +1,162 @@
+/**
+ * Advent of Code 2020
+ * Day 4: Passport Processing
+ * https://adventofcode.com/2020/day/4
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic -Wno-multichar 04.c
+ * Enable timer:
+ *     cc -O3 -march=native -mtune=native -Wno-multichar -DTIMER ../startstoptimer.c 04.c
+ * Get minimum runtime from timer output in bash:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : 34.1 µs
+ *     Raspberry Pi 5 (2.4 GHz)      : ? µs
+ */
+
+#include <stdio.h>
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
+
+#define FNAME "../aocinput/2020-04-input.txt"
+#define FSIZE 24000  // needed for my input: 21282 (+2 '\0')
+
+typedef enum field {
+    BYR, IYR, EYR, HGT, HCL, ECL, PID
+} Field;
+
+static char input[FSIZE];
+
+static int parseint(const char **s)
+{
+    int x = 0;
+    while (**s >= '0' && **s <= '9')
+        x = x * 10 + (*(*s)++ & 15);
+    return x;
+}
+
+static unsigned err(const char **s)
+{
+    for (; **s != '\n' && **s != ' '; (*s)++);
+    return 0;
+}
+
+static unsigned year(const char **s, const int min, const int max)
+{
+    (*s) += 4;  // skip key
+    const int yr = parseint(s);  // stop at first non-digit
+    if (**s == '\n' || **s == ' ')
+        return yr >= min && yr <= max;
+    return err(s);
+}
+
+static unsigned byr(const char **s)
+{
+    return year(s, 1920, 2002) << BYR;
+}
+
+static unsigned iyr(const char **s)
+{
+    return year(s, 2010, 2020) << IYR;
+}
+
+static unsigned eyr(const char **s)
+{
+    return year(s, 2020, 2030) << EYR;
+}
+
+static unsigned ecl(const char **s)
+{
+    (*s) += 4;  // skip key
+    unsigned val = 0;
+    while (**s != '\n' && **s != ' ')
+        val = val << 8 | *(*s)++;
+    switch (val) {
+        case 'amb': /* fall-through */
+        case 'blu': /* fall-through */
+        case 'brn': /* fall-through */
+        case 'grn': /* fall-through */
+        case 'gry': /* fall-through */
+        case 'hzl': /* fall-through */
+        case 'oth': return 1U << ECL;
+    }
+    return 0;
+}
+
+static unsigned hcl(const char **s)
+{
+    (*s) += 4;  // skip key
+    if (**s != '#')
+        return err(s);
+    int count = 0;
+    for ((*s)++; (**s >= '0' && **s <= '9') || (**s >= 'a' && **s <= 'f'); count++, (*s)++);
+    if (**s == '\n' || **s == ' ')
+        return count == 6 ? 1U << HCL : 0;
+    return err(s);
+}
+
+static unsigned hgt(const char **s)
+{
+    (*s) += 4;  // skip key
+    const int h = parseint(s);
+    unsigned unit = 0;
+    for (; **s != '\n' && **s != ' '; (*s)++)
+        unit = unit << 8 | **s;
+    switch (unit) {
+        case 'cm': return h >= 150 && h <= 193 ? 1U << HGT : 0;
+        case 'in': return h >=  59 && h <=  76 ? 1U << HGT : 0;
+    }
+    return 0;
+}
+
+static unsigned pid(const char **s)
+{
+    (*s) += 4;  // skip key
+    int count = 0;
+    for (; **s >= '0' && **s <= '9'; count++, (*s)++);
+    if (**s == '\n' || **s == ' ')
+        return count == 9 ? 1U << PID : 0;
+    return err(s);
+}
+
+int main(void)
+{
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) return 1;
+    fread(input, 1, FSIZE, f);
+    fclose(f);
+
+#ifdef TIMER
+    starttimer();
+#endif
+
+    int valid1 = 0, valid2 = 0;
+    unsigned present = 0, correct = 0;
+    for (const char *c = input; *c; ) {
+        switch (*c << 8 | *(c + 1)) {
+            case 'by': present |= 1U << BYR; correct |= byr(&c); break;
+            case 'ec': present |= 1U << ECL; correct |= ecl(&c); break;
+            case 'ey': present |= 1U << EYR; correct |= eyr(&c); break;
+            case 'hc': present |= 1U << HCL; correct |= hcl(&c); break;
+            case 'hg': present |= 1U << HGT; correct |= hgt(&c); break;
+            case 'iy': present |= 1U << IYR; correct |= iyr(&c); break;
+            case 'pi': present |= 1U << PID; correct |= pid(&c); break;
+            default: for (c += 4; *c != '\n' && *c != ' '; c++); break;  // skip optional key/val
+        }
+        c++;  // skip space or newline to next field or to empty line
+        if (*c == '\0' || *c == '\n') {  // end of passport
+            valid1 += present == 127U;
+            valid2 += correct == 127U;
+            present = correct = 0;
+            c++;  // skip null or newline (input[] must be at least 2 longer than data)
+        }
+    }
+    printf("%d %d\n", valid1, valid2);  // 245 133
+
+#ifdef TIMER
+    printf("Time: %.0f ns\n", stoptimer_ns());
+#endif
+}
