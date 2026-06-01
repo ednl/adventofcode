c0554e4d7af598a453d64a27e836ca5f2d63e2d3
E. Dronkert <ednl@users.noreply.github.com>
2026-06-01 04:38:41 +0200

    tiny speedup

--- 2020/01.c
+++ 2020/01.c
@@ -11,61 +11,26 @@
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :  5.21 µs (no sort: 12.3 µs)
- *     Mac Mini 2020 (M1 3.2 GHz)    :  9.4  µs (no sort: 18.4 µs)
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  5.00 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  9.4  µs
  *     Raspberry Pi 5 (2.4 GHz)      : 19.6  µs
  */
 
 #include <stdio.h>
-#ifndef NOSORT
-    #include <stdlib.h>  // qsort
-#endif
+#include <stdlib.h>  // qsort
+#include <stdint.h>  // uint64_t
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
 #define FNAME "../aocinput/2020-01-input.txt"
-#define FSIZE 1024  // needed for my input: 990
-#define N      200  // numbers in input file
-#define SUM   2020  // from puzzle description
+#define FSIZE 992  // must be divisible by 8. Needed for my input: 990=>992
+#define N     200  // numbers in input file
+#define SUM  2020  // from puzzle description
 
 static char input[FSIZE];
 static int data[N];
 
-static int parseint(const char **s)
-{
-    int x = 0;
-    while (**s >= '0')
-        x = x * 10 + (*(*s)++ & 15);
-    (*s)++;  // skip newline
-    return x;
-}
-
-#ifdef NOSORT
-static int find2(void)
-{
-    for (int i = 0; i < N - 1; ++i) {
-        const int r = SUM - data[i];
-        for (int j = i + 1; j < N; ++j)
-            if (r == data[j])
-                return data[i] * data[j];
-    }
-    return 0;
-}
-
-static int find3(void)
-{
-    for (int i = 0; i < N - 2; ++i)
-        for (int j = i + 1; j < N - 1; ++j) {
-            const int r = SUM - (data[i] + data[j]);
-            if (r > 0)
-                for (int k = j + 1; k < N; ++k)
-                    if (r == data[k])
-                        return data[i] * data[j] * data[k];
-        }
-    return 0;
-}
-#else
 static int sort_int_asc(const void *p, const void *q)
 {
     const int a = *(const int *)p;
@@ -88,7 +53,6 @@ static int twosum(const int *a, const int *b, const int sum)
     }
     return 0;
 }
-#endif
 
 int main(void)
 {
@@ -102,25 +66,39 @@ int main(void)
 #endif
 
     // Parse and sort input
-#ifndef NOSORT
-    const int *a = data;
-#endif
-    int *b = data;
-    for (const char *c = input; *c; *b++ = parseint(&c));
-#ifdef NOSORT
-    printf("%d\n", find2());  // 802011
-    printf("%d\n", find3());  // 248607374
-#else
-    qsort(data, N, sizeof *data, sort_int_asc);
+    {
+        uint64_t *inp64 = (uint64_t *)input;
+        for (int i = 0; i < (FSIZE >> 3); ++i)
+            inp64[i] &= UINT64_C(0x0f0f0f0f0f0f0f0f);
+
+        const char *c = input;
+        for (int i = 0; i < N; ++i)
+            if (*(c + 4) == '\n') {  // 4-digit number +newline
+                data[i] = *c * 1000
+                    + *(c + 1) * 100
+                    + *(c + 2) * 10
+                    + *(c + 3);
+                c += 5;
+            } else {  // 3-digit number +newline
+                data[i] = *c * 100
+                    + *(c + 1) * 10
+                    + *(c + 2);
+                c += 4;
+            }
+        qsort(data, N, sizeof *data, sort_int_asc);
+    }
 
     // Part 1
-    printf("%d\n", twosum(a, --b, SUM));  // 802011
+    printf("%d\n", twosum(data, data + (N - 1), SUM));  // 802011
 
     // Part 2
-    int p;
-    for (; !(p = twosum(a + 1, b, SUM - *a)); ++a);
-    printf("%d\n", *a * p);  // 248607374
-#endif
+    const int *a = data, *b = data + 2;
+    const int r = SUM - (*a + *(a + 1));
+    while (*b < r)
+        b++;
+    int p2;
+    for (; !(p2 = twosum(a + 1, b, SUM - *a)); ++a);
+    printf("%d\n", *a * p2);  // 248607374
 
 #ifdef TIMER
     printf("Time: %.0f ns\n", stoptimer_ns());
