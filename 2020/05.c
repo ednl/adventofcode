ee18b91d63478544c65d76226f8a10b1febe707e
E. Dronkert <ednl@users.noreply.github.com>
2026-06-02 12:25:58 +0200

    only check presence

--- 2020/05.c
+++ 2020/05.c
@@ -8,70 +8,63 @@
  *     cc -std=c17 -Wall -Wextra -pedantic 05.c
  * Enable timer:
  *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 05.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
  * Get minimum runtime from timer output in bash:
- *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 23.8 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 39.1 µs
- *     Raspberry Pi 5 (2.4 GHz)      : 78.6 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 1.55 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :    ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      :    ? µs
  */
 
 #include <stdio.h>
-#include <stdlib.h>  // qsort
+#include <stdbool.h>
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
 #define FNAME "../aocinput/2020-05-input.txt"
-#define N 761  // lines in input file
-#define L 10   // line length = 10x F/B/L/R (excl. newline)
-#define FSIZE (N * (L + 1))  // +newline
+#define PASS 761  // boarding passes (lines in input file)
+#define LEN  10   // line length = 10x F/B/L/R (excl. newline)
+#define RANGE (1 << LEN)  // all possible boarding pass IDs
+#define FSIZE (PASS * (LEN + 1))  // +newline
 
 static char input[FSIZE];
-static unsigned seat[N];
-
-// Qsort helper function: sort u32 ascending
-static int cmp_u32_asc(const void *p, const void *q)
-{
-    const unsigned a = *(const unsigned *)p;
-    const unsigned b = *(const unsigned *)q;
-    if (a < b) return -1;
-    if (a > b) return +1;
-    return 0;
-}
+static bool pass[RANGE];  // boarding pass ID in input file?
 
 int main(void)
 {
     FILE *f = fopen(FNAME, "rb");
     if (!f) return 1;
-    fread(input, FSIZE, 1, f);  // known fixed size
+    fread(input, FSIZE, 1, f);  // read one block of fixed size
     fclose(f);
 
 #ifdef TIMER
-    starttimer();
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
 #endif
 
-    // Parse
+    // Parse input file, check off boarding pass IDs
     const char *c = input;
-    for (int i = 0; i < N; ++c, ++i) {  // skip newline
-        unsigned id = 0;
-        while (*c != '\n')
-            id = id << 1 | !(*c++ & 4);  // F or L = 0, B or R = 1
-        seat[i] = id;
+    for (int i = 0; i < PASS; ++i) {
+        int id = 0;
+        for (int j = 0; j < LEN; ++j)
+            id = id << 1 | !(*c++ & 4);  // F/L = 0, B/R = 1
+        pass[id] = true;
+        c++;  // skip newline
     }
 
-    // Part 1
-    qsort(seat, N, sizeof *seat, cmp_u32_asc);
-    printf("%u\n", seat[N - 1]);  // 861
-
-    // Part 2
-    for (int i = 1; i < N; ++i)
-        if (seat[i] - 1 != seat[i - 1]) {  // gap before this seat?
-            printf("%u\n", seat[i] - 1);  // 633
-            break;
-        }
+    int id = RANGE - 2;  // "Your seat wasn't at the very front or back, though"
+    while (!pass[id])  // skip non-existing seats at the back
+        id--;
+    printf("%d", id--);  // 861
+    while (pass[id])
+        id--;
+    printf(" %d\n", id);  // 633
 
 #ifdef TIMER
-    printf("Time: %.0f ns\n", stoptimer_ns());
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
 #endif
 }
