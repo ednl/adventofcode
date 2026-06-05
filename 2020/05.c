518d1e40c253f3d78e97a872a94328ea0e6b0853
E. Dronkert <ednl@users.noreply.github.com>
2026-06-05 11:44:06 +0200

    Revert "find last during parse"
    This reverts commit 0d4b21ea41b29bc76edeb023b6be6f79f6631361.

--- 2020/05.c
+++ 2020/05.c
@@ -13,7 +13,7 @@
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 1.52 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 1.55 µs
  *     Mac Mini 2020 (M1 3.2 GHz)    :    ? µs
  *     Raspberry Pi 5 (2.4 GHz)      : 4.33 µs
  */
@@ -45,23 +45,23 @@ starttimer();
 for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
 #endif
 
-    // Parse input file, check boarding passes, mark last boarding pass
-    register int last = 0;
+    // Parse input file, check off boarding pass IDs
     const char *c = input;
     for (int i = 0; i < PASS; ++i) {
         int id = 0;
         for (int j = 0; j < LEN; ++j)
             id = id << 1 | !(*c++ & 4);  // F/L = 0, B/R = 1
-        if (id > last)
-            last = id;
-        pass[id] = true;  // check
+        pass[id] = true;
         c++;  // skip newline
     }
 
-    printf("%d", last--);  // 861
-    while (pass[last])
-        last--;
-    printf(" %d\n", last);  // 633
+    int id = RANGE - 2;  // "Your seat wasn't at the very front or back, though"
+    while (!pass[id])  // skip non-existing seats at the back
+        id--;
+    printf("%d", id--);  // 861
+    while (pass[id])
+        id--;
+    printf(" %d\n", id);  // 633
 
 #ifdef TIMER
 }
