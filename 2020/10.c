4e3ee3067a4e7df1de9a6557833efb7817cf955f
E. Dronkert <ednl@users.noreply.github.com>
2026-06-10 12:06:25 +0200

    style update

--- 2020/10.c
+++ 2020/10.c
@@ -5,68 +5,74 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *    clang -std=gnu17 -O3 -march=native -Wall -Wextra 10.c ../startstoptimer.c
- *    gcc   -std=gnu17 -O3 -march=native -Wall -Wextra 10.c ../startstoptimer.c
- * Get minimum runtime:
- *     m=99999999;for((i=0;i<5000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo $m;done
- * Minimum runtime:
- *     Mac Mini 2020 (M1 3.2 GHz)          : 15 µs
- *     Raspberry Pi 5 (2.4 GHz)            : 22 µs
- *     ThinkPad Ubuntu (i5 8250U 1.8 GHz)  : 22 µs
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) : 34 µs
- *     Air 2013 (i5 Haswell 4250U 1.3 GHz) : 46 µs
- *     Raspberry Pi 4 (1.8 GHz)            : 77 µs
+ *     cc -std=c17 -Wall -Wextra -pedantic -Wno-char-subscripts 10.c
+ * Enable timer:
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 10.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
+ * Get minimum runtime from timer output in bash:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 0.48 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : ? µs
  */
 
-#include <stdio.h>     // fopen, fclose, fgets, printf
+#include <stdio.h>
 #include <stdint.h>    // int64_t
 #include <inttypes.h>  // PRId64
 #include <stdbool.h>
-#include "../startstoptimer.h"
+#ifdef TIMER
+    #include <string.h>  // memset
+    #include "../startstoptimer.h"
+#endif
 
 #define FNAME "../aocinput/2020-10-input.txt"
-#define JOLTRANGE 256  // max value in input file: 256-1-3=252
-#define MINGAP 1
-#define MAXGAP 3
+#define FSIZE 400  // needed for my input: 352
+#define JOLTS 200  // needed for my imput: 180+1+3 = 184
+#define MAXGAP  3
 
-static bool adapter[JOLTRANGE];
-static int64_t arrangements[JOLTRANGE];
+static char input[FSIZE];
+static bool adapter[JOLTS];
+static int64_t arrangements[JOLTS];
 
 int main(void)
 {
-    starttimer();
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) return 1;
+    const char *const end = input + fread(input, 1, FSIZE, f);
+    fclose(f);
 
-    FILE *f = fopen(FNAME, "r");
-    if (!f)
-        return 1;
-    int maxjolts = 0;
-    char buf[8];
-    while (fgets(buf, sizeof buf, f)) {
-        int jolts = 0;
-        char *s = buf;
-        while (*s != '\n')
-            jolts = jolts * 10 + (*s++ & 15);  // slightly faster than fscanf
+#ifdef TIMER
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
+    memset(arrangements, 0, sizeof arrangements);
+#endif
+
+    const uint64_t *const end64 = (const uint64_t *)input + ((end - input) >> 3) + 1;
+    for (uint64_t *digit = (uint64_t *)input; digit != end64; ++digit)
+        *digit &= UINT64_C(0x0f0f0f0f0f0f0f0f);
+    int gaps = 1, maxjolts = 0;
+    for (const char *c = input; c != end; ++gaps, ++c) {
+        int jolts = *c++;
+        while (*c != '\n')
+            jolts = jolts * 10 + *c++;
         adapter[jolts] = true;
         if (jolts > maxjolts)
             maxjolts = jolts;
     }
-    fclose(f);
     adapter[0] = true;  // charging outlet at 0 jolts
     adapter[maxjolts += MAXGAP] = true;  // device being charged at the other end
 
     // Part 1
-    int gapcount1 = 0, gapcount3 = 0;
-    for (int i = 0; i < maxjolts;) {
-        int j = i + 1;
-        while (!adapter[j])
-            ++j;
-        switch (j - i) {
-            case MINGAP: ++gapcount1; break;
-            case MAXGAP: ++gapcount3; break;
-        }
-        i = j;
-    }
-    printf("%d\n", gapcount1 * gapcount3);  // 2574
+    // Assume no gaps of length 2:
+    //   x + 3y = M (maxjolts)
+    //   x +  y = N (gaps)
+    //       2y = (M-N)
+    //        y = (M-N)/2
+    //   x      = N - (M-N)/2
+    const int gap3 = (maxjolts - gaps) >> 1;
+    printf("%d", (gaps - gap3) * gap3);  // pasrt 1: 2574
 
     // Part 2
     arrangements[0] = 1;  // charging outlet is fixed
@@ -76,8 +82,10 @@ int main(void)
     for (int j = MAXGAP; j <= maxjolts; ++j)
         for (int i = j - MAXGAP; i < j; ++i)
             arrangements[j] += arrangements[i] * adapter[i];
-    printf("%"PRId64"\n", arrangements[maxjolts]);  // 2644613988352
+    printf(" %"PRId64"\n", arrangements[maxjolts]);  // part 2: 2644613988352
 
-    printf("Time: %.0f µs\n", stoptimer_us());
-    return 0;
+#ifdef TIMER
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
+#endif
 }
