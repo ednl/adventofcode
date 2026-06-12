a13339e8f69f7509b5e03f035b6f77e86d31d0f1
E. Dronkert <ednl@users.noreply.github.com>
2026-06-12 14:53:39 +0200

    some style updates

--- 2020/11.c
+++ 2020/11.c
@@ -3,11 +3,9 @@
 
 #include <stdio.h>   // fopen, fclose, getline
 #include <stdlib.h>  // free
-
-// For terminal display
-#define HOME    "\e[H"
-#define CLEAR   "\e[2J"
-#define HIDECUR "\e[?25l"
+#include <string.h>  // memcpy
+#include <stdint.h>  // int8_t
+#include "../startstoptimer.h"
 
 #define INPUT_FLOOR '.'
 #define INPUT_EMPTY 'L'
@@ -15,29 +13,29 @@
 #define STATE_FLOOR  -1
 #define STATE_EMPTY   0
 #define STATE_OCCUP   1
-
-// Test input
-// Correct answers: 37, 26
-// #define H (10 + 2)
-// #define W (10 + 2)
-// const char *inp = "../aocinput/2020-11-example.txt";
-
-// My puzzle input
-// Correct answers: 2303, 2057
-// Determine manually from input file
-#define H (91 + 2)
-#define W (98 + 2)
-const char *inp = "../aocinput/2020-11-input.txt";
+#if EXAMPLE
+    // For terminal display
+    #define HOME    "\e[H"
+    #define CLEAR   "\e[2J"
+    #define HIDECUR "\e[?25l"
+    #define H (10 + 2)
+    #define W (10 + 2)
+    const char *inp = "../aocinput/2020-11-example.txt";
+#else
+    #define H (91 + 2)
+    #define W (98 + 2)
+    const char *inp = "../aocinput/2020-11-input.txt";
+#endif
 
 // Original data and two copies for evolving
 // signed because state = -1 | 0 | 1
 #define LEN (H * W)
-int data[LEN], area1[LEN], area2[LEN];
+int8_t data[LEN], area1[LEN], area2[LEN];
 
 // Read and parse the input file
 // '.' = floor (-1) never changes
 // 'L' = empty seat (0) can become '#' = occupied (1)
-void read()
+void read(void)
 {
     FILE *fp;
     char *s = NULL;
@@ -73,29 +71,27 @@ void read()
 }
 
 // Fresh copies of input data
-void init()
+void init(void)
 {
-    for (int i = 0; i < LEN; ++i) {
-        area1[i] = area2[i] = data[i];
-    }
+    memcpy(area1, data, sizeof data);
+    memcpy(area2, data, sizeof data);
 }
 
-int neighbours(int *area, int index, int thresh, int part)
+int neighbours(int8_t *area, int index, int threshold, int part)
 {
-    int i, j, k, dk, nb = 0;
-    for (i = -1; i <= 1; ++i) {      // row offset (direction)
-        dk = i * W - 1;              // first change in index
-        for (j = -1; j <= 1; ++j) {  // col offset (direction)
-            if (i || j) {            // not the centre
-                k = index + dk;      // first new index in this direction
-                while (1) {          // area is surrounded by empty seats, so loop will break
+    int nb = 0;
+    for (int i = -1; i <= 1; ++i) {      // row offset (direction)
+        int dk = i * W - 1;              // first change in index
+        for (int j = -1; j <= 1; ++j) {  // col offset (direction)
+            if (i || j) {                // not the centre
+                int k = index + dk;      // first new index in this direction
+                while (1) {              // area is surrounded by empty seats, so loop will break
                     // Part 1 = only direct neighbours, part 2 = 8 lines of sight
                     if (area[k] != STATE_FLOOR || part == 1) {
                         // Check & stop looking in this direction when reaching a seat
                         if (area[k] == STATE_OCCUP) {
-                            if (++nb == thresh) {  // reached threshold?
-                                return 1;         // no point in counting more
-                            }
+                            if (++nb == threshold)  // reached threshold?
+                                return 1;        // no point in counting more
                         }
                         break;
                     }
@@ -108,11 +104,11 @@ int neighbours(int *area, int index, int thresh, int part)
     return 0;  // did not reach threshold
 }
 
-int evolve(int *a1, int *a2, int part)
+int evolve(int8_t *restrict a1, int8_t *restrict a2, int part)
 {
-    int r, c, k = W, thresh = part + 3, changed = 0;
-    for (r = 1; r < H - 1; ++r) {
-        for (c = 1; c < W - 1; ++c) {
+    int k = W, threshold = part + 3, changed = 0;
+    for (int r = 1; r < H - 1; ++r) {
+        for (int c = 1; c < W - 1; ++c) {
             ++k;
             if (a1[k] == STATE_EMPTY) {
                 if (neighbours(a1, k, 1, part)) {  // any neighbours?
@@ -122,7 +118,7 @@ int evolve(int *a1, int *a2, int part)
                     changed = 1;
                 }
             } else if (a1[k] == STATE_OCCUP) {
-                if (neighbours(a1, k, thresh, part)) {  // at least 4 or 5 neighbours?
+                if (neighbours(a1, k, threshold, part)) {  // at least 4 or 5 neighbours?
                     a2[k] = STATE_EMPTY;
                     changed = 1;
                 } else {
@@ -136,17 +132,15 @@ int evolve(int *a1, int *a2, int part)
 }
 
 // Print map of occupied seats on a (large) terminal
-void show(int *area)
+#if EXAMPLE
+void show(const int8_t *const area)
 {
-    int i, j, k;
-    unsigned char c;
-    volatile unsigned long delay;
-
     printf("%s", CLEAR);
     printf("%s", HOME);
-    for (i = 0; i < H; ++i) {
-        for (j = 0; j < W; ++j) {
-            k = i * W + j;
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+            int k = i * W + j;
+            char c;
             switch (area[k]) {
                 case STATE_FLOOR: c = INPUT_FLOOR; break;
                 case STATE_EMPTY: c = INPUT_EMPTY; break;
@@ -158,38 +152,36 @@ void show(int *area)
         printf("\n");
     }
     printf("\n");
-    for (delay = 0; delay < 20000000; ++delay);
+    for (volatile unsigned long delay = 0; delay < 20000000; ++delay);
 }
+#endif
 
 // Count number of occupied seats
-int occupied(int *area)
+int occupied(const int8_t *const area)
 {
     int n = 0;
-    for (int i = 0; i < LEN; ++i) {
-        if (area[i] == STATE_OCCUP) {
-            n++;
-        }
-    }
+    for (int i = 0; i < LEN; ++i)
+        n += area[i] == STATE_OCCUP;
     return n;
 }
 
 int main(void)
 {
-    int part, *p, *q, *t;
-
     read();
-    for (part = 1; part <= 2; ++part) {
+    starttimer();
+    for (int part = 1; part <= 2; ++part) {
         init();
-        p = (int *)area1;
-        q = (int *)area2;
+        int8_t *p = area1;
+        int8_t *q = area2;
         while (evolve(p, q, part)) {
-            t = p;
+            void *const tmp = p;
             p = q;
-            q = t;
-            // show(p);
+            q = tmp;
+        #if EXAMPLE
+            show(p);
+        #endif
         }
-        printf("%d\n", occupied(p));
+        printf("%d\n", occupied(p));  // part 1: 2303, part 2: 2057
     }
-
-    return 0;
+    printf("Time: %.0f us\n", stoptimer_us());
 }
