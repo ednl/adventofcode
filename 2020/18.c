dee2cd4be25a9ceec76337b17822a14a15d6285d
E. Dronkert <ednl@users.noreply.github.com>
2026-06-21 11:16:17 +0200

    faster with stacks but runtime not halved

--- 2020/18.c
+++ 2020/18.c
@@ -13,7 +13,7 @@
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 19.8 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 14.2 µs
  *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
  *     Raspberry Pi 5 (2.4 GHz)      : ? µs
  */
@@ -27,54 +27,28 @@
 
 #define FNAME "../aocinput/2020-18-input.txt"
 #define FSIZE 24000  // needed for my input: 20422
+#define STACK1 4   // needed for my input: 2
+#define STACK2 16  // needed for my input: 12
 
 typedef uint64_t u64;
+typedef struct term {
+    u64 val;
+    char op;
+} Term;
+
+static const Term mark = {0};
+static const Term init = {.val = 0, .op = '+'};
+
 static char input[FSIZE];
-static char *ch = input;
-
-static u64 sum1(void)
-{
-    u64 res = 0;
-    char op = '+';
-    for (;;)
-        if (*ch >= '0') {
-            if (op == '+')
-                res += *ch++ & 15;
-            else
-                res *= *ch++ & 15;
-        } else switch (*ch) {
-            case '*': /* fall-through */
-            case '+': op = *ch; ch += 2; break;
-            case ' ': ch++; break;
-            case '(': ch++; if (op == '+') res += sum1(); else res *= sum1(); break;
-            default : ch++; return res;  // ')' or '\n'
-        }
-}
-
-static u64 sum2(void)
-{
-    u64 res = 0;
-    char op = '+';
-    for (;;)
-        if (*ch >= '0') {
-            if (op == '+')
-                res += *ch++ & 15;
-            else
-                res *= *ch++ & 15;
-        } else switch (*ch) {
-            case '*': ch += 2; return res * sum2();
-            case '+': ch += 2; op = '+'; break;
-            case ' ': ch++; break;
-            case '(': ch++; if (op == '+') res += sum2(); else res *= sum2(); break;
-            default : ch++; return res;  // ')' or '\n'
-        }
-}
+static Term stack1[STACK1];
+static Term stack2[STACK2];
+static int slen1, slen2;
 
 int main(void)
 {
     FILE *f = fopen(FNAME, "rb");
     if (!f) return 1;
-    fread(input, 1, FSIZE, f);
+    const char *const end = input + fread(input, 1, FSIZE, f);
     fclose(f);
 
 #ifdef TIMER
@@ -82,10 +56,60 @@ starttimer();
 for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
 #endif
 
-    u64 part1 = 0, part2 = 0;
-    for (ch = input; *ch; part1 += sum1());
-    for (ch = input; *ch; part2 += sum2());
-    printf("%"PRIu64" %"PRIu64"\n", part1, part2);  // 280014646144 9966990988262
+    u64 sum1 = 0, sum2 = 0;
+    Term term1 = init, term2 = init;
+    for (const char *c = input; c != end; ++c)
+        switch (*c) {  // cases ordered by value was fastest for me
+            case '\n':
+                sum1 += term1.val;
+                term1 = init;
+                while (slen2) {  // clean up stack with multiplications
+                    const u64 tmp = term2.val;
+                    term2 = stack2[--slen2];
+                    if (term2.op == '+') term2.val += tmp; else term2.val *= tmp;
+                }
+                sum2 += term2.val;
+                term2 = init;
+                break;
+            case ' ':  // skip
+                break;
+            case '(':  // push
+                stack1[slen1++] = term1;
+                term1 = init;
+                stack2[slen2++] = mark;  // first add marker (op=0)
+                stack2[slen2++] = term2;
+                term2 = init;
+                break;
+            case ')':  // pop
+                {
+                    const u64 tmp = term1.val;
+                    term1 = stack1[--slen1];
+                    if (term1.op == '+') term1.val += tmp; else term1.val *= tmp;
+                }
+                while (stack2[--slen2].op) {  // remove marker last
+                    const u64 tmp = term2.val;
+                    term2 = stack2[slen2];
+                    if (term2.op == '+') term2.val += tmp; else term2.val *= tmp;
+                }
+                break;
+            case '*':  // mul, push for part 2
+                term1.op = term2.op = '*';
+                stack2[slen2++] = term2;
+                term2 = init;
+                c++;  // operator always +space, so c+=2
+                break;
+            case '+':  // add
+                term1.op = term2.op = '+';
+                c++;  // operator always +space, so c+=2
+                break;
+            default:
+                {
+                    const u64 tmp = *c & 15;
+                    if (term1.op == '+') term1.val += tmp; else term1.val *= tmp;
+                    if (term2.op == '+') term2.val += tmp; else term2.val *= tmp;
+                }
+        }
+    printf("%"PRIu64" %"PRIu64"\n", sum1, sum2);  // 280014646144 9966990988262
 
 #ifdef TIMER
 }
---
+++ 2020/18recursion.c
@@ -0,0 +1,94 @@
+/**
+ * Advent of Code 2020
+ * Day 18: Operation Order
+ * https://adventofcode.com/2020/day/18
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 18recursion.c
+ * Enable timer:
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 18recursion.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
+ * Get minimum runtime from timer output in bash:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  19.8 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :   ?   µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 124   µs
+ */
+
+#include <stdio.h>
+#include <stdint.h>  // uint64_t
+#include <inttypes.h>  // PRId64
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
+
+#define FNAME "../aocinput/2020-18-input.txt"
+#define FSIZE 24000  // needed for my input: 20422
+
+typedef uint64_t u64;
+static char input[FSIZE];
+static char *ch = input;
+
+static u64 sum1(void)
+{
+    u64 res = 0;
+    char op = '+';
+    for (;;)
+        if (*ch >= '0') {
+            if (op == '+')
+                res += *ch++ & 15;
+            else
+                res *= *ch++ & 15;
+        } else switch (*ch) {
+            case '*': /* fall-through */
+            case '+': op = *ch; ch += 2; break;
+            case ' ': ch++; break;
+            case '(': ch++; if (op == '+') res += sum1(); else res *= sum1(); break;
+            default : ch++; return res;  // ')' or '\n'
+        }
+}
+
+static u64 sum2(void)
+{
+    u64 res = 0;
+    char op = '+';
+    for (;;)
+        if (*ch >= '0') {
+            if (op == '+')
+                res += *ch++ & 15;
+            else
+                res *= *ch++ & 15;
+        } else switch (*ch) {
+            case '*': ch += 2; return res * sum2();  // max recursion level: 10
+            case '+': ch += 2; op = '+'; break;
+            case ' ': ch++; break;
+            case '(': ch++; if (op == '+') res += sum2(); else res *= sum2(); break;
+            default : ch++; return res;  // ')' or '\n'
+        }
+}
+
+int main(void)
+{
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) return 1;
+    fread(input, 1, FSIZE, f);
+    fclose(f);
+
+#ifdef TIMER
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
+#endif
+
+    u64 part1 = 0, part2 = 0;
+    for (ch = input; *ch; part1 += sum1());
+    for (ch = input; *ch; part2 += sum2());
+    printf("%"PRIu64" %"PRIu64"\n", part1, part2);  // 280014646144 9966990988262
+
+#ifdef TIMER
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
+#endif
+}
