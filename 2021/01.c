15dcf57050a0f46eae73fd08ed54b19dc9fd66d9
E. Dronkert <ednl@users.noreply.github.com>
2026-07-01 13:11:28 +0200

    faster parser

--- 2021/01.c
+++ 2021/01.c
@@ -13,12 +13,13 @@
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 4.03 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 0.83 µs
  *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
  *     Raspberry Pi 5 (2.4 GHz)      : ? µs
  */
 
 #include <stdio.h>
+#include <stdint.h>  // uint32_t
 #include "../startstoptimer.h"
 
 #define FNAME "../aocinput/2021-01-input.txt"
@@ -26,21 +27,23 @@
 #define N 2000
 
 static char input[FSIZE];
-static int depth[N];
+static uint32_t data[N];  // 4 bytes per entry
 
-static int inc(const int win)
+// Count how many sliding windows of measurements
+// are increasing from one to the next
+static int inc(const int windowsize)
 {
-    int n = 0;
-    for (int i = win; i < N; ++i)
-        n += depth[i] > depth[i - win];
-    return n;
+    int count = 0;
+    for (int i = windowsize; i < N; ++i)
+        count += data[i] > data[i - windowsize];
+    return count;
 }
 
 int main(void)
 {
     FILE *f = fopen(FNAME, "rb");
     if (!f) return 1;
-    fread(input, 1, FSIZE, f);  // read char-by-char to max size
+    fread(input, 1, FSIZE, f);  // read char-by-char to EOF or max size
     fclose(f);
 
 #ifdef TIMER
@@ -48,20 +51,20 @@ starttimer();
 for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
 #endif
 
+    // Count number of lines with 3-digit numbers
+    int three = 0;
+    for (const char *eol = input + 3; *eol == '\n'; three++, eol += 4);
+
     const char *c = input;
-    for (int i = 0; i < N; ++i) {
-        register int a = *(const int *)c;
-        register int n = a & 15;
-        a >>= 8; n = n * 10 + (a & 15);
-        a >>= 8; n = n * 10 + (a & 15);
-        a >>= 8;
-        if (a != '\n') {
-            n = n * 10 + (a & 15);
-            c += 5;
-        } else
-            c += 4;
-        depth[i] = n;
-    }
+    // Parse 3-digit numbers: only convert byte order from big-endian
+    // (=character stream order) to little-endian, leave ASCII values
+    for (int i = 0; i < three; c += 4, i++)
+        data[i] = *c << 16 | *(c + 1) << 8 | *(c + 2);
+    // Parse 4-digit numbers, same deal
+    for (int i = three; i < N; c += 5, i++)
+        data[i] = *c << 24 | *(c + 1) << 16 | *(c + 2) << 8 | *(c + 3);
+
+    // Count increasing (windows of) measurements
     printf("%d %d\n", inc(1), inc(3));  // 1466 1491
 
 #ifdef TIMER
