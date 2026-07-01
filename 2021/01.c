0a31c1792843d6b0be0f1cb970e16cd9e4a35b01
E. Dronkert <ednl@users.noreply.github.com>
2026-07-01 03:29:08 +0200

    style update & timing

--- 2021/01.c
+++ 2021/01.c
@@ -1,9 +1,34 @@
+/**
+ * Advent of Code 2021
+ * Day 1:
+ * https://adventofcode.com/2021/day/1
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 01.c
+ * Enable timer:
+ *     cc -O3 -march=native -mtune=native ../startstoptimer.c 01.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
+ * Get minimum runtime from timer output in bash:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 4.03 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : ? µs
+ */
+
 #include <stdio.h>
+#include "../startstoptimer.h"
 
+#define FNAME "../aocinput/2021-01-input.txt"
+#define FSIZE 10000  // needed for my input: 9690
 #define N 2000
-static int depth[N] = {0};
 
-static int inc(int win)
+static char input[FSIZE];
+static int depth[N];
+
+static int inc(const int win)
 {
     int n = 0;
     for (int i = win; i < N; ++i)
@@ -13,18 +38,34 @@ static int inc(int win)
 
 int main(void)
 {
-    FILE *f = fopen("../aocinput/2021-01-input.txt", "r");
-    if (!f)
-        return 1;
-
-    int i = 0;
-    while (i < N && fscanf(f, "%d ", &depth[i]) == 1)
-        ++i;
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) return 1;
+    fread(input, 1, FSIZE, f);  // read char-by-char to max size
     fclose(f);
-    if (i != N)
-        return 2;
 
-    printf("Part 1: %d\n", inc(1));
-    printf("Part 2: %d\n", inc(3));
-    return 0;
+#ifdef TIMER
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
+#endif
+
+    const char *c = input;
+    for (int i = 0; i < N; ++i) {
+        register int a = *(const int *)c;
+        register int n = a & 15;
+        a >>= 8; n = n * 10 + (a & 15);
+        a >>= 8; n = n * 10 + (a & 15);
+        a >>= 8;
+        if (a != '\n') {
+            n = n * 10 + (a & 15);
+            c += 5;
+        } else
+            c += 4;
+        depth[i] = n;
+    }
+    printf("%d %d\n", inc(1), inc(3));  // 1466 1491
+
+#ifdef TIMER
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
+#endif
 }
