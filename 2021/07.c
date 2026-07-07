999da980d3c4b5a8c00bff47afb5c62c8b5c806c
E. Dronkert <ednl@users.noreply.github.com>
2026-07-07 18:03:27 +0200

    style update

--- 2021/07.c
+++ 2021/07.c
@@ -1,124 +1,85 @@
+/**
+ * Advent of Code 2021
+ * Day 7: The Treachery of Whales
+ * https://adventofcode.com/2021/day/7
+ * By: E. Dronkert https://github.com/ednl
+ *
+ * Compile:
+ *     cc -std=c17 -Wall -Wextra -pedantic 07.c
+ * Enable timer:
+ *     cc -O3 -march=native -mtune=native ../startstoptimer.c 07.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
+ * Get minimum runtime from timer output in bash:
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ * Minimum runtime measurements:
+ *     Macbook Pro 2024 (M4 4.4 GHz) :    ? µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : 1.83 µs
+ *     Raspberry Pi 5 (2.4 GHz)      :    ? µs
+ */
+
 #include <stdio.h>
-#include <stdlib.h>
-// #include <stdbool.h>
-// #include <stdint.h>
-// #include <time.h>
+#include <stdlib.h>  // abs
+#ifdef TIMER
+    #include <string.h>  // memset
+    #include "../startstoptimer.h"
+#endif
 
-#define N 1000
-static int data[N] = {0};
+#define FNAME "../aocinput/2021-07-input.txt"
+#define FSIZE 4000  // needed for my input: 3896
+#define N 1000  // 1000 values in input file
+#define M 2000  // value range 0..1999 (needed for my input: 1879)
 
-// static uint64_t nanotimer(void)
-// {
-//     static bool start = true;
-//     static struct timespec t0;
-
-//     if (start) {
-//         start = false;
-//         clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
-//         return 0;
-//     } else {
-//         struct timespec t1;
-//         clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-//         start = true;
-//         return (uint64_t)(t1.tv_sec - t0.tv_sec) * UINT64_C(1000000000) + (uint64_t)t1.tv_nsec - (uint64_t)t0.tv_nsec;
-//     }
-// }
-
-static int cmp(const void *a, const void *b)
-{
-    int p = *(const int*)a;
-    int q = *(const int*)b;
-    return (q < p) - (p < q);
-}
-
-static int cost1(const int target)
-{
-    int cost = 0;
-    for (int i = 0; i < N; ++i)
-        cost += abs(data[i] - target);
-    return cost;
-}
-
-static int cost2(const int target)
-{
-    int cost = 0;
-    for (int i = 0; i < N; ++i) {
-        int n = abs(data[i] - target);
-        cost += n * (n + 1) / 2;
-    }
-    return cost;
-}
-
-// Gradient of cost1 between x and x+1 (=> dx=1)
-// Sum of: +1 for points to the left,
-//         -1 for points to the right
-static int grad1(const int x)
-{
-    int dy = 0;
-    for (int i = 0; i < N; ++i)
-        dy += data[i] <= x;
-    return (dy << 1) - N;
-}
-
-// Gradient of cost2 between x and x+1 (=> dx=1)
-// Sum of: distance to x+1 for points to the left,
-//         distance to x   for points to the right
-static int grad2(const int x)
-{
-    int dy = 0;
-    for (int i = 0; i < N; ++i) {
-        const int dist = x - data[i];
-        dy += dist + (dist >= 0);
-    }
-    return dy;
-}
-
-static int bisect(int a, int b, int (*f)(const int x), int (*g)(const int x))
-{
-    while (a < b) {
-        const int c = (a + b)/2, d = g(c);
-        if (d > 0)
-            b = c;
-        else if (d < 0)
-            a = c + 1;
-        else {
-            a = c;
-            break;
-        }
-    }
-    return f(a);
-}
+static char input[FSIZE];
+static int pos[N];
+static char crab[M];
 
 int main(void)
 {
-    FILE *f = fopen("../aocinput/2021-07-input.txt", "r");
-    for (int i = 0, a; fscanf(f, "%d", &a) == 1; ++i) {
-        data[i] = a;
-        fgetc(f);  // CSV with newline
-    }
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) return 1;
+    // const char *const end = input + fread(input, 1, FSIZE, f);
+    fread(input, 1, FSIZE, f);
     fclose(f);
 
-    // Start timer
-    // nanotimer();
+#ifdef TIMER
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
+    memset(crab, 0, sizeof crab);
+#endif
 
-    // Linear cost => take median
-    // N is even, so N/2 is good (N/2-1 will be the same)
-    qsort(data, N, sizeof(int), cmp);
-    printf("Part 1: %d\n", cost1(data[N/2]));  // 349357
-
+    // Parse, index, sum
+    int sum = 0;
+    const char *c = input;
+    for (int i = 0; i < N; ++c, ++i) {
+        int x = *c++ & 15;
+        while (*c > ',')
+            x = x * 10 + (*c++ & 15);
+        pos[i] = x;
+        crab[x]++;
+        sum += x;
+    }
     // Quadratic cost => take mean
     // Could go 1 either way because of rounding, but this happens to work for me
-    int mean = 0;
-    for (int i = 0; i < N; ++i)
-        mean += data[i];
-    mean /= N;
-    printf("Part 2: %d\n", cost2(mean));  // 96708205
+    const int mean = sum / N;
 
-    // Run time of the algorithms only, not the disk read
-    // printf("%.9f s\n", nanotimer() * 1e-9);
-    printf("%d\n", bisect(0, 2000, cost1, grad1));
-    // printf("%d\n", bisect(0, 2000, cost2, grad2));
-    for (int i = 338; i < 345; ++i)
-        printf("%d %d %d\n", i, cost1(i), grad1(i));
-    return 0;
+    // Linear cost => take median
+    int i = 0;
+    for (int count = 0; count < 500; ++i)
+        count += crab[i];
+    const int median = i - 1;  // pos 500 and 501 are identical, so overshot by 1
+
+    // Cost functions
+    int fuel1 = 0, fuel2 = 0;
+    for (int i = 0; i < N; ++i) {
+        fuel1 += abs(pos[i] - median);
+        const int x = abs(pos[i] - mean);
+        fuel2 += x * (x + 1) >> 1;
+    }
+    printf("%d %d\n", fuel1, fuel2);  // 349357 96708205
+
+#ifdef TIMER
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
+#endif
 }
