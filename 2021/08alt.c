08373f863d45f9e1184b5dee9a3975bfc8c75ed6
E. Dronkert <ednl@users.noreply.github.com>
2026-07-08 12:30:41 +0200

    timing

--- 2021/08alt.c
+++ 2021/08alt.c
@@ -13,9 +13,9 @@
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :    ? µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 14.0 µs
- *     Raspberry Pi 5 (2.4 GHz)      :    ? µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  6.18 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : 14.0  µs
+ *     Raspberry Pi 5 (2.4 GHz)      : 16.6  µs
  *
  * From: https://github.com/maneatingape/advent-of-code-rust/blob/main/src/year2021/day08.rs
  * Listing each digit and the number of segments that are lit when that digit is displayed:
