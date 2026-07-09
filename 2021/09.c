6644be8d4d52060d4b8d88ab82811faceb62b30c
E. Dronkert <ednl@users.noreply.github.com>
2026-07-09 17:55:58 +0200

    topn

--- 2021/09.c
+++ 2021/09.c
@@ -5,20 +5,20 @@
  * By: E. Dronkert https://github.com/ednl
  *
  * Compile:
- *     cc -std=c17 -Wall -Wextra -pedantic 09.c
+ *     cc -std=c17 -Wall -Wextra -pedantic ../topn.c 09.c
  * Enable timer:
- *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 09.c
+ *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c ../topn.c 09.c
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
  *     Macbook Pro 2024 (M4 4.4 GHz) :  84 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 144 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : 134 µs
  *     Raspberry Pi 5 (2.4 GHz)      :   ? µs
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include "../topn.h"
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
@@ -28,14 +28,14 @@
 #define MAXBASINS  256
 #define STACKSIZE  100
 
-typedef struct {
+typedef struct point {
     int x, y;
 } Point;
 
-static unsigned char height[DIM + 2][DIM + 2] = {0};  // extend grid to simplify edge cases
-static bool done[DIM + 2][DIM + 2] = {0};  // keep track of already processed locations
-static Point todo[STACKSIZE] = {0};  // keep track of locations still to be processed
-static int basinsize[MAXBASINS] = {0};  // size of each basin found
+static char height[DIM + 2][DIM + 2];  // extend grid to simplify edge cases
+static bool done[DIM + 2][DIM + 2];    // keep track of already processed locations
+static Point todo[STACKSIZE];          // keep track of locations still to be processed
+static int basinsize[MAXBASINS];       // size of each basin found
 
 // Floodfill the basin, return basin size
 static int fillbasin(int x, int y)
@@ -47,10 +47,6 @@ static int fillbasin(int x, int y)
         if (!done[p.x][p.y]) {
             done[p.x][p.y] = true;
             if (height[p.x][p.y] != MAXHEIGHT) {
-                if (sp > STACKSIZE - 4) {
-                    fprintf(stderr, "Imminent stack overflow!\n");
-                    return -1;
-                }
                 if (!done[p.x + 1][p.y]) todo[sp++] = (Point){p.x + 1, p.y};
                 if (!done[p.x - 1][p.y]) todo[sp++] = (Point){p.x - 1, p.y};
                 if (!done[p.x][p.y + 1]) todo[sp++] = (Point){p.x, p.y + 1};
@@ -63,11 +59,11 @@ static int fillbasin(int x, int y)
 }
 
 // Sort int array in descending order
-static int descending(const void *a, const void *b)
+static int descending(const void *p, const void *q)
 {
-    const int p = *(const int*)a;
-    const int q = *(const int*)b;
-    return (p < q) - (q < p);
+    const int a = *(const int *)p;
+    const int b = *(const int *)q;
+    return (a < b) - (b < a);
 }
 
 int main(void)
@@ -75,17 +71,17 @@ int main(void)
     FILE *f = fopen("../aocinput/2021-09-input.txt", "r");
     for (int i = 1; i <= DIM; ++i) {
         for (int j = 1; j <= DIM; ++j)
-            height[i][j] = (unsigned char)(fgetc(f) - '0');
-        fgetc(f);  // newline
+            height[i][j] = fgetc(f) - '0';
+        fgetc(f);  // skip newline
     }
     fclose(f);
 
 #ifdef TIMER
-    // Timer starts after read from disk
     starttimer();
 #endif
+
     // None shall pass
-    for (int i = 0; i < DIM + 2; ++i)
+    for (int i = 1; i < DIM + 1; ++i)
         height[i][0] = height[0][i] = height[i][DIM + 1] = height[DIM + 1][i] = MAXHEIGHT;
 
     int risk = 0;
@@ -97,20 +93,14 @@ int main(void)
                 height[i][j] < height[i + 1][j] &&
                 height[i][j] < height[i][j + 1]) {
                 risk += height[i][j] + 1;
-                if (basincount == MAXBASINS) {
-                    fprintf(stderr, "The number of basins is too damn high!\n");
-                    return 1;
-                }
                 basinsize[basincount++] = fillbasin(i, j);
             }
         }
     }
-    qsort(basinsize, basincount, sizeof *basinsize, descending);
+    topn(basinsize, 3, basincount, sizeof *basinsize, descending);
+    printf("%d %d\n", risk, basinsize[0] * basinsize[1] * basinsize[2]);  // 506 931200
 
-    printf("Part 1: %d\n", risk);  // 506
-    printf("Part 2: %d\n", basinsize[0] * basinsize[1] * basinsize[2]);  // 931200
 #ifdef TIMER
     printf("Time: %.0f us\n", stoptimer_us());
 #endif
-    return 0;
 }
