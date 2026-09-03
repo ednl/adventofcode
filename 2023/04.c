8876e762795a0d9b5fc8873a04a085e25242eae2
E. Dronkert <ednl@users.noreply.github.com>
2026-09-03 21:58:11 +0200

    style update

--- 2023/04.c
+++ 2023/04.c
@@ -8,66 +8,82 @@
  *     cc -std=c17 -Wall -Wextra -pedantic 04.c
  * Enable timer:
  *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 04.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
  * Get minimum runtime from timer output in bash:
- *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz)       :  17 µs
- *     Mac Mini 2020 (M1 3.2 GHz)          :  24 µs
- *     Raspberry Pi 5 (2.4 GHz)            :  52 µs
- *     iMac 2013 (i5 Haswell 4570 3.2 GHz) :  59 µs
- *     Raspberry Pi 4 (1.8 GHz)            : 124 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 2.90 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    : ? µs
+ *     iMac 2013 (i5 4570 3.2 GHz)   : ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      : ? µs
  */
 
-#include <stdio.h>    // fopen, fclose, fgets, printf
-#include <string.h>   // memset
-#include <stdbool.h>  // bool
-#include "../startstoptimer.h"
+#include <stdio.h>
+#include <string.h>  // memset
+#ifdef TIMER
+    #include "../startstoptimer.h"
+#endif
 
-#define NAME "../aocinput/2023-04-input.txt"
-#define N 202  // lines (cards) in input
+#define FNAME "../aocinput/2023-04-input.txt"
+#define CARDS 202  // 202 cards (= lines in input file)
+#define WINS  10   // 10 winning numbers
+#define HAVE  25   // 25 numbers I have
 
-static bool win[100];  // numbers in range [0..99]
-static int copies[N];
+// "Card xxx:" + "| " + newline = 12 chars
+#define FSIZE (CARDS * ((WINS + HAVE) * 3 + 12))
 
-static int min(const int a, const int b)
+static char input[FSIZE];
+static int copies[CARDS];
+
+static inline int min(const int a, const int b)
 {
     return a < b ? a : b;
 }
 
-// Convert 1 or 2 digits to number (' ' & 15 = 0, so leading space is fine)
-static int readnum(const char *s)
+static inline int readnum(const char *s)
 {
-    return ((*s & 15) * 10) + (*(s + 1) & 15);
+    return (*s & 15) * 10 + (*(s + 1) & 15);
 }
 
 int main(void)
 {
-    FILE *f = fopen(NAME, "r");
-    if (!f) { fputs("File not found.\n", stderr); return 1; }
+    FILE *f = fopen(FNAME, "rb");  // fread requires binary mode
+    if (!f) return 1;
+    fread(input, FSIZE, 1, f);  // read as single block
+    fclose(f);
 
-    starttimer();
+#ifdef TIMER
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
+    memset(copies, 0, sizeof copies);
+#endif
+
     int part1 = 0, part2 = 0;
-    char buf[128];  // every line is 116 chars + '\n\0'
-    for (int card = 0; fgets(buf, sizeof buf, f); ) {  // read one line at a time
-        memset(win, 0, sizeof win);       // reset winning number scoring
-        char *s = buf + 10;               // skip to first winning number
-        for (; *s != '|'; s += 3)         // until '|', skip to next winning number
-            win[readnum(s)] = true;       // mark as winning number
+    const char *c = input;
+    for (int card = 0; card < CARDS; ) {
+        c += 10;  // skip "Card xxx: "
+        __uint128_t wins = 0;
+        for (int i = 0; i < WINS; c += 3, ++i)
+            wins |= (__uint128_t)1 << readnum(c);
 
-        int match = 0;                    // how many winning numbers I have on this card
-        for (s += 2; *s; s += 3)          // skip divider, until '\0', skip to next number I have
-            match += win[readnum(s)];     // match numbers I have with winning
+        c += 2;  // skip "| "
+        int match = 0;  // winning numbers count on this card
+        for (int i = 0; i < HAVE; c += 3, ++i)
+            match += wins >> readnum(c) & 1;
+        part1 += match ? (1 << (match - 1)) : 0;
 
-        part1 += match ? (1 << (match - 1)) : 0;  // score for current card
-        const int add = ++copies[card];   // count original card as one more copy
+        const int add = ++copies[card];  // count original card as one more copy
         part2 += add;
 
-        const int lim = min(++card + match, N);  // 1 past maximum index of extra copies to add
+        const int lim = min(++card + match, CARDS);  // 1 past maximum index of extra copies to add
         for (int i = card; i < lim; ++i)  // add extra copies
             copies[i] += add;
     }
-    fclose(f);
-    printf("%d %d\n", part1, part2);  // 24733 5422730
-    printf("Time: %.0f us\n", stoptimer_us());
-    return 0;
+    printf("%u %u\n", part1, part2);  // 24733 5422730
+
+#ifdef TIMER
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
+#endif
 }
