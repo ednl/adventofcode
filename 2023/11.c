79f9679ef7b4d09b0bca6a78964f0e30d99321de
E. Dronkert <ednl@users.noreply.github.com>
2026-09-10 10:36:59 +0200

    style update

--- 2023/11.c
+++ 2023/11.c
@@ -8,39 +8,36 @@
  *     cc -std=c17 -Wall -Wextra -pedantic 11.c
  * Enable timer:
  *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 11.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
  * Get minimum runtime from timer output in bash:
- *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 11.5 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 17   µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  9.5 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  ?   µs
  *     Raspberry Pi 5 (2.4 GHz)      :  ?   µs
  */
 
-#include <stdio.h>     // fopen, fclose, fgets, printf
-#include <stdlib.h>    // malloc, free
+#include <stdio.h>
 #include <stdint.h>    // int64_t
 #include <inttypes.h>  // PRId64
 #ifdef TIMER
+    #include <string.h>  // memset
     #include "../startstoptimer.h"  // timer
 #endif
 
-#define EXAMPLE 0
-#if EXAMPLE
-    #define NAME "../aocinput/2023-11-example.txt"
-    #define N 10   // rows and columns in image map
-    #define M 100  // empty space expansion factor
-#else
-    #define NAME "../aocinput/2023-11-input.txt"
-    #define N 140      // rows and columns in image map
-    #define M 1000000  // empty space expansion factor
-#endif
+#define FNAME "../aocinput/2023-11-input.txt"
+#define N 140      // rows and columns in image map
+#define M 1000000  // empty space expansion factor part 2
+#define G 512      // galaxies, needed for my input: 430
 
-static char image[N][N + 2];
+static char image[N][N + 1];
 static int xcount[N], ycount[N], xshift[N], yshift[N];
+static int xpos[G], ypos[G];
 
 // Nifty algorithm by /u/NikitaSkybytskyi
 // https://www.reddit.com/r/adventofcode/comments/18fqxuq/an_on_algorithm_for_day_11/kcvwlev/
-static int64_t dist(int *pos, int *shift, int n, int64_t f)
+static int64_t dist(const int *const restrict pos, const int *const restrict shift, const int n, const int64_t f)
 {
     int64_t sum = 0, partial = 0;
     for (int i = 0; i < n; ++i) {
@@ -53,27 +50,28 @@ static int64_t dist(int *pos, int *shift, int n, int64_t f)
 
 int main(void)
 {
-    FILE *f = fopen(NAME, "r");
-    if (!f) { fputs("File not found.\n", stderr); return 1; }
-    for (int i = 0; i < N; ++i)
-        fgets(image[i], sizeof *image, f);
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) { fputs("File not found: "FNAME, stderr); return 1; }
+    fread(image, sizeof image, 1, f);  // read as one block
     fclose(f);
 
 #ifdef TIMER
-    starttimer();
+starttimer();
+for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
+    memset(xcount, 0, sizeof xcount);
+    memset(ycount, 0, sizeof ycount);
 #endif
 
     int galaxies = 0;
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < N; ++j)
             if (image[i][j] == '#') {
-                ++galaxies;
-                ++xcount[j];
-                ++ycount[i];
+                xcount[j]++;
+                ycount[i]++;
+                galaxies++;
             }
 
-    int *xpos = malloc((size_t)galaxies * sizeof *xpos);
-    int *ypos = malloc((size_t)galaxies * sizeof *ypos);
+    // x-pos sorted by x, y-pos sorted by y to avoid negative distance
     for (int i = 0, k = 0; i < N; ++i)
         for (int j = 0; j < xcount[i]; ++j)
             xpos[k++] = i;
@@ -88,15 +86,12 @@ int main(void)
         yshift[i] = yshift[i - 1] + !ycount[i];
     }
 
-    // example: 374, 8410
-    // input: 9608724, 904633799472
-    printf("Part 1: %"PRId64"\n", dist(xpos, xshift, galaxies, 2) + dist(ypos, yshift, galaxies, 2));
-    printf("Part 2: %"PRId64"\n", dist(xpos, xshift, galaxies, M) + dist(ypos, yshift, galaxies, M));
+    printf("%"PRId64" %"PRId64"\n",  // 9608724 904633799472
+        dist(xpos, xshift, galaxies, 2) + dist(ypos, yshift, galaxies, 2),
+        dist(xpos, xshift, galaxies, M) + dist(ypos, yshift, galaxies, M));
 
 #ifdef TIMER
-    printf("Time: %.0f ns\n", stoptimer_ns());
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
 #endif
-
-    free(xpos);
-    free(ypos);
 }
