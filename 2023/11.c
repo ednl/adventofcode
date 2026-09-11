1bab1ad2152ad03239869c6033d60ac2b03683b9
E. Dronkert <ednl@users.noreply.github.com>
2026-09-11 15:45:14 +0200

    simpler but also more complicated

--- 2023/11.c
+++ 2023/11.c
@@ -13,9 +13,9 @@
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) :  9.1 µs
- *     Mac Mini 2020 (M1 3.2 GHz)    : 13.6 µs
- *     Raspberry Pi 5 (2.4 GHz)      : 25.2 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) :  7.80 µs
+ *     Mac Mini 2020 (M1 3.2 GHz)    :  ? µs
+ *     Raspberry Pi 5 (2.4 GHz)      :  ? µs
  */
 
 #include <stdio.h>
@@ -27,27 +27,12 @@
 #endif
 
 #define FNAME "../aocinput/2023-11-input.txt"
-#define N 140      // rows and columns in image map
-#define M 1000000  // empty space expansion factor part 2
-#define G 512      // galaxies, needed for my input: 430
+#define N  140      // rows and columns in image map
+#define F1 2        // empty space expansion factor part 1
+#define F2 1000000  // empty space expansion factor part 2
 
-static char image[N][N + 1];          // +newline
-static uint8_t xcount[N], ycount[N];  // number of galaxies per line (reset between timing loops, so small datatype)
-static int xpos[G], ypos[G];          // galaxy positions
-static int xshift[N], yshift[N];      // how many shifts (= empty lines) until here
-
-// Nifty algorithm by /u/NikitaSkybytskyi
-// https://www.reddit.com/r/adventofcode/comments/18fqxuq/an_on_algorithm_for_day_11/kcvwlev/
-static int64_t dist(const int *const restrict pos, const int *const restrict shift, const int n, const int64_t f)
-{
-    int64_t sum = 0, partial = 0;
-    for (int i = 0; i < n; ++i) {
-        const int64_t exp = (f - 1) * shift[pos[i]] + pos[i];
-        sum += i * exp - partial;
-        partial += exp;
-    }
-    return sum;
-}
+static char image[N][N + 1];  // +newline
+static uint8_t gx[N];  // galaxies per col
 
 int main(void)
 {
@@ -59,37 +44,46 @@ int main(void)
 #ifdef TIMER
 starttimer();
 for (int TIMERLOOP = 0; TIMERLOOP < 1000; ++TIMERLOOP) {
-    memset(xcount, 0, sizeof xcount);
-    memset(ycount, 0, sizeof ycount);
+    memset(gx, 0, sizeof gx);
 #endif
 
-    int galaxies = 0;
-    for (int i = 0; i < N; ++i)
+    // Parse and y-axis result
+    int64_t part1 = 0, part2 = 0;
+    register int64_t sum1 = 0, sum2 = 0, tot = 0;
+    for (int i = 0; i < N; ++i) {
+        register int64_t gy = 0;  // galaxies per row
         for (int j = 0; j < N; ++j)
             if (image[i][j] == '#') {
-                xcount[j]++;
-                ycount[i]++;
-                galaxies++;
+                gx[j]++;
+                gy++;
             }
-
-    // x-pos sorted by x, y-pos sorted by y to avoid negative distance
-    for (int i = 0, k = 0; i < N; ++i)
-        for (int j = 0; j < xcount[i]; ++j)
-            xpos[k++] = i;
-    for (int i = 0, k = 0; i < N; ++i)
-        for (int j = 0; j < ycount[i]; ++j)
-            ypos[k++] = i;
-
-    xshift[0] = !xcount[0];
-    yshift[0] = !ycount[0];
-    for (int i = 1; i < N; ++i) {
-        xshift[i] = xshift[i - 1] + !xcount[i];
-        yshift[i] = yshift[i - 1] + !ycount[i];
+        if (gy) {
+            part1 += gy * sum1;
+            part2 += gy * sum2;
+            tot += gy;  // running total of galaxies
+            sum1 += tot;
+            sum2 += tot;
+        } else {
+            sum1 += tot * F1;
+            sum2 += tot * F2;
+        }
     }
 
-    printf("%"PRId64" %"PRId64"\n",  // 9608724 904633799472
-        dist(xpos, xshift, galaxies, 2) + dist(ypos, yshift, galaxies, 2),
-        dist(xpos, xshift, galaxies, M) + dist(ypos, yshift, galaxies, M));
+    // Sum of all pairwise distances on one axis
+    // https://github.com/maneatingape/advent-of-code-rust/blob/main/src/year2023/day11.rs
+    sum1 = sum2 = tot = 0;
+    for (int i = 0; i < N; ++i)
+        if (gx[i]) {
+            part1 += gx[i] * sum1;
+            part2 += gx[i] * sum2;
+            tot += gx[i];  // running total of galaxies
+            sum1 += tot;
+            sum2 += tot;
+        } else {
+            sum1 += tot * F1;
+            sum2 += tot * F2;
+        }
+    printf("%"PRId64" %"PRId64"\n", part1, part2);  // 9608724 904633799472
 
 #ifdef TIMER
 }
