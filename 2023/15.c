57cf7d075aa3d1d058c91381f9ef1cc66c6406a7
E. Dronkert <ednl@users.noreply.github.com>
2026-09-15 10:40:01 +0200

    debug

--- 2023/15.c
+++ 2023/15.c
@@ -13,7 +13,7 @@
  * Get minimum runtime from timer output in bash:
  *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz) : 13.1 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz) : 11.9 µs
  *     Mac Mini 2020 (M1 3.2 GHz)    : 21.3 µs
  *     Raspberry Pi 5 (2.4 GHz)      : 81.4 µs
  */
@@ -50,8 +50,10 @@ static inline u32 next(const u32 prev, const u8 byte)
 static void rem(const u32 box, const u32 label)
 {
     for (u8 i = 0; i < count[box]; ++i)
-        if (lens[box][i].label == label)
+        if (lens[box][i].label == label) {
             memmove(&lens[box][i], &lens[box][i + 1], (--count[box] - i) * sizeof (Lens));
+            return;  // thanks /u/terje_wiig_mathisen for spotting that I forgot this
+        }
 }
 
 // Insert lens into box
