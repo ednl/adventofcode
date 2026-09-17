431966cf7927c054498f1b06c07a64532bf01646
E. Dronkert <ednl@users.noreply.github.com>
2026-09-17 16:08:40 +0200

    style update

--- 2023/18.c
+++ 2023/18.c
@@ -12,103 +12,94 @@
  *     cc -std=c17 -Wall -Wextra -pedantic 18.c
  * Enable timer:
  *     cc -O3 -march=native -mtune=native -DTIMER ../startstoptimer.c 18.c
+ * Test output with timer enabled:
+ *     ./a.out | tail -n1
  * Get minimum runtime from timer output in bash:
- *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out|tail -n1|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
+ *     m=99999999;for((i=0;i<20000;++i));do t=$(./a.out 2>&1 1>/dev/null|awk '{print $2}');((t<m))&&m=$t&&echo "$m ($i)";done
  * Minimum runtime measurements:
- *     Macbook Pro 2024 (M4 4.4 GHz)    : 2.46 µs
- *     Apple M1 Mac Mini 2020 (3.2 GHz) : 4.08 µs
+ *     Macbook Pro 2024 (M4 4.4 GHz)    :    ? µs
+ *     Apple M1 Mac Mini 2020 (3.2 GHz) : 2.94 µs
  *     Raspberry Pi 5 (2.4 GHz)         :    ? µs
  */
 
-#include <stdio.h>     // fopen, fclose, fscanf, printf
+#include <stdio.h>
 #include <stdint.h>    // int64_t
-#include <inttypes.h>  // PRId64
+#include <inttypes.h>  // PRIu64
 #ifdef TIMER
     #include "../startstoptimer.h"
 #endif
 
-#define EXAMPLE 0
-#if EXAMPLE
-    #define NAME "../aocinput/2023-18-example.txt"
-    #define N 14  // number of trench digging instructions in example
-#else
-    #define NAME "../aocinput/2023-18-input.txt"
-    #define N 766  // number of trench digging instructions in input
-#endif
+#define FNAME "../aocinput/2023-18-input.txt"
+#define FSIZE ((1<<13)|(1<<12))  // 12288, needed for my input: 10759
 
-typedef enum dir {
-    RIGHT=0, DOWN, LEFT, UP
-} Dir;
+typedef enum dir {R, D, L, U} Dir;  // R=0, D=1, L=2, U=3
+static char input[FSIZE];
 
-typedef struct dig {
-    Dir dir;  // 0..3 = R,D,L,U
-    int len;  // must hold 20-bit number for part 2
-} Dig;
-
-// Two different trenches for parts 1 & 2
-static Dig trench[2][N];
-
-static Dir char2dir(const int c)
+// Shoelace: A = 1/2 . sum((y[i] + y[i+1]).(x[i] - x[i+1]))
+// https://en.wikipedia.org/wiki/Shoelace_formula
+// For two points on horizontal line: y[i] = y[i+1], so y[i] + y[i+1] = 2y
+// For two points on vertical line  : x[i] = x[i+1], so x[i] - x[i+1] = 0
+// Bring the 1/2 in the sum: A = sum(y.dx) for horizontal lines only.
+static void shoelace(int64_t *const restrict a, int64_t *const restrict b, int64_t *const restrict y, const int len, const int dir)
 {
-    switch (c) {
-        case 'R': return RIGHT;
-        case 'D': return DOWN;
-        case 'L': return LEFT;
-        case 'U': return UP;
+    switch (dir) {
+        case R: *a -= *y * len; break;
+        case D: *y +=      len; break;
+        case L: *a += *y * len; break;
+        case U: *y -=      len; break;
     }
-    return 0;
+    *b += len;
+}
+
+// Pick: i = A - b/2 + 1, but add border b
+// https://en.wikipedia.org/wiki/Pick%27s_theorem
+// A can be negative, depending on direction of Shoelace
+static int64_t pick(const int64_t a, const int64_t b)
+{
+    return (a > 0 ? a : -a) + (b >> 1) + 1;
 }
 
 int main(void)
 {
-    FILE *f = fopen(NAME, "r");
-    if (!f) { fputs("File not found.\n", stderr); return 1; }
-    for (int i = 0, dir, len, hex; i < N && fscanf(f, "%c %2d (#%6x)\n", &dir, &len, &hex) == 3; ++i) {
-        trench[0][i] = (Dig){char2dir(dir), len};  // digging instructions for part 1
-        trench[1][i] = (Dig){hex & 3, hex >> 4};   // last hex digit: 0=R,1=D,2=L,3=U
-    }
+    FILE *f = fopen(FNAME, "rb");
+    if (!f) { fputs("File not found: "FNAME, stderr); return 1; }
+    fread(input, 1, FSIZE, f);  // read single bytes until EOF
     fclose(f);
 
-#if EXAMPLE || defined(DEBUG)
-    const char *dir2char = "RDLU";
-    for (int i = 0; i < N; ++i) {
-        const Dig *const t0 = &trench[0][i];
-        const Dig *const t1 = &trench[1][i];
-        printf("%c %d | %c %6d\n", dir2char[t0->dir], t0->len, dir2char[t1->dir], t1->len);
-    }
-    printf("\n");
-#endif
-
 #ifdef TIMER
-    starttimer();
+starttimer();
+for (unsigned TIMERLOOP = 1000; TIMERLOOP--; ) {
 #endif
 
-    const Dig *t = &trench[0][0];  // start at first trench dig instruction from part 1
-    for (int part = 1; part <= 2; ++part) {
-        int64_t a = 0, b = 0, y = 0;  // area, border, y-position
-        for (int i = 0; i < N; ++i, ++t) {  // trench array must be exactly the right size to seamlessly go from part 1 to 2
-            // Shoelace: A = 1/2 . sum((y[i] + y[i+1]).(x[i] - x[i+1]))
-            // https://en.wikipedia.org/wiki/Shoelace_formula
-            // For two points on horizontal line: y[i] = y[i+1], so y[i] + y[i+1] = 2y
-            // For two points on vertical line: x[i] = x[i+1], so x[i] - x[i+1] = 0
-            // Bring the 1/2 in the sum: A = sum(y.dx) for horizontal lines only.
-            switch (t->dir) {
-                case RIGHT: a -= y * t->len; break;  // area shrinks using Shoelace
-                case DOWN : y +=     t->len; break;  // adjust position by trench length down (only y needed)
-                case LEFT : a += y * t->len; break;  // area grows using Shoelace
-                case UP   : y -=     t->len; break;  // adjust position by trench length up (only y needed)
-            }
-            b += t->len;  // trench length of width 1 = border area
+    int64_t a1 = 0, b1 = 0, y1 = 0;
+    int64_t a2 = 0, b2 = 0, y2 = 0;
+    for (const char *c = input; *c; c += 8) {
+        // Part 1
+        const int dir1 = (*c * 143 + 69) >> 6 & 3;  // RDLU = 0123
+        int len1;
+        if (*(c + 3) == ' ') {
+            len1 = *(c + 2) & 15;  // 2..9
+            c += 6;
+        } else {
+            len1 = 10 | (*(c + 3) & 1);  // 10,11
+            c += 7;
         }
-        // Pick: i = A - b/2 + 1, but add border b
-        // https://en.wikipedia.org/wiki/Pick%27s_theorem
-        // A can be negative, depending on direction of Shoelace
-        // example:    62    952408144115
-        // input  : 46334 102000662718092
-        printf("Part %d: %"PRId64"\n", part, (a >= 0 ? a : -a) + b/2 + 1);
+        shoelace(&a1, &b1, &y1, len1, dir1);
+
+        // Part 2
+        const int len2 =
+              (( *c      & 16 ?  *c      & 15 : ( *c      & 7) + 9) << 16)
+            | ((*(c + 1) & 16 ? *(c + 1) & 15 : (*(c + 1) & 7) + 9) << 12)
+            | ((*(c + 2) & 16 ? *(c + 2) & 15 : (*(c + 2) & 7) + 9) <<  8)
+            | ((*(c + 3) & 16 ? *(c + 3) & 15 : (*(c + 3) & 7) + 9) <<  4)
+            | ((*(c + 4) & 16 ? *(c + 4) & 15 : (*(c + 4) & 7) + 9));
+        const int dir2 = *(c + 5) & 3;
+        shoelace(&a2, &b2, &y2, len2, dir2);
     }
+    printf("%"PRIu64" %"PRIu64"\n", pick(a1, b1), pick(a2, b2)); // 46334 102000662718092
 
 #ifdef TIMER
-    printf("Time: %.0f ns\n", stoptimer_ns());
+}
+fprintf(stderr, "Time: %.0f ns\n", stoptimer_us());  // 1000 loops: µs=ns
 #endif
 }
